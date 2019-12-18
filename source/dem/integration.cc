80ee58935cf9dc4e14ea70a28812017e2f33c39a
blaisb <blais.bruno@gmail.com>
2019-12-17 22:40:24 -0500

    Fixed indentation to follow Lethe convention

--- source/dem/integration.cc
+++ source/dem/integration.cc
@@ -6,56 +6,84 @@
  */
 
 #include "dem/integration.h"
+
 #include <deal.II/particles/particle_handler.h>
 using namespace dealii;
 
-Integration::Integration() {
-}
+Integration::Integration()
+{}
 
 
-void Integration::eulerIntegration(Particles::ParticleHandler<3,3> &particle_handler, ReadInputScript readInput)
+void Integration::eulerIntegration(
+  Particles::ParticleHandler<3, 3> &particle_handler,
+  ReadInputScript                   readInput)
 {
-	for (auto particle = particle_handler.begin(); particle != particle_handler.end(); ++particle)
-	{
-		//Acceleration calculation:
-		particle->get_properties()[10] = readInput.g[0] + (particle->get_properties()[13]) / readInput.mass;
-		particle->get_properties()[11] = readInput.g[1] + (particle->get_properties()[14]) / readInput.mass;
-		particle->get_properties()[12] = readInput.g[2] + (particle->get_properties()[15]) / readInput.mass;
+  for (auto particle = particle_handler.begin();
+       particle != particle_handler.end();
+       ++particle)
+    {
+      // Acceleration calculation:
+      particle->get_properties()[10] =
+        readInput.g[0] + (particle->get_properties()[13]) / readInput.mass;
+      particle->get_properties()[11] =
+        readInput.g[1] + (particle->get_properties()[14]) / readInput.mass;
+      particle->get_properties()[12] =
+        readInput.g[2] + (particle->get_properties()[15]) / readInput.mass;
 
-		//Velocity integration:
-		particle->get_properties()[7] = particle->get_properties()[7] + readInput.dt * particle->get_properties()[10];
-		particle->get_properties()[8] = particle->get_properties()[8] + readInput.dt * particle->get_properties()[11];
-		particle->get_properties()[9] = particle->get_properties()[9] + readInput.dt * particle->get_properties()[12];
-		//Position integration:
-		particle->get_properties()[4] = particle->get_properties()[4] + readInput.dt * particle->get_properties()[7];
-		particle->get_properties()[5] = particle->get_properties()[5] + readInput.dt * particle->get_properties()[8];
-		particle->get_properties()[6] = particle->get_properties()[6] + readInput.dt * particle->get_properties()[9];
+      // Velocity integration:
+      particle->get_properties()[7] =
+        particle->get_properties()[7] +
+        readInput.dt * particle->get_properties()[10];
+      particle->get_properties()[8] =
+        particle->get_properties()[8] +
+        readInput.dt * particle->get_properties()[11];
+      particle->get_properties()[9] =
+        particle->get_properties()[9] +
+        readInput.dt * particle->get_properties()[12];
+      // Position integration:
+      particle->get_properties()[4] =
+        particle->get_properties()[4] +
+        readInput.dt * particle->get_properties()[7];
+      particle->get_properties()[5] =
+        particle->get_properties()[5] +
+        readInput.dt * particle->get_properties()[8];
+      particle->get_properties()[6] =
+        particle->get_properties()[6] +
+        readInput.dt * particle->get_properties()[9];
 
-		particle->set_location({particle->get_properties()[4], particle->get_properties()[5], particle->get_properties()[6]});
-	}
+      particle->set_location({particle->get_properties()[4],
+                              particle->get_properties()[5],
+                              particle->get_properties()[6]});
+    }
 }
 
 /*
-void Integration::velVerIntegration(Particles::ParticleHandler<3,3> &particle_handler, float dt)
+void Integration::velVerIntegration(Particles::ParticleHandler<3,3>
+&particle_handler, float dt)
 {
-	for (auto particle = particle_handler.begin(); particle != particle_handler.end(); ++particle)
-	{
-		//Position integration:
-		particle->get_properties()[4] = particle->get_properties()[4] + dt * (particle->get_properties()[7] + 0.5 * dt * (particle->get_properties()[10]));
-		particle->get_properties()[5] = particle->get_properties()[5] + dt * (particle->get_properties()[8] + 0.5 * dt * (particle->get_properties()[11]));
-		particle->get_properties()[6] = particle->get_properties()[6] + dt * (particle->get_properties()[9] + 0.5 * dt * (particle->get_properties()[12]));
-		//Velocity integration:
-		//******************************************************************
-		//here it need the new acceleration which should be updated after the contact force calculation:
-		particle->get_properties()[7] = () + 0.5 * dt * ();
-		//******************************************************************
+  for (auto particle = particle_handler.begin(); particle !=
+particle_handler.end(); ++particle)
+  {
+    //Position integration:
+    particle->get_properties()[4] = particle->get_properties()[4] + dt *
+(particle->get_properties()[7] + 0.5 * dt * (particle->get_properties()[10]));
+    particle->get_properties()[5] = particle->get_properties()[5] + dt *
+(particle->get_properties()[8] + 0.5 * dt * (particle->get_properties()[11]));
+    particle->get_properties()[6] = particle->get_properties()[6] + dt *
+(particle->get_properties()[9] + 0.5 * dt * (particle->get_properties()[12]));
+    //Velocity integration:
+    //******************************************************************
+    //here it need the new acceleration which should be updated after the
+contact force calculation: particle->get_properties()[7] = () + 0.5 * dt * ();
+    //******************************************************************
 
 
 
 
 
-		particle->set_location({particle->get_properties()[4], particle->get_properties()[5], particle->get_properties()[6]});
-	}
+    particle->set_location({particle->get_properties()[4],
+particle->get_properties()[5], particle->get_properties()[6]});
+  }
 
 }
 */
