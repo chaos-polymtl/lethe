57655689a254928f1eaa72586c854ceec53e0529
blaisb <blais.bruno@gmail.com>
2019-12-17 23:03:19 -0500

    More refactoring Now unto the real test writing and code optimization

--- include/dem/read_input_script.h
+++ include/dem/read_input_script.h
@@ -1,9 +1,22 @@
-/*
- * ReadInputScript.h
+/* ---------------------------------------------------------------------
  *
- *  Created on: Sep 24, 2019
- *      Author: meteor
+ * Copyright (C) 2019 - 2019 by the Lethe authors
+ *
+ * This file is part of the Lethe library
+ *
+ * The Lethe library is free software; you can use it, redistribute
+ * it, and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * The full text of the license can be found in the file LICENSE at
+ * the top level of the Lethe distribution.
+ *
+ * ---------------------------------------------------------------------
+
+ *
+ * Author: Shahab Golshan, Polytechnique Montreal, 2019
  */
+
 #ifndef READINPUTSCRIPT_H_
 #define READINPUTSCRIPT_H_
 
