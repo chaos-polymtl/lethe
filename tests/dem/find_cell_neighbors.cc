4f6afcd49cf09130ffbfea7a77f32adeb6af80ba
blaisb <blais.bruno@gmail.com>
2019-12-29 14:35:55 -0500

    Some work on the Euler integration, but the test still does not work. We need to cut the dependency on the parameter handler for the other functions. Just pass the structures that are required. The integrators are a good place to start for that

--- tests/dem/find_cell_neighbors.cc
+++ tests/dem/find_cell_neighbors.cc
@@ -1,3 +1,25 @@
+/* ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2019 - 2019 by the Lethe authors
+ *
+ * This file is part of the Lethe library
+ *
+ * The Lethe library is free software; you can use it, redistribute
+ * it, and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * The full text of the license can be found in the file LICENSE at
+ * the top level of the Lethe distribution.
+ *
+ * ---------------------------------------------------------------------
+
+ *
+ * Author: Shahab Golshan, Polytechnique Montreal, 2019-
+ */
+
+// Generate a triangulation that is twiced refined and check if the cells
+// neighbors are the correct ones
+
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/grid_tools.h>
 #include <deal.II/grid/tria.h>
