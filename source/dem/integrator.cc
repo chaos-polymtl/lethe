fc8fe21ac87e392670c23810ab9f89238ce7072e
Bruno Blais <blais.bruno@gmail.com>
2020-01-23 19:50:20 -0500

    Temporarily disabled the RK2 integrator because I am unsure if it is viably implemented (actually I am unsure if our current architecture is made to support multiple stages integrations, we might need to add a mechanism to call the calculation of forces from the integrator instead)

--- source/dem/integrator.cc
+++ source/dem/integrator.cc
@@ -17,74 +17,78 @@ Integrator<dim, spacedim>::Integrator()
 // Why are all the numbers for the properties hard-coded? This should
 // Come out of an enum class or something like this...
 
-template <int dim, int spacedim>
-void
-Integrator<dim, spacedim>::rk2Integration(
-  Particles::ParticleHandler<dim, spacedim> &particle_handler,
-  Point<dim>                                 g,
-  float                                      dt)
-{
-  for (auto particle = particle_handler.begin();
-       particle != particle_handler.end();
-       ++particle)
-    {
-      // Acceleration calculation:
-      double axStar = particle->get_properties()[10];
-      double ayStar = particle->get_properties()[11];
-      double azStar = particle->get_properties()[12];
-
-      particle->get_properties()[10] =
-        g[0] +
-        (particle->get_properties()[13]) / (particle->get_properties()[19]);
-      particle->get_properties()[11] =
-        g[1] +
-        (particle->get_properties()[14]) / (particle->get_properties()[19]);
-      particle->get_properties()[12] =
-        g[2] +
-        (particle->get_properties()[15]) / (particle->get_properties()[19]);
+// Not sure if this implementation of the RK2 integrator is valid and there are
+// currently no unit tests for it
 
 
-      // Velocity integration:
-      double vxStar = particle->get_properties()[7];
-      double vyStar = particle->get_properties()[8];
-      double vzStar = particle->get_properties()[9];
+// template <int dim, int spacedim>
+// void
+// Integrator<dim, spacedim>::rk2Integration(
+//  Particles::ParticleHandler<dim, spacedim> &particle_handler,
+//  Point<dim>                                 g,
+//  float                                      dt)
+//{
+//  for (auto particle = particle_handler.begin();
+//       particle != particle_handler.end();
+//       ++particle)
+//    {
+//      // Acceleration calculation:
+//      double axStar = particle->get_properties()[10];
+//      double ayStar = particle->get_properties()[11];
+//      double azStar = particle->get_properties()[12];
 
-      particle->get_properties()[7] =
-        particle->get_properties()[7] +
-        (dt / 2.0) * (axStar + particle->get_properties()[10]);
+//      particle->get_properties()[10] =
+//        g[0] +
+//        (particle->get_properties()[13]) / (particle->get_properties()[19]);
+//      particle->get_properties()[11] =
+//        g[1] +
+//        (particle->get_properties()[14]) / (particle->get_properties()[19]);
+//      particle->get_properties()[12] =
+//        g[2] +
+//        (particle->get_properties()[15]) / (particle->get_properties()[19]);
 
-      particle->get_properties()[8] =
-        particle->get_properties()[8] +
-        (dt / 2.0) * (ayStar + particle->get_properties()[11]);
-      particle->get_properties()[9] =
-        particle->get_properties()[9] +
-        (dt / 2.0) * (azStar + particle->get_properties()[12]);
 
-      // Position integration:
-      particle->get_properties()[4] =
-        particle->get_properties()[4] +
-        (dt / 2.0) * (vxStar + particle->get_properties()[7]);
-      particle->get_properties()[5] =
-        particle->get_properties()[5] +
-        (dt / 2.0) * (vyStar + particle->get_properties()[8]);
-      particle->get_properties()[6] =
-        particle->get_properties()[6] +
-        (dt / 2.0) * (vzStar + particle->get_properties()[9]);
+//      // Velocity integration:
+//      double vxStar = particle->get_properties()[7];
+//      double vyStar = particle->get_properties()[8];
+//      double vzStar = particle->get_properties()[9];
 
-      particle->set_location({particle->get_properties()[4],
-                              particle->get_properties()[5],
-                              particle->get_properties()[6]});
+//      particle->get_properties()[7] =
+//        particle->get_properties()[7] +
+//        (dt / 2.0) * (axStar + particle->get_properties()[10]);
 
-      // Angular velocity using Euler:
-      /*
-      particle->get_properties()[16] = particle->get_properties()[16] +
-        (particle->get_properties()[21]) / (particle->get_properties()[20]);
-      particle->get_properties()[17] =particle->get_properties()[17] +
-        (particle->get_properties()[22]) / (particle->get_properties()[20]);
-      particle->get_properties()[18] = particle->get_properties()[18] +
-        (particle->get_properties()[23]) / (particle->get_properties()[20]);
-        */
-    }
-}
+//      particle->get_properties()[8] =
+//        particle->get_properties()[8] +
+//        (dt / 2.0) * (ayStar + particle->get_properties()[11]);
+//      particle->get_properties()[9] =
+//        particle->get_properties()[9] +
+//        (dt / 2.0) * (azStar + particle->get_properties()[12]);
+
+//      // Position integration:
+//      particle->get_properties()[4] =
+//        particle->get_properties()[4] +
+//        (dt / 2.0) * (vxStar + particle->get_properties()[7]);
+//      particle->get_properties()[5] =
+//        particle->get_properties()[5] +
+//        (dt / 2.0) * (vyStar + particle->get_properties()[8]);
+//      particle->get_properties()[6] =
+//        particle->get_properties()[6] +
+//        (dt / 2.0) * (vzStar + particle->get_properties()[9]);
+
+//      particle->set_location({particle->get_properties()[4],
+//                              particle->get_properties()[5],
+//                              particle->get_properties()[6]});
+
+//      // Angular velocity using Euler:
+//      /*
+//      particle->get_properties()[16] = particle->get_properties()[16] +
+//        (particle->get_properties()[21]) / (particle->get_properties()[20]);
+//      particle->get_properties()[17] =particle->get_properties()[17] +
+//        (particle->get_properties()[22]) / (particle->get_properties()[20]);
+//      particle->get_properties()[18] = particle->get_properties()[18] +
+//        (particle->get_properties()[23]) / (particle->get_properties()[20]);
+//        */
+//    }
+//}
 
 template class Integrator<3, 3>;
