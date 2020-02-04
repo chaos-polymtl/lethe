adeb52ed585633dc94d8476ca12189be292862be
shahab.golshan14@gmail.com <shahab.golshan14@gmail.com>
2020-02-04 16:46:43 -0500

    Modified insertion class

--- tests/dem/insertion_uniform.cc
+++ tests/dem/insertion_uniform.cc
@@ -13,7 +13,7 @@
 #include <vector>
 
 #include "../tests.h"
-#include "dem/particle_insertion.h"
+#include "dem/uniform_insertion.h"
 #include "dem/particle_wall_contact_detection.h"
 
 using namespace dealii;
@@ -37,33 +37,27 @@ test()
   float              y_max        = 0.05;
   float              z_max        = 0.05;
   double             dp           = 0.005;
-  int                nInsert      = 10;
+  int                inserted_number_at_step      = 10;
   int                rhop         = 2500;
+  int distance_threshold = 2;
   Point<dim>         g            = {0, 0, -9.81};
 
   Particles::ParticleHandler<dim, dim> particle_handler(tr,
                                                         mapping,
                                                         n_properties);
 
-  int                         nPart = 0;
-  Particles::PropertyPool     pool(n_properties);
+  int                         active_particle_number = 0;
+  Particles::PropertyPool     property_pool(n_properties);
   Particles::Particle<3>      particle;
-  ParticleInsertion<dim, dim> ins1(
-    x_min, y_min, z_min, x_max, y_max, z_max, dp, nInsert);
-  ins1.uniformInsertion(particle_handler,
-                        tr,
-                        nPart,
-                        pool,
-                        x_min,
-                        y_min,
-                        z_min,
-                        x_max,
-                        y_max,
-                        z_max,
-                        dp,
-                        nInsert,
-                        rhop,
-                        g);
+
+  UniformInsertion<dim, dim> ins1(
+    x_min, y_min, z_min, x_max, y_max, z_max, dp,
+    inserted_number_at_step, distance_threshold);
+
+ins1.insert(particle_handler, tr, active_particle_number, property_pool,
+            x_min, y_min, z_min, x_max, y_max, z_max, dp,
+            inserted_number_at_step, rhop, g, distance_threshold);
+
 
   int i = 1;
   for (auto particle = particle_handler.begin();
