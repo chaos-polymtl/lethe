95a8d00f6678bb784e0606ea4311907920442727
shahab.golshan14@gmail.com <shahab.golshan14@gmail.com>
2020-02-16 16:12:19 -0500

    Non-linear particle-wall contact force is defined in a separate class, class particle_wall_contact_force is removed

---
+++ include/dem/pw_nonlinear_force.h
@@ -0,0 +1,67 @@
+/* ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2019 - 2019 by the Lethe authors
+ *
+ * This file is part of the Lethe library
+ *
+ * The Lethe library is free software; you can use it, redistribute
+ * it, and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * The full text of the license can be found in the file LICENSE at
+ * the top level of the Lethe distribution.
+ *
+ * ---------------------------------------------------------------------
+
+ *
+ * Author: Shahab Golshan, Polytechnique Montreal, 2019
+ */
+
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+#include <deal.II/particles/particle.h>
+
+#include "dem/dem_solver_parameters.h"
+#include "dem/physical_info_struct.h"
+#include "dem/pw_contact_force.h"
+#include "dem/pw_contact_info_struct.h"
+#include <dem/dem_properties.h>
+
+using namespace dealii;
+
+#ifndef PWNONLINEARFORCE_H_
+#define PWNONLINEARFORCE_H_
+
+/**
+ * Calculation of the non-linear particle-wall contact force using the
+ * information obtained from the fine search and physical properties of
+ * particles and walls
+ *
+ * @note
+ *
+ * @author Shahab Golshan, Bruno Blais, Polytechnique Montreal 2019-
+ */
+
+template <int dim, int spacedim = dim>
+class PWNonLinearForce : public PWContactForce<dim, spacedim> {
+public:
+  PWNonLinearForce() {}
+
+  /**
+   * Carries out the calculation of the particle-wall contact force using
+   * non-linear (Hertzian) model
+   *
+   * @param pw_pairs_in_contact Required information for calculation of the
+   * particle-wall contact force, these information were obtained in the
+   * fine search
+   * @param physical_properties Physical properties of particles and walls
+   */
+  virtual void calculate_pw_contact_force(
+      std::vector<std::map<int, pw_contact_info_struct<dim, spacedim>>>
+          &pw_pairs_in_contact,
+      const physical_info_struct<dim> &physical_properties) override;
+};
+
+#endif
