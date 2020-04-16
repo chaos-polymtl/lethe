e3b2c076acd0214f4597664f3c727550cc851be3
shahabgol <shahab.golshan14@gmail.com>
2020-04-16 13:57:23 -0400

    cleared tests, fixed torques and cross products for 2d (#27)

--- tests/dem/insertion_uniform.cc
+++ tests/dem/insertion_uniform.cc
@@ -1,70 +1,87 @@
+/* ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2019 - 2019 by the Lethe authors
+ *
+ * This file is part of the Lethe library
+ *
+ * The Lethe library is free software; you can use it, redistribute
+ * it, and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * The full text of the license can be found in the file LICENSE at
+ * the top level of the Lethe distribution.
+ *
+ * ---------------------------------------------------------------------
+
+ *
+ * Author: Shahab Golshan, Polytechnique Montreal, 2019-
+ */
+
+// Inserting one particle using uniform insertion class
+
 #include <deal.II/base/parameter_handler.h>
 #include <deal.II/base/point.h>
-
 #include <deal.II/fe/mapping_q.h>
-
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/grid_tools.h>
 #include <deal.II/grid/tria.h>
-
 #include <deal.II/particles/particle.h>
+
+#include "../tests.h"
+#include <dem/dem_solver_parameters.h>
+#include <dem/uniform_insertion.h>
 
 #include <iostream>
 #include <vector>
 
-#include "../tests.h"
-#include "dem/dem_solver_parameters.h"
-#include "dem/uniform_insertion.h"
-
 using namespace dealii;
 
-template <int dim>
-void
-test()
-{
+template <int dim> void test() {
+  // Creating the mesh and refinement
   parallel::distributed::Triangulation<dim> tr(MPI_COMM_WORLD);
-  GridGenerator::hyper_cube(tr, -1, 1, true);
-  int numRef = 2;
-  tr.refine_global(numRef);
-  int                      cellNum = tr.n_active_cells();
-  MappingQ<dim>            mapping(1);
+  int hyper_cube_length = 1;
+  GridGenerator::hyper_cube(tr, -1 * hyper_cube_length, hyper_cube_length,
+                            true);
+  int refinement_number = 2;
+  tr.refine_global(refinement_number);
+
+  MappingQ<dim> mapping(1);
   DEMSolverParameters<dim> dem_parameters;
 
-  const unsigned int n_properties                 = 21;
-  dem_parameters.insertionInfo.x_min              = -0.05;
-  dem_parameters.insertionInfo.y_min              = -0.05;
-  dem_parameters.insertionInfo.z_min              = -0.05;
-  dem_parameters.insertionInfo.x_max              = 0.05;
-  dem_parameters.insertionInfo.y_max              = 0.05;
-  dem_parameters.insertionInfo.z_max              = 0.05;
+  // Defining simulation general parameters
+  const unsigned int n_properties = 21;
+  dem_parameters.insertionInfo.x_min = -0.05;
+  dem_parameters.insertionInfo.y_min = -0.05;
+  dem_parameters.insertionInfo.z_min = -0.05;
+  dem_parameters.insertionInfo.x_max = 0.05;
+  dem_parameters.insertionInfo.y_max = 0.05;
+  dem_parameters.insertionInfo.z_max = 0.05;
   dem_parameters.insertionInfo.inserted_this_step = 10;
   dem_parameters.insertionInfo.distance_threshold = 2;
-  dem_parameters.physicalProperties.diameter      = 0.005;
-  dem_parameters.physicalProperties.density       = 2500;
+  dem_parameters.physicalProperties.diameter = 0.005;
+  dem_parameters.physicalProperties.density = 2500;
 
+  // Defining particle handler
   Particles::ParticleHandler<dim> particle_handler(tr, mapping, n_properties);
 
   Particles::PropertyPool property_pool(n_properties);
-  Particles::Particle<3>  particle;
 
-  UniformInsertion<dim> ins1(dem_parameters);
-  ins1.insert(particle_handler, tr, property_pool, dem_parameters);
+  // Calling uniform insertion
+  UniformInsertion<dim> insertion_object(dem_parameters);
+  insertion_object.insert(particle_handler, tr, property_pool, dem_parameters);
 
-  int i = 1;
+  // Output
+  int particle_number = 1;
   for (auto particle = particle_handler.begin();
-       particle != particle_handler.end();
-       ++particle, ++i)
-    {
-      deallog << "Particle " << i
-              << " is inserted at: " << particle->get_location()[0] << " "
-              << particle->get_location()[1] << " "
-              << particle->get_location()[2] << " " << std::endl;
-    }
+       particle != particle_handler.end(); ++particle, ++particle_number) {
+    deallog << "Particle " << particle_number
+            << " is inserted at: " << particle->get_location()[0] << " "
+            << particle->get_location()[1] << " " << particle->get_location()[2]
+            << " " << std::endl;
+  }
 }
 
-int
-main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
 
   initlog();
--- tests/dem/particle_particle_fine_search.cc
+++ tests/dem/particle_particle_fine_search.cc
@@ -1,76 +1,96 @@
+/* ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2019 - 2019 by the Lethe authors
+ *
+ * This file is part of the Lethe library
+ *
+ * The Lethe library is free software; you can use it, redistribute
+ * it, and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * The full text of the license can be found in the file LICENSE at
+ * the top level of the Lethe distribution.
+ *
+ * ---------------------------------------------------------------------
+
+ *
+ * Author: Shahab Golshan, Polytechnique Montreal, 2019-
+ */
+
+// In this test, the performance of particle-particle fine search class is
+// evaluated
+
 #include <deal.II/base/parameter_handler.h>
 #include <deal.II/base/point.h>
-
 #include <deal.II/fe/mapping_q.h>
-
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/grid_tools.h>
 #include <deal.II/grid/tria.h>
-
 #include <deal.II/particles/particle.h>
 #include <deal.II/particles/particle_handler.h>
 #include <deal.II/particles/particle_iterator.h>
 
+#include "../tests.h"
+#include <dem/dem_solver_parameters.h>
+#include <dem/find_cell_neighbors.h>
+#include <dem/pp_broad_search.h>
+#include <dem/pp_contact_info_struct.h>
+#include <dem/pp_fine_search.h>
+
 #include <iostream>
 #include <vector>
 
-#include "../tests.h"
-#include "dem/dem_solver_parameters.h"
-#include "dem/find_cell_neighbors.h"
-#include "dem/pp_broad_search.h"
-#include "dem/pp_contact_info_struct.h"
-#include "dem/pp_fine_search.h"
-
 using namespace dealii;
 
-template <int dim>
-void
-test()
-{
+template <int dim> void test() {
+  // Creating the mesh and refinement
   parallel::distributed::Triangulation<dim> triangulation(MPI_COMM_WORLD);
-  GridGenerator::hyper_cube(triangulation, -1, 1, true);
-  int numRef = 2;
-  triangulation.refine_global(numRef);
-  int           cellNum = triangulation.n_active_cells();
+  int hyper_cube_length = 1;
+  GridGenerator::hyper_cube(triangulation, -1 * hyper_cube_length,
+                            hyper_cube_length, true);
+  int refinement_number = 2;
+  triangulation.refine_global(refinement_number);
   MappingQ<dim> mapping(1);
 
+  // Defining general simulation parameters
   const unsigned int n_properties = 21;
-
-  Particles::ParticleHandler<dim> particle_handler(triangulation,
-                                                   mapping,
+  Particles::ParticleHandler<dim> particle_handler(triangulation, mapping,
                                                    n_properties);
 
+  // Finding cell neighbors
   std::vector<std::set<typename Triangulation<dim>::active_cell_iterator>>
-    cellNeighbor;
+      cell_neighbors_list;
 
   FindCellNeighbors<dim> cn1;
-  cellNeighbor = cn1.find_cell_neighbors(triangulation);
+  cell_neighbors_list = cn1.find_cell_neighbors(triangulation);
 
-  PPBroadSearch<dim> ppbs;
-  PPFineSearch<dim>  ppfs;
+  // Creating broad and fine particle-particle search objects
+  PPBroadSearch<dim> broad_search_object;
+  PPFineSearch<dim> fine_search_obejct;
 
+  // Inserting two particles in contact
   Point<3> position1 = {0.4, 0, 0};
-  int      id1       = 0;
+  int id1 = 0;
   Point<3> position2 = {0.40499, 0, 0};
-  int      id2       = 1;
-  float    dt        = 0.00001;
+  int id2 = 1;
+  float dt = 0.00001;
 
   Particles::Particle<dim> particle1(position1, position1, id1);
   typename Triangulation<dim>::active_cell_iterator cell1 =
-    GridTools::find_active_cell_around_point(triangulation,
-                                             particle1.get_location());
+      GridTools::find_active_cell_around_point(triangulation,
+                                               particle1.get_location());
   Particles::ParticleIterator<dim> pit1 =
-    particle_handler.insert_particle(particle1, cell1);
-  pit1->get_properties()[0]  = id1;
-  pit1->get_properties()[1]  = 1;
-  pit1->get_properties()[2]  = 0.005;
-  pit1->get_properties()[3]  = 2500;
-  pit1->get_properties()[4]  = 0;
-  pit1->get_properties()[5]  = 0;
-  pit1->get_properties()[6]  = 0;
-  pit1->get_properties()[7]  = 0;
-  pit1->get_properties()[8]  = 0;
-  pit1->get_properties()[9]  = 0;
+      particle_handler.insert_particle(particle1, cell1);
+  pit1->get_properties()[0] = id1;
+  pit1->get_properties()[1] = 1;
+  pit1->get_properties()[2] = 0.005;
+  pit1->get_properties()[3] = 2500;
+  pit1->get_properties()[4] = 0;
+  pit1->get_properties()[5] = 0;
+  pit1->get_properties()[6] = 0;
+  pit1->get_properties()[7] = 0;
+  pit1->get_properties()[8] = 0;
+  pit1->get_properties()[9] = 0;
   pit1->get_properties()[10] = 0;
   pit1->get_properties()[11] = 0;
   pit1->get_properties()[12] = 0;
@@ -82,20 +102,20 @@ test()
 
   Particles::Particle<dim> particle2(position2, position2, id2);
   typename Triangulation<dim>::active_cell_iterator cell2 =
-    GridTools::find_active_cell_around_point(triangulation,
-                                             particle2.get_location());
+      GridTools::find_active_cell_around_point(triangulation,
+                                               particle2.get_location());
   Particles::ParticleIterator<dim> pit2 =
-    particle_handler.insert_particle(particle2, cell2);
-  pit2->get_properties()[0]  = id2;
-  pit2->get_properties()[1]  = 1;
-  pit2->get_properties()[2]  = 0.005;
-  pit2->get_properties()[3]  = 2500;
-  pit2->get_properties()[4]  = 0;
-  pit2->get_properties()[5]  = 0;
-  pit2->get_properties()[6]  = 0;
-  pit2->get_properties()[7]  = 0;
-  pit2->get_properties()[8]  = 0;
-  pit2->get_properties()[9]  = 0;
+      particle_handler.insert_particle(particle2, cell2);
+  pit2->get_properties()[0] = id2;
+  pit2->get_properties()[1] = 1;
+  pit2->get_properties()[2] = 0.005;
+  pit2->get_properties()[3] = 2500;
+  pit2->get_properties()[4] = 0;
+  pit2->get_properties()[5] = 0;
+  pit2->get_properties()[6] = 0;
+  pit2->get_properties()[7] = 0;
+  pit2->get_properties()[8] = 0;
+  pit2->get_properties()[9] = 0;
   pit2->get_properties()[10] = 0;
   pit2->get_properties()[11] = 0;
   pit2->get_properties()[12] = 0;
@@ -105,48 +125,52 @@ test()
   pit2->get_properties()[16] = 1;
   pit2->get_properties()[17] = 1;
 
-  std::map<int,
-           std::pair<Particles::ParticleIterator<dim>,
-                     Particles::ParticleIterator<dim>>>
-    pairs;
-  ppbs.find_PP_Contact_Pairs(particle_handler, cellNeighbor, pairs);
+  // Calling broad search
+  std::map<int, std::pair<Particles::ParticleIterator<dim>,
+                          Particles::ParticleIterator<dim>>>
+      pairs;
+  broad_search_object.find_PP_Contact_Pairs(particle_handler,
+                                            cell_neighbors_list, pairs);
 
-  std::map<int, std::map<int, pp_contact_info_struct<dim>>> inContactInfo;
+  // Calling fine search
+  std::map<int, std::map<int, pp_contact_info_struct<dim>>>
+      contact_pairs_information;
 
-  ppfs.pp_Fine_Search(pairs, inContactInfo, dt);
+  fine_search_obejct.pp_Fine_Search(pairs, contact_pairs_information, dt);
 
-  for (auto in_contact_info_iterator = inContactInfo.begin();
-       in_contact_info_iterator != inContactInfo.end();
-       ++in_contact_info_iterator)
-    {
-      auto info_it = (&in_contact_info_iterator->second)->begin();
-      while (info_it != (&in_contact_info_iterator->second)->end())
-        {
-          deallog << "The normal overlap of contacting paritlce is: "
-                  << info_it->second.normal_overlap << std::endl;
-          deallog << "The normal vector of collision is: "
-                  << info_it->second.normal_vector[0] << " "
-                  << info_it->second.normal_vector[1] << " "
-                  << info_it->second.normal_vector[2] << std::endl;
-          deallog << "Normal relative velocity at contact point is: "
-                  << info_it->second.normal_relative_velocity << std::endl;
-          deallog << "The tangential overlap of contacting paritlce is: "
-                  << info_it->second.tangential_overlap[0] << " "
-                  << info_it->second.tangential_overlap[1] << " "
-                  << info_it->second.tangential_overlap[2] << std::endl;
-          deallog << "Tangential relative velocity at contact point is: "
-                  << info_it->second.tangential_relative_velocity[0] << " "
-                  << info_it->second.tangential_relative_velocity[1] << " "
-                  << info_it->second.tangential_relative_velocity[2]
-                  << std::endl;
-          info_it++;
-        }
+  // Output
+  for (auto in_contact_info_iterator = contact_pairs_information.begin();
+       in_contact_info_iterator != contact_pairs_information.end();
+       ++in_contact_info_iterator) {
+    auto information_iterator = (&in_contact_info_iterator->second)->begin();
+    while (information_iterator != (&in_contact_info_iterator->second)->end()) {
+      deallog << "The normal overlap of contacting paritlce is: "
+              << information_iterator->second.normal_overlap << std::endl;
+      deallog << "The normal vector of collision is: "
+              << information_iterator->second.normal_vector[0] << " "
+              << information_iterator->second.normal_vector[1] << " "
+              << information_iterator->second.normal_vector[2] << std::endl;
+      deallog << "Normal relative velocity at contact point is: "
+              << information_iterator->second.normal_relative_velocity
+              << std::endl;
+      deallog << "The tangential overlap of contacting paritlce is: "
+              << information_iterator->second.tangential_overlap[0] << " "
+              << information_iterator->second.tangential_overlap[1] << " "
+              << information_iterator->second.tangential_overlap[2]
+              << std::endl;
+      deallog << "Tangential relative velocity at contact point is: "
+              << information_iterator->second.tangential_relative_velocity[0]
+              << " "
+              << information_iterator->second.tangential_relative_velocity[1]
+              << " "
+              << information_iterator->second.tangential_relative_velocity[2]
+              << std::endl;
+      ++information_iterator;
     }
+  }
 }
 
-int
-main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   initlog();
   Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
   test<3>();
