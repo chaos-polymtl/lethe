6eb35f582345ee2846a311a81aeb0913eb20976a
Lucka Barbeau <luckabarbeau@gmail.com>
2020-06-01 17:48:59 -0400

    add solvers

---
+++ applications/gls_sharp_navier_stokes_3d/gls_sharp_navier_stokes_3d.cc
@@ -0,0 +1,73 @@
+/* ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2019 - by the Lethe authors
+ *
+ * This file is part of the Lethe library
+ *
+ * The Lethe library is free software; you can use it, redistribute
+ * it, and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either
+ * version 3.1 of the License, or (at your option) any later version.
+ * The full text of the license can be found in the file LICENSE at
+ * the top level of the Lethe distribution.
+ *
+ * ---------------------------------------------------------------------
+
+*
+* Author: Bruno Blais, Polytechnique Montreal, 2019-
+*/
+
+#include "solvers/gls_navier_stokes.h"
+
+int
+main(int argc, char *argv[])
+{
+  try
+    {
+      if (argc != 2)
+        {
+          std::cout << "Usage:" << argv[0] << " input_file" << std::endl;
+          std::exit(1);
+        }
+      Utilities::MPI::MPI_InitFinalize mpi_initialization(
+        argc, argv, numbers::invalid_unsigned_int);
+
+      ParameterHandler                prm;
+      NavierStokesSolverParameters<3> NSparam;
+      NSparam.declare(prm);
+      // Parsing of the file
+      prm.parse_input(argv[1]);
+      NSparam.parse(prm);
+
+      GLSNavierStokesSolver<3> problem_3d(NSparam,
+                                          NSparam.fem_parameters.velocityOrder,
+                                          NSparam.fem_parameters.pressureOrder);
+      problem_3d.solve();
+    }
+  catch (std::exception &exc)
+    {
+      std::cerr << std::endl
+                << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Exception on processing: " << std::endl
+                << exc.what() << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
+    }
+  catch (...)
+    {
+      std::cerr << std::endl
+                << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Unknown exception!" << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
+    }
+  return 0;
+}
