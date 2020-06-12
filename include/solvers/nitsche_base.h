5cbec3444f4aa7e227660ab22a37b012f59cdfef
CaroleAnneDaunais <carole.anne.daunais@hotmail.com>
2020-06-12 11:15:56 -0400

    dde SolidBase.h

---
+++ include/solvers/nitsche_base.h
@@ -0,0 +1,160 @@
+/* ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2019 - 2019 by the Lethe authors
+ *
+ * This file is part of the Lethe library
+ *
+ * The Lethe library is free software; you can use it, redistribute
+ * it, and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * The full text of the license can be found in the file LICENSE at
+ * the top level of the Lethe distribution.
+ *
+ * ---------------------------------------------------------------------
+
+ *
+ * Authors: Carole-Anne Daunais, Valérie Bibeau, Polytechnique Montreal, 2020
+ */
+
+#ifndef lethe_solid_base_h
+#define lethe_solid_base_h
+
+// Dealii Includes
+
+// Base
+#include <deal.II/base/conditional_ostream.h>
+#include <deal.II/base/convergence_table.h>
+#include <deal.II/base/function.h>
+#include <deal.II/base/index_set.h>
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/table_handler.h>
+#include <deal.II/base/timer.h>
+#include <deal.II/base/utilities.h>
+
+// Lac
+#include <deal.II/lac/affine_constraints.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/precondition_block.h>
+#include <deal.II/lac/solver_bicgstab.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/solver_gmres.h>
+#include <deal.II/lac/sparse_direct.h>
+#include <deal.II/lac/sparse_ilu.h>
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/sparsity_tools.h>
+#include <deal.II/lac/vector.h>
+
+// Lac - Trilinos includes
+#include <deal.II/lac/trilinos_parallel_block_vector.h>
+#include <deal.II/lac/trilinos_precondition.h>
+#include <deal.II/lac/trilinos_solver.h>
+#include <deal.II/lac/trilinos_sparse_matrix.h>
+#include <deal.II/lac/trilinos_vector.h>
+
+// Grid
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_in.h>
+#include <deal.II/grid/grid_out.h>
+#include <deal.II/grid/grid_refinement.h>
+#include <deal.II/grid/grid_tools.h>
+#include <deal.II/grid/manifold_lib.h>
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/tria_accessor.h>
+#include <deal.II/grid/tria_iterator.h>
+
+// Dofs
+#include <deal.II/dofs/dof_accessor.h>
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_renumbering.h>
+#include <deal.II/dofs/dof_tools.h>
+
+// Fe
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/mapping_q.h>
+
+// Numerics
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/error_estimator.h>
+#include <deal.II/numerics/matrix_tools.h>
+#include <deal.II/numerics/solution_transfer.h>
+#include <deal.II/numerics/vector_tools.h>
+
+// Distributed
+#include <deal.II/distributed/grid_refinement.h>
+#include <deal.II/distributed/solution_transfer.h>
+
+//Particles
+#include <deal.II/particles/data_out.h>
+
+
+// Lethe Includes
+#include <core/bdf.h>
+#include <core/boundary_conditions.h>
+#include <core/manifolds.h>
+#include <core/parameters.h>
+#include <core/pvd_handler.h>
+
+#include "navier_stokes_solver_parameters.h"
+
+// Std
+#include <fstream>
+#include <iostream>
+
+using namespace dealii;
+
+/**
+ * A base class that stores the methods related
+ *
+ * @tparam dim An integer that denotes the dimension of the space in which
+ * the flow is solved
+ *
+ * @tparam VectorType  The Vector type used for the solvers
+ *
+ * @tparam DofsType the type of dof storage indices
+ *
+ * @ingroup solvers
+ * @author Bruno Blais, 2019
+ */
+
+template <int dim, int spacedim = dim>
+class SolidBase 
+{
+public:
+
+  //Member functions
+  SolidBase(NavierStokesSolverParameters<dim> &nsparam);
+  ~SolidBase();
+  void initial_setup();
+  void setup_particles();
+  void output_particles(const Particles::ParticleHandler<spacedim> &particles,
+                          std::string                               fprefix,
+                          const unsigned int                        iter,
+                          const double                              time) const;
+  Particles::ParticleHandler<spacedim>  get_solid_particle_handler();
+
+private:
+
+  // Member variables
+  DofsType locally_owned_dofs;
+  DofsType locally_relevant_dofs;
+
+  MPI_Comm           mpi_communicator;
+  const unsigned int n_mpi_processes;
+  const unsigned int this_mpi_process;
+
+  std::shared_ptr<parallel::DistributedTriangulationBase<dim,spacedim>>  solid_tria;
+  DoFHandler<dim, spacedim>                                              solid_dh;
+  FESystem<dim, spacedim>                                                solid_fe;
+  std::unique_ptr<Quadrature<dim>>                                       solid_quadrature_formula;
+  Particles::ParticleHandler<spacedim>                                   solid_particle_handler;
+  
+  TimerOutput computing_timer;
+
+  NavierStokesSolverParameters<dim> nsparam;
+};
+
+#endif
