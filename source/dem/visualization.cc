546a9811e2fb8695b15a41bd1fce698b75d4848a
shahab.golshan14@gmail.com <shahab.golshan14@gmail.com>
2020-09-07 19:10:17 -0400

    precision_6

--- source/dem/visualization.cc
+++ source/dem/visualization.cc
@@ -82,7 +82,7 @@ Visualization<dim>::print_xyz(
   std::vector<std::pair<std::string, int>>       properties)
 {
   std::vector<int> precision = {
-    0, 0, 3, 3, 3, 3, 3, 1, 1, 1, 2, 2, 2, 1, 1, 1, 3, 3, 3, 3, 3,
+    0, 0, 5, 5, 3, 3, 3, 1, 1, 1, 2, 2, 2, 1, 1, 1, 5, 3, 3, 3, 3,
   };
   if (Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0)
     std::cout
