a93ed24e5ab56845213a504e519668124f2129b4
tonielgeitani <tonielgeitani@gmail.com>
2020-09-15 16:43:51 -0400

    added the cfd-dem parameter files

---
+++ include/core/parameters_cfd_dem.h
@@ -0,0 +1,70 @@
+/* ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2019 - 2020 by the Lethe authors
+ *
+ * This file is part of the Lethe library
+ *
+ * The Lethe library is free software; you can use it, redistribute
+ * it, and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * The full text of the license can be found in the file LICENSE at
+ * the top level of the Lethe distribution.
+ *
+ * ---------------------------------------------------------------------
+
+ *
+ * Author: Toni EL Geitani, Bruno Blais, Polytechnique Montreal, 2020-
+ */
+
+#ifndef lethe_parameters_cfd_dem_h
+#define lethe_parameters_cfd_dem_h
+
+#include <deal.II/base/function.h>
+#include <deal.II/base/parsed_function.h>
+
+#include <core/parameters.h>
+
+using namespace dealii;
+/**
+ * The analytical solution class provides an interface for all common
+ * dlement required for the calculation of analytical solution
+ * All equation-specific analytical solution should derive
+ * from the base class but also call it's declare_parameters and
+ *parse_parameters routine. This allows specialize class to focus on their
+ *specificity and forget about other non-specific elements that are generic to
+ *the calculation of analytical solutions
+ **/
+
+namespace Parameters
+{
+  enum class VoidFractionMode
+  {
+    function
+  };
+
+
+  template <int dim>
+  class VoidFraction
+  {
+  public:
+    VoidFraction()
+      : void_fraction(1)
+
+    {}
+
+    virtual void
+    declare_parameters(ParameterHandler &prm);
+    virtual void
+    parse_parameters(ParameterHandler &prm);
+
+
+  public:
+    VoidFractionMode               mode;
+    Functions::ParsedFunction<dim> void_fraction;
+  };
+
+
+} // namespace Parameters
+
+#endif
