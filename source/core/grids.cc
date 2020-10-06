85ac74e5fb779c96c9ddee391b6df8dcd19529f8
acdaigneault <71884806+acdaigneault@users.noreply.github.com>
2020-10-06 13:59:52 -0400

    Periodic hills mesh (#72)
    Periodic hills meshing using deal.II tools. Manifolds are attached to the face and the mesh supports
    full high-order interpolation.
    
    
    Co-authored-by: Audrey Collard-Daigneault <audrey.collard-daigneault@polymtl.ca>
    Co-authored-by: Bruno Blais <blais.bruno@gmail.com>

--- source/core/grids.cc
+++ source/core/grids.cc
@@ -5,6 +5,9 @@
 #include "core/boundary_conditions.h"
 #include "core/grids.h"
 
+// Per_Hills needed includes
+#include "core/per_hills_grid.h"
+
 // Std
 #include <fstream>
 #include <iostream>
@@ -34,6 +37,13 @@ attach_grid_to_triangulation(
         *triangulation,
         mesh_parameters.grid_type,
         mesh_parameters.grid_arguments);
+    }
+
+  // Periodic Hills grid
+  else if (mesh_parameters.type == Parameters::Mesh::Type::per_hills)
+    {
+      PeriodicHillsGrid<dim, spacedim> grid;
+      grid.make_grid(*triangulation);
     }
   else
     throw std::runtime_error(
