21e2f6c9ac592198e0e9edbca4e4e62a8b9e7d15
acdaigneault <acdaigneault@gmail.com>
2020-10-29 15:29:57 -0400

    First attempt to generate vectors of average velocities Added flow_control template with Trilinos vectors

--- source/solvers/postprocessing_velocities.cc
+++ source/solvers/postprocessing_velocities.cc
@@ -1,46 +1,49 @@
 #include <solvers/postprocessing_velocities.h>
 
-
-template std::vector<Tensor<1, 2>>
+template void
 PostprocessingVelocities<2, TrilinosWrappers::MPI::Vector>::
   calculate_velocity_fluctuations(
     const DoFHandler<2> &                dof_handler,
     const TrilinosWrappers::MPI::Vector &evaluation_point,
     const Parameters::SimulationControl &simulation_control,
     const Parameters::FEM &              fem_parameters,
-    const double &                       step_number,
+    const Parameters::PostProcessing &   post_processing,
+    const double &                       current_time,
     const double &                       bulk_velocity,
     const MPI_Comm &                     mpi_communicator);
 
-template std::vector<Tensor<1, 3>>
+template void
 PostprocessingVelocities<3, TrilinosWrappers::MPI::Vector>::
   calculate_velocity_fluctuations(
     const DoFHandler<3> &                dof_handler,
     const TrilinosWrappers::MPI::Vector &evaluation_point,
     const Parameters::SimulationControl &simulation_control,
     const Parameters::FEM &              fem_parameters,
-    const double &                       step_number,
+    const Parameters::PostProcessing &   post_processing,
+    const double &                       current_time,
     const double &                       bulk_velocity,
     const MPI_Comm &                     mpi_communicator);
 
-template std::vector<Tensor<1, 2>>
+template void
 PostprocessingVelocities<2, TrilinosWrappers::MPI::BlockVector>::
   calculate_velocity_fluctuations(
     const DoFHandler<2> &                     dof_handler,
     const TrilinosWrappers::MPI::BlockVector &evaluation_point,
     const Parameters::SimulationControl &     simulation_control,
     const Parameters::FEM &                   fem_parameters,
-    const double &                            step_number,
+    const Parameters::PostProcessing &        post_processing,
+    const double &                            current_time,
     const double &                            bulk_velocity,
     const MPI_Comm &                          mpi_communicator);
 
-template std::vector<Tensor<1, 3>>
+template void
 PostprocessingVelocities<3, TrilinosWrappers::MPI::BlockVector>::
   calculate_velocity_fluctuations(
     const DoFHandler<3> &                     dof_handler,
     const TrilinosWrappers::MPI::BlockVector &evaluation_point,
     const Parameters::SimulationControl &     simulation_control,
     const Parameters::FEM &                   fem_parameters,
-    const double &                            step_number,
+    const Parameters::PostProcessing &        post_processing,
+    const double &                            current_time,
     const double &                            bulk_velocity,
     const MPI_Comm &                          mpi_communicator);
