0c0fcb06b4dceb45e82a0c15a6b208cc3a71d7c1
acdaigneault <acdaigneault@gmail.com>
2020-10-30 08:59:01 -0400

    Added AeverageVelocities class * Parallel partition not working yet

--- include/solvers/postprocessing_velocities.h
+++ include/solvers/postprocessing_velocities.h
@@ -108,186 +108,84 @@
 
 using namespace dealii;
 
-
-template <int dim, typename VectorType>
-class PostprocessingVelocities
+template <int dim, typename VectorType, typename DofsType>
+class AverageVelocities
 {
 public:
-  void
-  calculate_velocity_fluctuations(
-    const DoFHandler<dim> &              dof_handler,
-    const VectorType &                   evaluation_point,
+  VectorType
+  calculate_average_velocities(
+    const VectorType &                   present_solution,
     const Parameters::SimulationControl &simulation_control,
-    const Parameters::FEM &              fem_parameters,
     const Parameters::PostProcessing &   post_processing,
     const double &                       current_time,
-    const double &                       bulk_velocity,
+    const DofsType &                     locally_owned_dofs,
+    const DofsType &                     locally_relevant_dofs,
     const MPI_Comm &                     mpi_communicator);
-  std::vector<std::vector<double>>
-  average_velocities();
-  std::vector<std::vector<double>>
-  velocity_fluctuations();
-  void
-  calculate_velocity_profiles(const double &bulk_velocity);
-
+  VectorType
+  nondimensionalize_average_velocities(const double bulk_velocity);
 
 private:
-  std::vector<Tensor<1, dim>> u_dt;
-  // std::vector<Tensor<1, dim>> velocity_fluctuations;
-  // std::vector<Tensor<1, dim>> average_velocities;
-  std::vector<double>         velocity_fluctuations_u;
-  std::vector<double>         velocity_fluctuations_v;
-  std::vector<double>         velocity_fluctuations_w;
-  std::vector<double>         average_velocities_u;
-  std::vector<double>         average_velocities_v;
-  std::vector<double>         average_velocities_w;
-  std::vector<Tensor<1, dim>> velocity_profiles;
-  std::vector<Tensor<1, dim>> reynolds_stress;
-  std::vector<Tensor<1, dim>> shear_stress;
+  VectorType sum_velocity_dt;
+  VectorType average_velocities;
+  VectorType nondimensionalized_average_velocities;
 };
 
-template <int dim, typename VectorType>
-void
-PostprocessingVelocities<dim, VectorType>::calculate_velocity_fluctuations(
-  const DoFHandler<dim> &              dof_handler,
-  const VectorType &                   evaluation_point,
+template <int dim, typename VectorType, typename DofsType>
+VectorType
+AverageVelocities<dim, VectorType, DofsType>::calculate_average_velocities(
+  const VectorType &                   present_solution,
   const Parameters::SimulationControl &simulation_control,
-  const Parameters::FEM &              fem_parameters,
   const Parameters::PostProcessing &   post_processing,
   const double &                       current_time,
-  const double &                       bulk_velocity,
+  const DofsType &                     locally_owned_dofs,
+  const DofsType &                     locally_relevant_dofs,
   const MPI_Comm &                     mpi_communicator)
 {
-  const FiniteElement<dim> &fe = dof_handler.get_fe();
-  const MappingQ<dim>       mapping(fe.degree, fem_parameters.qmapping_all);
-  QGauss<dim>               quadrature_formula(fe.degree + 1);
-  const unsigned int        n_q_points = quadrature_formula.size();
-  const FEValuesExtractors::Vector velocities(0);
-  std::vector<Tensor<1, dim>>      present_velocity_values(n_q_points);
-  double total_time = current_time - post_processing.initial_time;
+  const double         total_time = current_time - post_processing.initial_time;
+  const TrilinosScalar trilinos_total_time = total_time;
+  const TrilinosScalar trilinos_dt         = simulation_control.dt;
+  VectorType           velocity_dt;
+  velocity_dt.reinit(locally_owned_dofs,
+                     locally_relevant_dofs,
+                     mpi_communicator);
+  velocity_dt.equ(trilinos_dt, present_solution);
 
-  FEValues<dim> fe_values(mapping,
-                          fe,
-                          quadrature_formula,
-                          update_values | update_gradients |
-                            update_quadrature_points | update_JxW_values);
-
-  int point = 0;
-  for (const auto &cell : dof_handler.active_cell_iterators())
+  if (current_time - 0.0 < 1e-6)
     {
-      if (cell->is_locally_owned())
-        {
-          fe_values.reinit(cell);
-          fe_values[velocities].get_function_values(evaluation_point,
-                                                    present_velocity_values);
-          // std::cout << "size : " << dof_handler.end() << std::endl;
-          for (unsigned int q = 0; q < n_q_points; q++)
-            {
-              if (abs(current_time - 0.0) < 1e-6)
-                {
-                  u_dt.push_back(present_velocity_values[q] *
-                                 0); // Tensor of zeros
-                  average_velocities_u.push_back(0.);
-                  average_velocities_v.push_back(0.);
-                  velocity_fluctuations_u.push_back(0.);
-                  velocity_fluctuations_v.push_back(0.);
-                  if (dim == 3)
-                    {
-                      average_velocities_w.push_back(0.);
-                      velocity_fluctuations_w.push_back(0.);
-                    }
-                }
-              else if (total_time > 1e-6)
-                {
-                  u_dt[point] +=
-                    present_velocity_values[q] * simulation_control.dt;
-                  average_velocities_u[point] = u_dt[point][0] / total_time;
-                  average_velocities_v[point] = u_dt[point][1] / total_time;
-                  velocity_fluctuations_u[point] =
-                    present_velocity_values[q][0] - average_velocities_u[point];
-                  velocity_fluctuations_v[point] =
-                    present_velocity_values[q][1] - average_velocities_v[point];
-                  if (dim == 3)
-                    {
-                      average_velocities_w[point] = u_dt[point][2] / total_time;
-                      velocity_fluctuations_w[point] =
-                        present_velocity_values[q][2] -
-                        average_velocities_w[point];
-                    }
-                }
-
-              /*
-              u_dt                    = Utilities::MPI::sum(u_dt,
-              mpi_communicator); average_velocities_u    =
-              Utilities::MPI::sum(average_velocities_u, mpi_communicator);
-              average_velocities_v    =
-              Utilities::MPI::sum(average_velocities_v, mpi_communicator);
-              average_velocities_w    =
-              Utilities::MPI::sum(average_velocities_w, mpi_communicator);
-              velocity_fluctuations_u =
-              Utilities::MPI::sum(velocity_fluctuations_u, mpi_communicator);
-              velocity_fluctuations_v =
-              Utilities::MPI::sum(velocity_fluctuations_v, mpi_communicator);
-              velocity_fluctuations_w =
-              Utilities::MPI::sum(velocity_fluctuations_w, mpi_communicator);
-              */
-
-              // std::cout << "u_dt : " << u_dt[point] << std::endl;
-              std::cout << "average_velocities : "
-                        << average_velocities_u[point] << ","
-                        << average_velocities_v[point] << ","
-                        << average_velocities_w[point] << std::endl;
-              std::cout << "velocity_fluctuations : "
-                        << velocity_fluctuations_u[point] << ","
-                        << velocity_fluctuations_v[point] << ","
-                        << velocity_fluctuations_w[point] << std::endl;
-              point++;
-            }
-        }
+      std::cout << "foo0" << std::endl;
+      // Reinitilizing vectors with zeros at t = 0
+      sum_velocity_dt.reinit(present_solution);
+      average_velocities.reinit(present_solution);
+      std::cout << "foo" << std::endl;
     }
+  else if (abs(total_time) < 1e-6)
+    {
+      // Starting to sum velocity*dt because t = initial time
+      sum_velocity_dt = velocity_dt;
+      std::cout << "foo2" << std::endl;
+    }
+  else if (total_time >= 1e-6)
+    {
+      // Generating average velocities at each time step
+      sum_velocity_dt += velocity_dt;
+      average_velocities.equ(1. / trilinos_total_time, sum_velocity_dt);
+      std::cout << "foo3" << std::endl;
+    }
+  std::cout << "foo4" << std::endl;
+  return average_velocities;
 }
 
-template <int dim, typename VectorType>
-std::vector<std::vector<double>>
-PostprocessingVelocities<dim, VectorType>::average_velocities()
+template <int dim, typename VectorType, typename DofsType>
+VectorType
+AverageVelocities<dim, VectorType, DofsType>::
+  nondimensionalize_average_velocities(const double bulk_velocity)
 {
-  std::vector<std::vector<double>> av;
-  if (dim == 2)
-    {
-      av[0] = average_velocities_u;
-      av[1] = average_velocities_v;
-    }
-  else if (dim == 3)
-    {
-      av[2] = average_velocities_w;
-    }
-  return av;
+  const TrilinosScalar trilinos_bulk_velocity = bulk_velocity;
+  nondimensionalized_average_velocities       = average_velocities;
+  nondimensionalized_average_velocities.equ(trilinos_bulk_velocity,
+                                            average_velocities);
+  return nondimensionalized_average_velocities;
 }
-
-template <int dim, typename VectorType>
-std::vector<std::vector<double>>
-PostprocessingVelocities<dim, VectorType>::velocity_fluctuations()
-{
-  std::vector<std::vector<double>> vf;
-  if (dim == 2)
-    {
-      vf[0] = velocity_fluctuations_u;
-      vf[1] = average_velocities_v;
-    }
-  else if (dim == 3)
-    {
-      vf[2] = velocity_fluctuations_w;
-    }
-  return vf;
-}
-
-
-template <int dim, typename VectorType>
-void
-PostprocessingVelocities<dim, VectorType>::calculate_velocity_profiles(
-  const double &bulk_velocity)
-{}
-
 
 
 #endif
--- source/solvers/postprocessing_velocities.cc
+++ source/solvers/postprocessing_velocities.cc
@@ -1,49 +1,45 @@
 #include <solvers/postprocessing_velocities.h>
+/*
+template TrilinosWrappers::MPI::Vector
+AverageVelocities<2, TrilinosWrappers::MPI::Vector, DoFHandler>::
+  calculate_average_velocities(
+  const TrilinosWrappers::MPI::Vector &present_solution,
+  const Parameters::SimulationControl &simulation_control,
+  const Parameters::PostProcessing &   post_processing,
+  const double &                       current_time,
+  const DoFHandler &                     locally_owned_dofs,
+  const DoFHandler &                     locally_relevant_dofs,
+  const MPI_Comm &                     mpi_communicator);
 
-template void
-PostprocessingVelocities<2, TrilinosWrappers::MPI::Vector>::
-  calculate_velocity_fluctuations(
-    const DoFHandler<2> &                dof_handler,
-    const TrilinosWrappers::MPI::Vector &evaluation_point,
-    const Parameters::SimulationControl &simulation_control,
-    const Parameters::FEM &              fem_parameters,
-    const Parameters::PostProcessing &   post_processing,
-    const double &                       current_time,
-    const double &                       bulk_velocity,
-    const MPI_Comm &                     mpi_communicator);
+template TrilinosWrappers::MPI::Vector
+AverageVelocities<3, TrilinosWrappers::MPI::Vector, IndexSet>::
+  calculate_average_velocities(
+  const TrilinosWrappers::MPI::Vector &present_solution,
+  const Parameters::SimulationControl &simulation_control,
+  const Parameters::PostProcessing &   post_processing,
+  const double &                       current_time,
+  const DoFHandler &                     locally_owned_dofs,
+  const DoFHandler &                     locally_relevant_dofs,
+  const MPI_Comm &                     mpi_communicator);
 
-template void
-PostprocessingVelocities<3, TrilinosWrappers::MPI::Vector>::
-  calculate_velocity_fluctuations(
-    const DoFHandler<3> &                dof_handler,
-    const TrilinosWrappers::MPI::Vector &evaluation_point,
-    const Parameters::SimulationControl &simulation_control,
-    const Parameters::FEM &              fem_parameters,
-    const Parameters::PostProcessing &   post_processing,
-    const double &                       current_time,
-    const double &                       bulk_velocity,
-    const MPI_Comm &                     mpi_communicator);
+template TrilinosWrappers::MPI::BlockVector
+AverageVelocities<2, TrilinosWrappers::MPI::BlockVector, IndexSet>::
+  calculate_average_velocities(
+  const TrilinosWrappers::MPI::BlockVector &present_solution,
+  const Parameters::SimulationControl      &simulation_control,
+  const Parameters::PostProcessing &        post_processing,
+  const double &                            current_time,
+  const DoFHandler &                     locally_owned_dofs,
+  const DoFHandler &                     locally_relevant_dofs,
+  const MPI_Comm &                     mpi_communicator);
 
-template void
-PostprocessingVelocities<2, TrilinosWrappers::MPI::BlockVector>::
-  calculate_velocity_fluctuations(
-    const DoFHandler<2> &                     dof_handler,
-    const TrilinosWrappers::MPI::BlockVector &evaluation_point,
-    const Parameters::SimulationControl &     simulation_control,
-    const Parameters::FEM &                   fem_parameters,
-    const Parameters::PostProcessing &        post_processing,
-    const double &                            current_time,
-    const double &                            bulk_velocity,
-    const MPI_Comm &                          mpi_communicator);
-
-template void
-PostprocessingVelocities<3, TrilinosWrappers::MPI::BlockVector>::
-  calculate_velocity_fluctuations(
-    const DoFHandler<3> &                     dof_handler,
-    const TrilinosWrappers::MPI::BlockVector &evaluation_point,
-    const Parameters::SimulationControl &     simulation_control,
-    const Parameters::FEM &                   fem_parameters,
-    const Parameters::PostProcessing &        post_processing,
-    const double &                            current_time,
-    const double &                            bulk_velocity,
-    const MPI_Comm &                          mpi_communicator);
+template TrilinosWrappers::MPI::BlockVector
+AverageVelocities<3, TrilinosWrappers::MPI::BlockVector, IndexSet>::
+calculate_average_velocities(
+  const TrilinosWrappers::MPI::BlockVector &present_solution,
+  const Parameters::SimulationControl      &simulation_control,
+  const Parameters::PostProcessing &        post_processing,
+  const double &                            current_time,
+  const DoFHandler &                     locally_owned_dofs,
+  const DoFHandler &                     locally_relevant_dofs,
+  const MPI_Comm &                     mpi_communicator); */
