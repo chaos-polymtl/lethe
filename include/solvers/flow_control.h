c57040be6abd70b2f38df0ec0ca34e536d906138
acdaigneault <acdaigneault@gmail.com>
2020-11-02 16:47:13 -0500

    Improved flow control algorithm Avoided negative force Added bulk_velocity output Added template for flow control with trilinos vectors

--- include/solvers/flow_control.h
+++ include/solvers/flow_control.h
@@ -26,6 +26,27 @@
 #include <deal.II/fe/fe_values.h>
 #include <deal.II/fe/mapping_q.h>
 
+// Lac
+#include <deal.II/lac/affine_constraints.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/precondition_block.h>
+#include <deal.II/lac/solver_bicgstab.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/solver_gmres.h>
+#include <deal.II/lac/sparse_direct.h>
+#include <deal.II/lac/sparse_ilu.h>
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/sparsity_tools.h>
+#include <deal.II/lac/vector.h>
+
+// Lac - Trilinos includes
+#include <deal.II/lac/trilinos_parallel_block_vector.h>
+#include <deal.II/lac/trilinos_precondition.h>
+#include <deal.II/lac/trilinos_solver.h>
+#include <deal.II/lac/trilinos_sparse_matrix.h>
+#include <deal.II/lac/trilinos_vector.h>
+
 // Lethe includes
 #include <core/parameters.h>
 
@@ -48,6 +69,8 @@ public:
                  const MPI_Comm &                      mpi_communicator);
   std::vector<double>
   flow_summary();
+  double
+  bulk_velocity();
 
 private:
   // The coefficients are stored in the following fashion :
@@ -55,9 +78,14 @@ private:
   Tensor<1, dim> beta;
   double         beta_n;
   double         beta_n1     = 0;
+  double         beta_1n;
   double         flow_rate_n = 0;
   double         flow_rate_1n;
-  double         area = 0;
+  double         area;
+
+  // Variables used to improve convergence
+  bool           adjusted = false;
+  double         threshold = 1.005; // 0.5%
 };
 
 template <int dim, typename VectorType>
@@ -71,6 +99,7 @@ FlowControl<dim, VectorType>::calculate_beta(
   const unsigned int &                  step_number,
   const MPI_Comm &                      mpi_communicator)
 {
+  beta_1n      = beta_n;
   beta_n       = beta_n1;
   flow_rate_1n = flow_rate_n;
 
@@ -87,7 +116,7 @@ FlowControl<dim, VectorType>::calculate_beta(
                                    fe,
                                    face_quadrature_formula,
                                    update_values | update_quadrature_points |
-                                     update_JxW_values | update_normal_vectors);
+                                   update_JxW_values | update_normal_vectors);
 
   unsigned int boundary_id = flow_control.id_flow_control;
 
@@ -97,46 +126,83 @@ FlowControl<dim, VectorType>::calculate_beta(
 
   // Calculating area and volumetric flow rate at the inlet flow
   for (const auto &cell : dof_handler.active_cell_iterators())
+  {
+    if (cell->is_locally_owned() && cell->at_boundary())
     {
-      if (cell->is_locally_owned() && cell->at_boundary())
+      for (unsigned int face = 0; face < GeometryInfo<dim>::faces_per_cell;
+           face++)
+      {
+        if (cell->face(face)->at_boundary())
         {
-          for (unsigned int face = 0; face < GeometryInfo<dim>::faces_per_cell;
-               face++)
+          fe_face_values.reinit(cell, face);
+          if (cell->face(face)->boundary_id() == boundary_id)
+          {
+            for (unsigned int q = 0; q < n_q_points; q++)
             {
-              if (cell->face(face)->at_boundary())
-                {
-                  fe_face_values.reinit(cell, face);
-                  if (cell->face(face)->boundary_id() == boundary_id)
-                    {
-                      for (unsigned int q = 0; q < n_q_points; q++)
-                        {
-                          area += fe_face_values.JxW(q);
-                          normal_vector = fe_face_values.normal_vector(q);
-                          fe_face_values[velocities].get_function_values(
-                            present_solution, velocity_values);
-                          flow_rate_n += velocity_values[q] * normal_vector *
-                                         fe_face_values.JxW(q);
-                        }
-                    }
-                }
+              area += fe_face_values.JxW(q);
+              normal_vector = fe_face_values.normal_vector(q);
+              fe_face_values[velocities].get_function_values(
+                present_solution, velocity_values);
+              flow_rate_n += velocity_values[q] * normal_vector *
+                             fe_face_values.JxW(q);
             }
+          }
         }
+      }
     }
+  }
 
   area        = Utilities::MPI::sum(area, mpi_communicator);
   flow_rate_n = Utilities::MPI::sum(flow_rate_n, mpi_communicator);
 
-  // Calculating the next beta coefficient
   const double dt          = simulation_control.dt;
   const double flow_rate_0 = flow_control.flow_rate;
 
+  // If flow is reached with no force, adjusted is set to true,
+  // threshold may be decreased
+  if (abs(beta_n - 0) < 1e-6 && step_number > 1)
+  {
+    if (abs(flow_rate_n) < abs(flow_rate_0))
+      adjusted = true;
+    else if (abs(flow_rate_1n - flow_rate_n) < abs(flow_rate_n - flow_rate_0)
+             && abs(threshold - 1.00125) < 1e-6)
+      threshold = 1 + 0.5 * (threshold - 1);
+  }
+
   if (step_number <= 1)
+  {
+    // Initial beta
     beta_n1 = flow_control.beta_0;
+  }
   else if (step_number == 2)
-    beta_n1 = beta_n - (flow_rate_0 - flow_rate_n) / (area * dt);
+  {
+    // 2nd time step
+    beta_n1 = 0.5 * (flow_rate_n - flow_rate_0) / (area * dt);
+  }
+  else if (abs(flow_rate_n) > abs(flow_rate_0) &&
+           abs(flow_rate_n) < threshold * abs(flow_rate_0)
+           && adjusted == false)
+  {
+    // If the flow rate between intended flow rate and the threshold,
+    // it decreases by itself (no force applied)
+    beta_n1 = 0;
+  }
   else
+  {
+    // Standard flow controller.
+    // Calculate the new beta to control the flow.
+    // If intended flow rate is reached, new beta only maintains the force to
+    // keep the flow at the intended value. Is so, if calculated beta is
+    // negative it's set to 0 to avoided +/- oscillations
     beta_n1 =
       beta_n - (flow_rate_0 - 2 * flow_rate_n + flow_rate_1n) / (area * dt);
+    // If flow rate was already adjusted and beta is the same sign
+    // than flow_rate_0
+    if (flow_rate_0 * beta_n1 > 0 && adjusted == true)
+      beta_n1 = 0;
+  }
+
+
 
   // Setting beta coefficient only to new time step
   if (flow_control.flow_direction == 0)
@@ -153,8 +219,16 @@ template <int dim, typename VectorType>
 std::vector<double>
 FlowControl<dim, VectorType>::flow_summary()
 {
-  std::vector<double> summary{area, flow_rate_n, beta_n1};
+  std::vector<double> summary{area, flow_rate_n, beta_n};
   return summary;
+}
+
+template <int dim, typename VectorType>
+double
+FlowControl<dim, VectorType>::bulk_velocity()
+{
+  double u_b = flow_rate_n / area;
+  return u_b;
 }
 
 #endif
