ac2c89d56645780c0e2195cf22cf589215073df5
Bruno <blais.bruno@gmail.com>
2020-11-25 13:25:53 -0500

    Moved everything related to force calculation to postprocessing_cfd

--- include/solvers/postprocessing_cfd.h
+++ include/solvers/postprocessing_cfd.h
@@ -14,12 +14,63 @@
  * ---------------------------------------------------------------------
 
  *
- * Author: Audrey Collard-Daigneault, Polytechnique Montreal, 2020 -
+ * Author: Audrey Collard-Daigneault, Bruno Blais, Polytechnique Montreal, 2020
+ -
  */
 
-#include <solvers/flow_control.h>
 
 #ifndef lethe_postprocessing_cfd_h
+
+
+// Base
+#  include <deal.II/base/quadrature_lib.h>
+
+// Lac
+#  include <deal.II/lac/dynamic_sparsity_pattern.h>
+#  include <deal.II/lac/vector.h>
+
+// Dofs
+#  include <deal.II/dofs/dof_accessor.h>
+#  include <deal.II/dofs/dof_handler.h>
+
+// Fe
+#  include <deal.II/fe/fe_q.h>
+#  include <deal.II/fe/fe_system.h>
+#  include <deal.II/fe/fe_values.h>
+#  include <deal.II/fe/mapping_q.h>
+
+// Lethe includes
+#  include <core/boundary_conditions.h>
+#  include <core/parameters.h>
+
+/**
+ * @brief Calculates the force due to the fluid motion on every boundary conditions
+ * @return std::vector of forces on each boundary condition
+ * Post-processing function
+ * This function calculates the force acting on each of the boundary conditions
+ * within the domain. It generates a vector which size is the number of boundary
+ * conditions
+ *
+ * @param dof_handler The dof_handler used for the calculation
+ *
+ * @param evaluation_point The solution at which the force is calculated
+ *
+ * @param physical_properties The parameters containing the required physical properties
+ *
+ * @param boundary_conditions The boundary conditions object
+ *
+ * @param mpi_communicator The mpi communicator. It is used to reduce the force calculation
+ */
+template <int dim, typename VectorType>
+std::vector<Tensor<1, dim>>
+calculate_forces(
+  const DoFHandler<dim> &                              dof_handler,
+  const VectorType &                                   evaluation_point,
+  const Parameters::PhysicalProperties &               physical_properties,
+  const BoundaryConditions::NSBoundaryConditions<dim> &boundary_conditions,
+  const MPI_Comm &                                     mpi_communicator);
+
+
 
 /**
  * @brief calculate_flow_rate. This function calculates the volumetric flow
