771b106d42b1eaf4d7c6e789f8035fc23881c31a
acdaigneault <71884806+acdaigneault@users.noreply.github.com>
2021-06-09 14:23:00 -0400

    RPT Photon count calculation with Monte Carlo method (#177)
    This branch adds the first step to an extension of Lethe for RPT. It allows photon counts calculation with the Monte Carlo method.
    
    The application to use this feature is rpt_3d.
    
    A complete example is provided with all parameters needed for the calculation. (rpt_count_calculation.prm)
    
    There are 4 unit tests :
    
    position_parameters to assure position parameters are proceed correctly in any different cases related to the particle position regarding the detector position.
    solid_angle verifies angles for the same reason than position_parameters.
    reactor_detector_path_lengths to assure path lengths calculation are proceed correctly.
    count to assure that the count is always the same with a fixed seed number for random number generator and iteration number.
    
    Former-commit-id: 67f89ef9201ca4cbe7306536091b95708f5823b0

---
+++ source/rpt/parameters_rpt.cc
@@ -0,0 +1,158 @@
+#include <rpt/parameters_rpt.h>
+#include <time.h>
+
+void
+Parameters::RPTParameters::declare_parameters(ParameterHandler &prm)
+{
+  prm.enter_subsection("rpt parameters");
+  {
+    prm.declare_entry("particle positions file",
+                      "none",
+                      Patterns::FileName(),
+                      "Particle positions file name");
+
+    prm.declare_entry(
+      "export counts",
+      "false",
+      Patterns::Bool(),
+      "Enable to export counts result in a .csv file <true|false>");
+
+    prm.declare_entry("monte carlo iteration",
+                      "1",
+                      Patterns::Integer(),
+                      "Number of Monte Carlo iteration");
+
+    prm.declare_entry("random number seed",
+                      "auto",
+                      Patterns::Anything(),
+                      "Seed of the random number generator <auto|number>");
+
+    prm.declare_entry("reactor radius",
+                      "1",
+                      Patterns::Double(),
+                      "Radius of the reactor or tank");
+
+    prm.declare_entry("peak-to-total ratio",
+                      "1",
+                      Patterns::Double(),
+                      "Peak-to-total ratio");
+
+    prm.declare_entry("sampling time",
+                      "1",
+                      Patterns::Double(),
+                      "Sampling time of the counting");
+  }
+  prm.leave_subsection();
+}
+
+void
+Parameters::RPTParameters::parse_parameters(ParameterHandler &prm)
+{
+  prm.enter_subsection("rpt parameters");
+  {
+    particle_positions_file = prm.get("particle positions file");
+    export_counts           = prm.get_bool("export counts");
+    n_monte_carlo_iteration = prm.get_integer("monte carlo iteration");
+    reactor_radius          = prm.get_double("reactor radius");
+    peak_to_total_ratio     = prm.get_double("peak-to-total ratio");
+    sampling_time           = prm.get_double("sampling time");
+
+    seed = (prm.get("random number seed") == "auto") ?
+             time(NULL) :
+             prm.get_integer("random number seed");
+  }
+  prm.leave_subsection();
+}
+
+
+void
+Parameters::InitialRPTParameters::declare_parameters(ParameterHandler &prm)
+{
+  prm.enter_subsection("parameter tuning");
+  {
+    prm.declare_entry("tuning",
+                      "false",
+                      Patterns::Bool(),
+                      "Enable parameter tuning <true|false>");
+
+    prm.declare_entry("dead time",
+                      "1",
+                      Patterns::Double(),
+                      "Dead time of the detector per accepted pulse");
+
+    prm.declare_entry("activity",
+                      "1",
+                      Patterns::Double(),
+                      "Activity of the tracer");
+
+    prm.declare_entry("gamma-rays emitted",
+                      "1",
+                      Patterns::Double(),
+                      "Number of gamma-rays emitted by each disintegration");
+
+    prm.declare_entry("attenuation coefficient reactor",
+                      "1",
+                      Patterns::Double(),
+                      "Total linear attenuation coefficient of the medium");
+
+    prm.declare_entry("attenuation coefficient detector",
+                      "1",
+                      Patterns::Double(),
+                      "Total linear attenuation coefficient of the detector");
+  }
+  prm.leave_subsection();
+}
+
+void
+Parameters::InitialRPTParameters::parse_parameters(ParameterHandler &prm)
+{
+  prm.enter_subsection("parameter tuning");
+  {
+    tuning             = prm.get_bool("tuning");
+    dead_time          = prm.get_double("dead time");
+    activity           = prm.get_double("activity");
+    gamma_rays_emitted = prm.get_double("gamma-rays emitted");
+    attenuation_coefficient_reactor =
+      prm.get_double("attenuation coefficient reactor");
+    attenuation_coefficient_detector =
+      prm.get_double("attenuation coefficient detector");
+  }
+  prm.leave_subsection();
+}
+
+void
+Parameters::DetectorParameters::declare_parameters(ParameterHandler &prm)
+{
+  prm.enter_subsection("detector parameters");
+  {
+    prm.declare_entry("number", "1", Patterns::Integer(), "Number of detector");
+
+    prm.declare_entry("radius",
+                      "1",
+                      Patterns::Double(),
+                      "Radius of all detectors");
+
+    prm.declare_entry("length",
+                      "1",
+                      Patterns::Double(),
+                      "Length of all detectors");
+
+    prm.declare_entry("detector positions file",
+                      "none",
+                      Patterns::FileName(),
+                      "Detector positions file name");
+  }
+  prm.leave_subsection();
+}
+
+void
+Parameters::DetectorParameters::parse_parameters(ParameterHandler &prm)
+{
+  prm.enter_subsection("detector parameters");
+  {
+    radius                  = prm.get_double("radius");
+    length                  = prm.get_double("length");
+    detector_positions_file = prm.get("detector positions file");
+  }
+  prm.leave_subsection();
+}
