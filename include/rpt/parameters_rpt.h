67f89ef9201ca4cbe7306536091b95708f5823b0
acdaigneault <71884806+acdaigneault@users.noreply.github.com>
2021-06-09 14:23:00 -0400

    RPT Photon count calculation with Monte Carlo method (#177)
    This branch adds the first step to an extension of Lethe for RPT. It allows photon counts calculation with the Monte Carlo method.
    
    The application to use this feature is rpt_3d.
    
    A complete example is provided with all parameters needed for the calculation. (rpt_count_calculation.prm)
    
    There are 4 unit tests :
    
    position_parameters to assure position parameters are proceed correctly in any different cases related to the particle position regarding the detector position.
    solid_angle verifies angles for the same reason than position_parameters.
    reactor_detector_path_lengths to assure path lengths calculation are proceed correctly.
    count to assure that the count is always the same with a fixed seed number for random number generator and iteration number.

---
+++ include/rpt/parameters_rpt.h
@@ -0,0 +1,119 @@
+/* ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2019 - 2021 by the Lethe authors
+ *
+ * This file is part of the Lethe library
+ *
+ * The Lethe library is free software; you can use it, redistribute
+ * it, and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * The full text of the license can be found in the file LICENSE at
+ * the top level of the Lethe distribution.
+ *
+ * ---------------------------------------------------------------------
+
+ *
+ * Author: Audrey Collard-Daigneault, Polytechnique Montreal, 2021-
+ */
+
+/*
+ *  This file defines parameters needed for the RPT simulation in the Parameters
+ * namespace.
+ */
+
+#ifndef lethe_rpt_parameters_h
+#define lethe_rpt_parameters_h
+
+#include <deal.II/base/config.h>
+
+#include <core/parameters.h>
+
+#include <deal.II/base/parameter_acceptor.h>
+#include <deal.II/base/parameter_handler.h>
+#include <deal.II/base/parsed_function.h>
+
+using namespace dealii;
+
+namespace Parameters
+{
+  /**
+   * @brief RPTParameters - Defines the common parameters for the RPT
+   * simulation as fixed parameters, number of Monte Carlo iterations and
+   * filename of the particle positions.
+   */
+
+  struct RPTParameters
+  {
+    // Particle positions filename
+    std::string particle_positions_file;
+
+    // Enable to export counts result in a .csv file
+    bool export_counts;
+
+    // Number of Monte Carlo iterations for alpha and theta
+    unsigned int n_monte_carlo_iteration;
+
+    // Seed of the random number generator
+    int seed;
+
+    // All parameters that are fixed by the user
+    double reactor_radius; // [m]
+    double peak_to_total_ratio;
+    double sampling_time; // [s]
+
+    static void
+    declare_parameters(ParameterHandler &prm);
+    void
+    parse_parameters(ParameterHandler &prm);
+  };
+
+  /**
+   * @brief InitialRPTParameters - Allows parameters tuning. If turned on,
+   * other parameters values are the initial guess for the optimization,
+   * otherwise those act as fixed parameters.
+   */
+
+  struct InitialRPTParameters
+  {
+    // Enable tuning parameters
+    bool tuning;
+
+    // Parameters to tune or fixed parameters if tuning is disable
+    double dead_time; // Dead time of the detector per accepted pulse
+    double activity;  // Activity of the tracer
+    double
+      gamma_rays_emitted; // Number of gamma-rays emitted by each disintegration
+    double attenuation_coefficient_reactor;  // Total linear attenuation
+                                             // coefficient of the medium
+    double attenuation_coefficient_detector; // Total linear attenuation
+                                             // coefficient of the detector
+
+    static void
+    declare_parameters(ParameterHandler &prm);
+    void
+    parse_parameters(ParameterHandler &prm);
+  };
+
+  /**
+   * @brief DetectorParameters - Defines information related to detectors. All
+   * detectors must have the same dimensions (r and l).
+   */
+
+  struct DetectorParameters
+  {
+    double      radius;
+    double      length;
+    std::string detector_positions_file;
+
+    static void
+    declare_parameters(ParameterHandler &prm);
+    void
+    parse_parameters(ParameterHandler &prm);
+  };
+
+} // namespace Parameters
+
+
+
+#endif // lethe_rpt_parameters_h
---
+++ include/rpt/rpt.h
@@ -0,0 +1,88 @@
+/* ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2020 - by the Lethe authors
+ *
+ * This file is part of the Lethe library
+ *
+ * The Lethe library is free software; you can use it, redistribute
+ * it, and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either
+ * version 3.1 of the License, or (at your option) any later version.
+ * The full text of the license can be found in the file LICENSE at
+ * the top level of the Lethe distribution.
+ *
+ * ---------------------------------------------------------------------
+
+*
+* Authors: Bruno Blais, Ghazaleh Mirakhori, Audrey Collard-Daigneault,
+* Polytechnique Montreal, 2020-
+*/
+
+/**
+ * This class allows to calculate the photon count from a particle received by
+ * a detector with the Monte Carlo method.
+ */
+
+#ifndef lethe_rpt_h
+#define lethe_rpt_h
+
+/**
+ * This class handles positions of particles and detector and allows to
+ * calculate count for every pair. It also shows results in terminal or exports
+ * them in .csv file if enable.
+ */
+
+// deal.II includes
+#include <deal.II/base/point.h>
+#include <deal.II/base/tensor.h>
+#include <deal.II/base/utilities.h>
+
+#include <rpt/detector.h>
+#include <rpt/parameters_rpt.h>
+#include <rpt/particle_detector_interactions.h>
+#include <rpt/radioactive_particle.h>
+#include <rpt/rpt_calculating_parameters.h>
+
+using namespace dealii;
+
+template <int dim>
+class RPT
+{
+public:
+  /**
+   * @brief Constructor for the RPT
+   *
+   * @param RPTparameters All parameters and positions needed for the count
+   * calculation
+   *
+   */
+  RPT(RPTCalculatingParameters &RPTparameters);
+
+  /**
+   * @brief Calculate photon count for all particle positions and detector
+   */
+  void
+  calculate();
+
+  /**
+   * @brief Read text file for particle positions and assign them to particle
+   * objects
+   */
+  void
+  assign_particle_positions();
+
+  /**
+   * @brief Read text file for detector positions and assign them to detector
+   * objects
+   */
+  void
+  assign_detector_positions();
+
+private:
+  RPTCalculatingParameters rpt_parameters;
+
+  std::vector<RadioParticle<dim>> particle_positions;
+  std::vector<Detector<dim>>      detectors;
+};
+
+#endif
