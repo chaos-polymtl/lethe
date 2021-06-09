b771a19cc724490c7adbf4f65bdc1aabe8db614f
acdaigneault <acdaigneault@gmail.com>
2021-06-09 11:27:23 -0400

    Fix count unit tests

--- tests/rpt/count.cc
+++ tests/rpt/count.cc
@@ -66,7 +66,7 @@ test()
   rpt_parameters.rpt_param.reactor_radius                       = 0.5;
   rpt_parameters.rpt_param.peak_to_total_ratio                  = 1;
   rpt_parameters.rpt_param.sampling_time                        = 1;
-  rpt_parameters.rpt_param.iteration_number                     = 10000;
+  rpt_parameters.rpt_param.n_monte_carlo_iteration              = 10000;
   rpt_parameters.rpt_param.seed                                 = 1;
   rpt_parameters.initial_param.dead_time                        = 1;
   rpt_parameters.initial_param.activity                         = 1;
