b23a1329518f4eb72dd34735885a60ea1d40b5b6
acdaigneault <acdaigneault@gmail.com>
2021-06-23 12:41:54 -0400

    Fix Python script to use NOMAD
    Because of the NOMAD multithreading, there was some issues about new parameter files writing and reading (same .prm for many runs)
    Change the name of the new parameter file to a temporary file in /tmp/

--- examples/rpt/parameters_tuning/rpt_lethe_nomad.py
+++ examples/rpt/parameters_tuning/rpt_lethe_nomad.py
@@ -2,32 +2,32 @@
 Name   : rpt_lethe_nomad.py
 Author : Audrey Collard-Daigneault
 Date   : 17-06-2021
-Desc   : This code is a mega Python code to run the rpt_3d lethe application with the NOMAD software for
+Desc   : This code is a meta Python code to run the rpt_3d lethe application with the NOMAD software for
          blackbox optimization
 """
 
 import os
 import sys
 
-# Path to files
-# path = "/mnt/DATA/rpt_postprocessing/lethe_nomad/"
-path = "./"
-
-# Parameter filenames
-initial_lethe_parameter_file = "rpt_parameters.prm"
-new_lethe_parameter_file = "rpt_new_parameters.prm"
-nomad_parameter_file = "param_nomad.txt"
-
-# List of parameters to tune
-parameters_to_tune = ["dead time", "activity", "attenuation coefficient reactor"]
-n_parameters_to_tune = len(parameters_to_tune)
+# Path to files/application
+path = "./"  # Current directory
+path_to_rpt_3d = "/home/audrey/work/lethe/build/applications/rpt_3d/rpt_3d"
 
 # Get new calculated parameters
 tmpfile = sys.argv[1]  # Temporary file for new values
 with open(tmpfile, "r") as values:
     new_values = values.read().split()
 
-# Search to modify .prm new calculated tuned parameters
+# Parameter filenames
+initial_lethe_parameter_file = "rpt_parameters.prm"
+tmp_lethe_parameter_file = "/tmp/rpt_new_parameters_" + tmpfile.split("/")[2] + ".prm"
+nomad_parameter_file = "param_nomad.txt"
+
+# List of parameters to tune
+parameters_to_tune = ["dead time", "activity", "attenuation coefficient reactor"]
+n_parameters_to_tune = len(parameters_to_tune)
+
+# Search to modify .prm with new calculated tuned parameters
 with open(path + initial_lethe_parameter_file, "r") as file:  # Read .prm file with split lines
     filestr = file.read().split("\n")
 
@@ -39,8 +39,11 @@ for line in range(0, len(filestr)):
             if parameters_to_tune[i] in filestr[line]:
                 filestr[line] = filestr[line].replace(last_value, " " + new_values[i])
 
-with open(path + new_lethe_parameter_file, "w") as file:  # Write .prm with new parameters
+with open(tmp_lethe_parameter_file, "w") as file:  # Write .prm with new parameters
     file.write("\n".join(filestr))
 
 # Call rpt_3d executable
-os.system(path + "rpt_3d " + path + new_lethe_parameter_file)
+os.system(path_to_rpt_3d + " " + tmp_lethe_parameter_file + " | tee -a NOMAD_input_test.txt")
+
+# Delete temporary parameter files
+os.remove(tmp_lethe_parameter_file)
