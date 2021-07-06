878189b332e0ec257a39339aff497c6697ead9a3
tonielgeitani <tonielgeitani@gmail.com>
2021-07-06 16:06:53 -0400

    add first attempt for smoothing void fraction

--- include/fem-dem/gls_vans.h
+++ include/fem-dem/gls_vans.h
@@ -181,8 +181,8 @@ private:
   const bool   PSPG               = true;
   const bool   SUPG               = true;
   const bool   DCDD               = true;
-  const bool   grad_div           = false;
-  const bool   full_stress_tensor = false;
+  const bool   grad_div           = true;
+  const bool   full_stress_tensor = true;
   const double GLS_u_scale        = 1;
 };
 
