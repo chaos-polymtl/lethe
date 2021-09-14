45f7003b36dfdc566b732971ee1bc229a6166943
Lucka <luckabarbeau@gmail.com>
2021-09-14 12:04:57 -0400

    correct bug in test try 2

--- tests/core/lethe_grid_tool_mesh_cut_by_flat_3.cc
+++ tests/core/lethe_grid_tool_mesh_cut_by_flat_3.cc
@@ -102,6 +102,18 @@ test()
     // Printing the final position for all the vertices
 
     #if DEAL_II_VERSION_GTE(9, 3, 0)
+      Legacy::DataOut<3> data_out;
+      data_out.attach_dof_handler(dof_handler);
+      data_out.add_data_vector(subdomain, "subdomain");
+      data_out.build_patches();
+      std::ofstream output("solution.vtu");
+      data_out.write_vtu(output);
+      Legacy::DataOut<2,DoFHandler<2,3>> flat_data_out;
+      flat_data_out.attach_dof_handler(flat_dof_handler);
+      flat_data_out.build_patches();
+      std::ofstream flat_output("flat_trig.vtu");
+      flat_data_out.write_vtu(flat_output);
+    #else
       DataOut<3> data_out;
       data_out.attach_dof_handler(dof_handler);
       data_out.add_data_vector(subdomain, "subdomain");
@@ -109,18 +121,6 @@ test()
       std::ofstream output("solution.vtu");
       data_out.write_vtu(output);
       DataOut<2,3> flat_data_out;
-      flat_data_out.attach_dof_handler(flat_dof_handler);
-      flat_data_out.build_patches();
-      std::ofstream flat_output("flat_trig.vtu");
-      flat_data_out.write_vtu(flat_output);
-    #else
-      Legacy::DataOut<3> data_out;
-      data_out.attach_dof_handler(dof_handler);
-      data_out.add_data_vector(subdomain, "subdomain");
-      data_out.build_patches();
-      std::ofstream output("solution.vtu");
-      data_out.write_vtu(output);
-      Legacy::DataOut<2,3> flat_data_out;
       flat_data_out.attach_dof_handler(flat_dof_handler);
       flat_data_out.build_patches();
       std::ofstream flat_output("flat_trig.vtu");
