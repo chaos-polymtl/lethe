39b72e9f7c1ef1dad95e787a02ce9ab11213f803
Luckabarbeau <Luckabarbeau@gmail.com>
2021-10-03 09:04:41 -0400

    Update include/core/ib_particle.h
    Co-authored-by: Bruno Blais <blais.bruno@gmail.com>

--- include/core/ib_particle.h
+++ include/core/ib_particle.h
@@ -57,7 +57,7 @@ public:
   initialise_all();
   /**
    * @brief
-   * initialised the value of the last state of the particle
+   * initialize the value of the last state of the particle
    *
    */
   void
