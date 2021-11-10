3a09179ca0a2e6bb772a94db508b1b56a2d4d10b
ghazalemir <55454234+ghazalemir@users.noreply.github.com>
2021-11-09 20:15:26 -0500

    Add multiple detectors with different parameters for each (#261)
    add multiple detectors with different parameters for each
    
    Update the Prm for the RPT examples
    
    Update rpt_cell_reconstruction_3d application_test prm

--- tests/rpt/reactor_detector_path_lengths.cc
+++ tests/rpt/reactor_detector_path_lengths.cc
@@ -56,8 +56,11 @@ test()
   Parameters::DetectorParameters detector_param;
   detector_param.radius = 0.5;
   detector_param.length = 1;
-  Point<3>    FP        = {0.5, 0, 1};
-  Point<3>    MP        = {1, 0, 1};
+  detector_param.dead_time.push_back(1);
+  detector_param.activity.push_back(1);
+  detector_param.attenuation_coefficient_reactor.push_back(1);
+  Point<3>    FP = {0.5, 0, 1};
+  Point<3>    MP = {1, 0, 1};
   Detector<3> detector(detector_param, 0, FP, MP);
 
   // Other parameters
