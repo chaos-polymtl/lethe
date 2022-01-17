92dfe12061650d2ad8f81b8e3d4608c9d2bea59a
Bruno Blais <blais.bruno@gmail.com>
2022-01-17 16:12:28 -0500

    Add the capacity for transient BC Vans & CFD-DEM (#345)
    Description of the problem
    Lethe supports transient boundary conditions, but the VANS and the CFD-DEM solvers don't integrate this capability
    
    Description of the solution
    The feature for transient Boundary conditions are added to these solvers
    
    How Has This Been Tested?
    This was quickly tested on a few cases to work, since this is already tested in the basic CFD solver and it uses the same functon, I don't think it is necessary to add a test.
    
    Former-commit-id: 2985a3fe4bb65fa799dca7746281165464d75396

--- source/fem-dem/gls_vans.cc
+++ source/fem-dem/gls_vans.cc
@@ -1021,6 +1021,18 @@ GLSVANSSolver<dim>::solve()
   while (this->simulation_control->integrate())
     {
       this->simulation_control->print_progression(this->pcout);
+      if ((this->simulation_control->get_step_number() %
+               this->simulation_parameters.mesh_adaptation.frequency !=
+             0 ||
+           this->simulation_parameters.mesh_adaptation.type ==
+             Parameters::MeshAdaptation::Type::none ||
+           this->simulation_control->is_at_start()) &&
+          this->simulation_parameters.boundary_conditions.time_dependent)
+        {
+          this->update_boundary_conditions();
+        }
+
+
       if (this->simulation_control->is_at_start())
         {
           initialize_void_fraction();
