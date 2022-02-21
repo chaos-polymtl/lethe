027577c7b89bee968f13002ced6fcad3e0a96686
Bruno Blais <blais.bruno@gmail.com>
2022-02-21 09:54:51 -0500

    Be more specific on the meaning of a variable

--- include/solvers/heat_transfer_scratch_data.h
+++ include/solvers/heat_transfer_scratch_data.h
@@ -364,10 +364,13 @@ public:
   PhysicalPropertiesManager            properties_manager;
   std::map<field, std::vector<double>> fields;
   std::vector<double>                  specific_heat;
-  std::vector<double>                  grad_specific_heat_temperature;
   std::vector<double>                  thermal_conductivity;
   std::vector<double>                  density;
   std::vector<double>                  viscosity;
+  // Gradient of the specific heat with respect to the temperature
+  // This is calculated by deriving the specific heat by the temperature
+  // (dCp/dT)
+  std::vector<double> grad_specific_heat_temperature;
 
   // Auxiliary property vector for VOF simulations
   std::vector<double> specific_heat_0;
