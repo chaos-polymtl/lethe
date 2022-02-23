54a9144efb377546e688eca865d083af876fe5de
Lucka <luckabarbeau@gmail.com>
2022-02-23 09:45:55 -0500

    WIP applied comments

--- include/fem-dem/ib_particles_dem.h
+++ include/fem-dem/ib_particles_dem.h
@@ -1,6 +1,21 @@
-//
-// Created by lucka on 2021-10-11.
-//
+/* ---------------------------------------------------------------------
+*
+* Copyright (C) 2019 - 2019 by the Lethe authors
+*
+* This file is part of the Lethe library
+*
+* The Lethe library is free software; you can use it, redistribute
+* it, and/or modify it under the terms of the GNU Lesser General
+* Public License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+* The full text of the license can be found in the file LICENSE at
+* the top level of the Lethe distribution.
+*
+* ---------------------------------------------------------------------
+
+*
+* Author: Lucka Barbeau, Shahab Golshan, Bruno Blais Polytechnique Montreal, 2021
+*/
 
 #ifndef LETHE_IB_PARTICLES_DEM_H
 #define LETHE_IB_PARTICLES_DEM_H
