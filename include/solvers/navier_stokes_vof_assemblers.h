42f57e4264ed5b1f915ab244eaa3481ab8a526dd
jeannejoachim <jeanne.joachim@gmail.com>
2022-03-12 18:44:27 -0500

    Add NonNewtonian assemblers to vof
    adapt classical non-newtonian assembler to the vof case, similarly to the newtonian assembler.

--- include/solvers/navier_stokes_vof_assemblers.h
+++ include/solvers/navier_stokes_vof_assemblers.h
@@ -154,4 +154,103 @@ public:
   const Parameters::SurfaceTensionForce STF_properties;
 };
 
+
+/**
+ * @brief Class that assembles the core of the Navier-Stokes equation
+ * using a Rheological model to predict non Newtonian behaviors
+ *
+ * @tparam dim An integer that denotes the number of spatial dimensions
+ *
+ * @ingroup assemblers
+ */
+
+
+template <int dim>
+class GLSNavierStokesVOFAssemblerNonNewtonianCore
+  : public NavierStokesAssemblerBase<dim>
+{
+public:
+  GLSNavierStokesVOFAssemblerNonNewtonianCore(
+    std::shared_ptr<SimulationControl> simulation_control)
+    : simulation_control(simulation_control)
+  {}
+
+  /**
+   * @brief Calculates an approximation of the gradient of the viscosity
+   * @param velocity_gradient The velocity gradient tensor on the quadrature point
+     @param velocity_hessians The velocity hessian tensor on the quadrture point
+     @param non_newtonian_viscosity The viscosity at which the gradient is calculated
+     @param d_gamma_dot Th difference in the shear rate magnitude to approximate the
+     viscosity variation with a slight change in the shear_rate magnitude
+   */
+  inline Tensor<1, dim>
+  get_viscosity_gradient(const Tensor<2, dim> &velocity_gradient,
+                         const Tensor<3, dim> &velocity_hessians,
+                         const double          shear_rate_magnitude,
+                         const double          grad_viscosity_shear_rate) const
+  {
+    // Calculates an approximation of the shear rate magnitude gradient using
+    // the derived form, since it does not change with rheological models
+    Tensor<1, dim> grad_shear_rate;
+    for (unsigned int d = 0; d < dim; ++d)
+      {
+        if (dim == 2)
+          {
+            for (unsigned int k = 0; k < dim; ++k)
+              {
+                grad_shear_rate[d] +=
+                  2 * (velocity_gradient[k][k] * velocity_hessians[k][d][k]) /
+                  shear_rate_magnitude;
+              }
+            grad_shear_rate[d] +=
+              (velocity_gradient[0][1] + velocity_gradient[1][0]) *
+              (velocity_hessians[0][d][1] + velocity_hessians[1][d][0]) /
+              shear_rate_magnitude;
+          }
+        else
+          {
+            for (unsigned int k = 0; k < dim; ++k)
+              {
+                grad_shear_rate[d] +=
+                  2 * (velocity_gradient[k][k] * velocity_hessians[k][d][k]) /
+                    shear_rate_magnitude +
+                  (velocity_gradient[(k + 1) % dim][(k + 2) % dim] +
+                   velocity_gradient[(k + 2) % dim][(k + 1) % dim]) *
+                    (velocity_hessians[(k + 1) % dim][d][(k + 2) % dim] +
+                     velocity_hessians[(k + 2) % dim][d][(k + 1) % dim]) /
+                    shear_rate_magnitude;
+              }
+          }
+      }
+    return grad_shear_rate * grad_viscosity_shear_rate;
+  };
+
+  /**
+   * @brief assemble_matrix Assembles the matrix
+   * @param scratch_data (see base class)
+   * @param copy_data (see base class)
+   */
+  virtual void
+  assemble_matrix(NavierStokesScratchData<dim> &        scratch_data,
+                  StabilizedMethodsTensorCopyData<dim> &copy_data) override;
+
+
+  /**
+   * @brief assemble_rhs Assembles the rhs
+   * @param scratch_data (see base class)
+   * @param copy_data (see base class)
+   */
+  virtual void
+  assemble_rhs(NavierStokesScratchData<dim> &        scratch_data,
+               StabilizedMethodsTensorCopyData<dim> &copy_data) override;
+
+  /**
+   * Enables SUPG stabilization for the Navier-Stokes formulation.
+   * We have not found any scenarios where it is relevant not to use SUPG
+   * stabilization yet.
+   */
+  const bool SUPG = true;
+
+  std::shared_ptr<SimulationControl> simulation_control;
+};
 #endif
