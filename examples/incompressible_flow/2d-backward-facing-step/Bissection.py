1e1751456e6081228ed860d9125e657eeb299b03
Charles Le Pailleur <charles.clepailleur@gmail.com>
2022-04-05 10:21:15 -0400

    Added Python files for analysis and bechmark values for backward-facing step example

---
+++ examples/incompressible_flow/2d-backward-facing-step/Bissection.py
@@ -0,0 +1,96 @@
+"""
+Postprocessing code for 2D-backward-facing-step example
+Computes the reattachment length (x_r) for Re = 100
+for several meshes with a bissection algorithm
+
+Author : Charles Le Pailleur
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pyvista as pv
+
+########################################
+########################################
+
+# EXAMPLE TO COMPUTE x_r (REATTACHMENT LENGTH) FOR Re = 100 
+# NOTE : THIS FILE MUST BE IN THE "2d-backward-facing-step" DIRECTORY TO 
+#        WORK PROPERLY
+
+# VARIABLES
+L_in = 15                    # Inlet length
+x_r_ref = 2.922              # Benchmark value for Re = 100 (Erturk 2008)
+							 # (see attached .txt file for other Reynolds
+							 #  x_r values)
+x_inf = 2.                   # Initial guesses
+x_sup = 3.
+tol = 1e-12                  # Bissection stop criterion
+
+# DATA EXTRACTION
+n = 11                       # Number of VTU files to be read
+file = list(range(0,n))
+data = list(range(0,n))
+N_elements = np.zeros(n)
+for i in range(0,n):
+    if i<10:
+        file[i] = ('Reynolds100-600/backward_facing_step_output.000' 
+                   + str(i) + '.0000.vtu')
+    elif i>=10:
+        file[i] = ('Reynolds100-600/backward_facing_step_output.00' 
+                   + str(i) + '.0000.vtu')
+    data[i] = pv.read(file[i])
+    data[i].set_active_vectors("velocity")
+    N_elements[i] = data[i].GetNumberOfElements(0)
+    
+# BISSECTION METHOD
+x_r = np.zeros(n)            # Reattachment length
+for i in range(0,n):
+    # Initial guesses
+    x1 = L_in + x_inf
+    x2 = L_in + x_sup
+    j = 0
+    # Bissection loop
+    while abs(x2-x1)>tol and j<=20:
+        xm = (x1+x2)/2
+        # Profiles calculation
+        a = np.array([x1, 0, 0])
+        b = np.array([x1, 0.01, 0])
+        profil1 = data[i].sample_over_line(a, b, resolution=1000)
+        u1 = profil1["velocity"][:,0]
+        a = np.array([xm, 0, 0])
+        b = np.array([xm, 2, 0])
+        profilm = data[i].sample_over_line(a, b, resolution=1000)
+        um = profilm["velocity"][:,0]
+        y = profil1["Distance"]
+        # Derivatives
+        dudy1 = (-u1[2] + 4*u1[1] - 3*u1[0]) / (y[2] - y[0])
+        dudym = (-um[2] + 4*um[1] - 3*um[0]) / (y[2] - y[0])
+        # Signs check
+        if dudy1*dudym<0:
+            x2 = xm
+        else:
+            x1 = xm
+        j+=1
+    x_r[i] = xm - L_in           # Distance from the step
+    print('Mesh refinement %.f : %.4f' % (i,x_r[i]))
+    
+# CONVERGENCE GRAPH
+mesh= np.linspace(0, n-1, n)
+plt.figure()
+plt.rc('axes', labelsize=15) 
+plt.scatter(mesh[1:], x_r[1:])
+plt.grid()
+plt.xticks([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+plt.xlabel("Mesh refinement step")
+plt.ylabel(r'$x_r$/h')
+
+# RELATIVE ERROR
+e = abs(x_r_ref-x_r)/x_r_ref
+
+# ERROR ANALYSIS
+plt.figure()
+plt.loglog(N_elements[1:],e[1:])
+plt.grid(which='minor')
+plt.xlabel("Number of elements")
+plt.ylabel(r'$\frac{x_r - \bar{x}_r}{x_r}$')
+
---
+++ examples/incompressible_flow/2d-backward-facing-step/velocity_distribution.py
@@ -0,0 +1,112 @@
+"""
+Postprocessing code for 2D-backward-facing-step example
+Computes velocity distributions at inlet and outlet and
+compares it with analytical solution (Poiseuille)
+
+Author : Charles Le Pailleur
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pyvista as pv
+
+########################################
+########################################
+
+# EXAMPLE TO COMPUTE VELOCITY DISTRIBUTION AT INLET AND OUTLET
+# NOTE : THIS FILE MUST BE IN THE "2d-backward-facing-step" DIRECTORY TO 
+#        WORK PROPERLY
+
+# VARIABLES
+L_out = 50                      # Outlet length
+L_in = 15                       # Inlet length
+h_in = 0.5                      # Half height of inlet
+h_out = 1                       # Half height of outlet
+u_moy_in = 1                    # Inflow mean velocity
+u_moy_out = 0.5                 # Outflow mean velocity
+plt.rc('axes', labelsize=14)
+L_out = L_in + L_out
+
+# DATA EXTRACTION
+n = 11                          # Number of VTU files to be read
+file = list(range(0,n))
+data = list(range(0,n))
+N = np.zeros(n)
+for i in range(0,n):
+    if i<10:
+        file[i] = ('Reynolds100-600/backward_facing_step_output.000' 
+                   + str(i) + '.0000.vtu')
+    elif i>=10:
+        file[i] = ('Reynolds100-600/backward_facing_step_output.00' 
+                   + str(i) + '.0000.vtu')
+    data[i] = pv.read(file[i])
+    data[i].set_active_vectors("velocity")
+
+###########################
+# Poiseuille Inlet
+###########################
+
+# EXACT SOLUTION
+k_in = -3/2*u_moy_in/h_in**2
+y_in_an = np.linspace(-h_in, h_in, 1001)
+u_in_an = k_in*(y_in_an**2-h_in**2)
+
+# WITH LETHE
+a = np.array([L_in, 1, 0])
+b = np.array([L_in, 2, 0])
+u_inlet_moy = np.zeros(n)
+u_inlet_max = np.zeros(n)
+mesh = np.linspace(0, n-1, n)
+plt.figure()
+for i in range(0,n):
+    # Data
+    data_inlet = data[i].sample_over_line(a, b, resolution=1000)
+    y_inlet = data_inlet["Distance"]
+    u_inlet = data_inlet["velocity"][:,0]
+    # Graph
+    plt.plot(u_inlet ,y_inlet, label="Mesh "+str(i))
+    # Mean and max velocities
+    u_inlet_moy[i] = np.mean(u_inlet)
+    u_inlet_max[i] = max(u_inlet)
+    
+# Graph setup
+plt.plot(u_in_an, y_inlet, label="Poiseuille", linestyle='--',
+         linewidth=2.0, color='k')
+plt.xlabel(r'$u_{in}$')
+plt.ylabel("y/h")
+plt.legend(fontsize=8)
+plt.axis([0, 2.5, 0, 1])
+
+####################
+# Poiseuille Outlet
+####################
+
+# EXACT SOLUTION
+k_out = -3/2*u_moy_out/h_out**2
+y_out_an = np.linspace(-h_out, h_out, 1001)
+u_out_an = k_out*(y_out_an**2-h_out**2)
+
+# LETHE
+a = np.array([L_out, 0, 0])
+b = np.array([L_out, 2, 0])
+u_outlet_moy = np.zeros(n)
+u_outlet_max = np.zeros(n)
+plt.figure()
+for i in range(1,n):
+    # Data
+    data_outlet = data[i].sample_over_line(a, b, resolution=1000)
+    y_outlet = data_outlet["Distance"]
+    u_outlet = data_outlet["velocity"][:,0]
+    # Graph
+    plt.plot(u_outlet, y_outlet, label="Mesh "+str(i))
+    # Mean and max velocities
+    u_outlet_moy[i] = np.mean(u_outlet)
+    u_outlet_max[i] = max(u_outlet)
+    
+# Graph setup
+plt.plot(u_out_an, y_outlet, label="Poiseuille", linestyle='--',
+         linewidth=2.0, color='k')
+plt.xlabel(r'$u_{out}$')
+plt.ylabel("y/h")
+plt.legend(fontsize=8)
+plt.axis([0,1.5,0,2])
