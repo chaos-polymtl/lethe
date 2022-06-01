e1cb081f993c63950dc610dcf6b0e036f4d68982
Shahab Golshan <shahab.golshan14@gmail.com>
2022-06-01 15:28:05 -0400

    Melting example (#479)
    Description of the problem
    I was planning to add a melting example in this PR, but I noticed there are some missing elements that should be added/addressed before adding the example. This PR adds the phase_change thermal expansion coefficient model.
    How Has This Been Tested?
    thermal_expansion_phase_change
    Future changes
    As mentioned, melting example will be added.
    
    Former-commit-id: 7b58b9cf020cbf517cfa6cfd35f20a784b2accc7

--- source/solvers/navier_stokes_assemblers.cc
+++ source/solvers/navier_stokes_assemblers.cc
@@ -1712,7 +1712,7 @@ BuoyancyAssembly<dim>::assemble_rhs(
       // Current temperature values
       double current_temperature = scratch_data.temperature_values[q];
 
-      strong_residual[q] += -force * thermal_expansion * current_temperature;
+      strong_residual[q] += force * thermal_expansion * current_temperature;
 
       // Assembly of the right-hand side
       for (unsigned int i = 0; i < n_dofs; ++i)
@@ -1720,8 +1720,8 @@ BuoyancyAssembly<dim>::assemble_rhs(
           const auto phi_u_i = scratch_data.phi_u[q][i];
 
           // Laplacian on the velocity terms
-          local_rhs(i) +=
-            -force * thermal_expansion * current_temperature * phi_u_i * JxW;
+          local_rhs(i) -=
+            force * thermal_expansion * current_temperature * phi_u_i * JxW;
         }
     }
 }
