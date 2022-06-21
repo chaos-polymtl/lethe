50fc0716819dfe7a97d10eadc69f1d6f4adb4a94
shahabgol <shahab.golshan14@gmail.com>
2022-06-21 13:44:42 -0400

    Rebase with master

--- source/solvers/gls_navier_stokes.cc
+++ source/solvers/gls_navier_stokes.cc
@@ -426,10 +426,10 @@ GLSNavierStokesSolver<dim>::setup_assemblers()
       // Surface tension force (STF)
       if (this->simulation_parameters.multiphysics.vof_parameters
             .surface_tension_force.enable)
-           {
-        this->assemblers.push_back(
-          std::make_shared<GLSNavierStokesVOFAssemblerSTF<dim>>(
-            this->simulation_control, this->simulation_parameters));
+        {
+          this->assemblers.push_back(
+            std::make_shared<GLSNavierStokesVOFAssemblerSTF<dim>>(
+              this->simulation_control, this->simulation_parameters));
 
           if (this->simulation_parameters.multiphysics.vof_parameters
                 .surface_tension_force.enable_marangoni_effect)
