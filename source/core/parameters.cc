e6efb6e91456f4d0f179277676e44980368d8157
voferreira <victor@LAPTOP-F2KIN6BL.localdomain>
2022-07-06 16:08:58 -0400

    Fix number of particles

--- source/core/parameters.cc
+++ source/core/parameters.cc
@@ -1991,7 +1991,7 @@ namespace Parameters
         prm.set("Function expression", "0; 0; 0");
       prm.leave_subsection();
 
-      unsigned int max_ib_particles = 1000;
+      unsigned int max_ib_particles = 10;
       particles.resize(max_ib_particles);
       for (unsigned int i = 0; i < max_ib_particles; ++i)
         {
