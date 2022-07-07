12f0f4ebf4f4526ad039984d3b5f65b2afecc7c0
CaroleAnneDaunais <carole.anne.daunais@hotmail.com>
2022-07-07 11:27:57 -0400

    generate_mass_matrix() function

--- include/solvers/post_processors_smoothing.h
+++ include/solvers/post_processors_smoothing.h
@@ -5,6 +5,7 @@
 // DEALII INCLUDES
 // Base
 #include <deal.II/base/tensor.h>
+#include <deal.II/lac/trilinos_sparse_matrix.h>
 
 #include <deal.II/dofs/dof_handler.h>
 #include <deal.II/dofs/dof_tools.h>
