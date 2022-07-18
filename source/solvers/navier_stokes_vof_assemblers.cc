f319d1134f906a0de4fa00bf5c8f5fc447e68a6c
Jeanne Joachim <72091781+jeannejoachim@users.noreply.github.com>
2022-07-18 10:56:51 -0400

    Vof ht fix dissipation (#527)
    Description of the problem
    Viscous dissipation in the air, with physical values chosen for numerical stability, lead to the air heating up the fluid of interest
    
    Description of the solution
    Add a parameter to choose where to apply viscous dissipation in the case of VOF
    Note that I preferred to leave the possibility to have viscous dissipation in both fluids, if they are close in viscosity
    Add a specialized ViscousDissipationVOF constructor
    
    How Has This Been Tested?
    Test added: air advected at the top edge, which does not lead to viscous dissipation so does not heat up the denser bottom fluid, as intended
    
    Documentation
     Update heat transfer documentation page
    
    Comments
    On the test, the error on the velocity and pressure is big because I did not give any analytical solution for that. I intend to change soon the analytic output to make it more adaptable, so that pressure and velocity are not outputted by default.
    
    
    
    Co-authored-by: Victor Oliveira Ferreira <65647639+voferreira@users.noreply.github.com>

--- source/solvers/navier_stokes_vof_assemblers.cc
+++ source/solvers/navier_stokes_vof_assemblers.cc
@@ -62,12 +62,14 @@ GLSNavierStokesVOFAssemblerCore<dim>::assemble_matrix(
     std::max_element(std::begin(phase_values), std::end(phase_values));
   bool solve_continuity(true);
 
-  if (vof_parameters.conservation.skip_mass_conservation_fluid_0)
+  if (vof_parameters.conservation.conservative_fluid ==
+      Parameters::FluidIndicator::fluid0)
     {
       if (*max_phase_cell < phase_cutoff)
         solve_continuity = false;
     }
-  else if (vof_parameters.conservation.skip_mass_conservation_fluid_1)
+  else if (vof_parameters.conservation.conservative_fluid ==
+           Parameters::FluidIndicator::fluid1)
     {
       if (*max_phase_cell > phase_cutoff)
         solve_continuity = false;
@@ -270,12 +272,14 @@ GLSNavierStokesVOFAssemblerCore<dim>::assemble_rhs(
     std::max_element(std::begin(phase_values), std::end(phase_values));
   bool solve_continuity(true);
 
-  if (vof_parameters.conservation.skip_mass_conservation_fluid_0)
+  if (vof_parameters.conservation.conservative_fluid ==
+      Parameters::FluidIndicator::fluid0)
     {
       if (*max_phase_cell < phase_cutoff)
         solve_continuity = false;
     }
-  else if (vof_parameters.conservation.skip_mass_conservation_fluid_1)
+  else if (vof_parameters.conservation.conservative_fluid ==
+           Parameters::FluidIndicator::fluid1)
     {
       if (*max_phase_cell > phase_cutoff)
         solve_continuity = false;
@@ -741,12 +745,14 @@ GLSNavierStokesVOFAssemblerNonNewtonianCore<dim>::assemble_matrix(
     std::max_element(std::begin(phase_values), std::end(phase_values));
   bool solve_continuity(true);
 
-  if (vof_parameters.conservation.skip_mass_conservation_fluid_0)
+  if (vof_parameters.conservation.conservative_fluid ==
+      Parameters::FluidIndicator::fluid0)
     {
       if (*max_phase_cell < phase_cutoff)
         solve_continuity = false;
     }
-  else if (vof_parameters.conservation.skip_mass_conservation_fluid_1)
+  else if (vof_parameters.conservation.conservative_fluid ==
+           Parameters::FluidIndicator::fluid1)
     {
       if (*max_phase_cell > phase_cutoff)
         solve_continuity = false;
@@ -986,12 +992,14 @@ GLSNavierStokesVOFAssemblerNonNewtonianCore<dim>::assemble_rhs(
     std::max_element(std::begin(phase_values), std::end(phase_values));
   bool solve_continuity(true);
 
-  if (vof_parameters.conservation.skip_mass_conservation_fluid_0)
+  if (vof_parameters.conservation.conservative_fluid ==
+      Parameters::FluidIndicator::fluid0)
     {
       if (*max_phase_cell < phase_cutoff)
         solve_continuity = false;
     }
-  else if (vof_parameters.conservation.skip_mass_conservation_fluid_1)
+  else if (vof_parameters.conservation.conservative_fluid ==
+           Parameters::FluidIndicator::fluid1)
     {
       if (*max_phase_cell > phase_cutoff)
         solve_continuity = false;
