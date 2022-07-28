8756c41da1703f41531dc184b86293f89aff370c
Amishga <amishga1999@gmail.com>
2022-07-28 16:00:26 -0400

    ribbon mixer srf example: change files in example folder

--- examples/incompressible_flow/3d_ribbon_mixer_srf/Np_vs_Re/template/launch.sh
+++ examples/incompressible_flow/3d_ribbon_mixer_srf/Np_vs_Re/template/launch.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#SBATCH --time=23:30:00
+#SBATCH --time=5:00:00
 #SBATCH --account=rrg-blaisbru
 #SBATCH --nodes=1
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=blais.bruno@gmail.com
 #SBATCH --ntasks-per-node=40 
 #SBATCH --output=%x-%j.out
 
