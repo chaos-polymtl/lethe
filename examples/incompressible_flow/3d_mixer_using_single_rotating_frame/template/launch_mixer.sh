6f7be8ee35cd9da0b1594962b7e46d243ca67ec5
Amishga <amishga1999@gmail.com>
2022-07-29 19:13:13 -0400

    Remove 2nd example: 3d_mixer_using_single_rotating_frame

--- examples/incompressible_flow/3d_mixer_using_single_rotating_frame/template/launch_mixer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=1:00:00
-#SBATCH --account=$yourgroupaccount
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=40 
-#SBATCH --output=%x-%j.out
-
-source $HOME/.dealii
-srun  $HOME/lethe/inst/bin/gls_navier_stokes_3d ribbon_gls.prm
