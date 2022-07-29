8e2b2d854c718bc6a8fa1c1f9090c4a30cfc150e
bibeauv <valerie.bibeau@polymtl.ca>
2022-07-29 15:16:43 -0400

    Files in examples

---
+++ examples/incompressible_flow/3d_mixer_using_single_rotating_frame/gather_torques.sh
@@ -0,0 +1,6 @@
+for i in $(cat case_index.txt) ; 
+do 
+echo "Gathering $i"
+echo "-------------"
+tail -1 ./$i/output/torque.00.dat   >> gather.dat
+done
--- examples/incompressible_flow/3d_mixer_using_single_rotating_frame/plot_torques.py
+++ examples/incompressible_flow/3d_mixer_using_single_rotating_frame/plot_torques.py
@@ -1,30 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-D=0.27
-N= 10 / 2 / np.pi
+D = 0.27
+N = 10 / 2 / np.pi
 
 Re = np.logspace(-1,np.log10(100),25)
 
-
 # Simulation Data
 C,Tx,Ty,Tz = np.loadtxt("gather.dat",unpack=True)
-n_dat =np.size(Tz)
-D=0.27
+n_dat = np.size(Tz)
+D = 0.27
 Np = 2 * np.pi * np.abs(Tz) / 1 / N**2 / D**5
 
 # Experimental data
-Re_1,Np_1, Re_2,Np_2, Re_3,Np_3 = np.loadtxt("newtonien_tiff.dat",unpack=True,skiprows=1)
-
-
-
+Re_1,Np_1, Re_2,Np_2, Re_3,Np_3 = np.loadtxt("experimental.dat",unpack=True,skiprows=1)
 
 plt.loglog(Re_1,Np_1,'o',mfc='None',label="Experiment 1")
 plt.loglog(Re_2,Np_2,'*',mfc='None',label="Experiment 2")
 plt.loglog(Re_3,Np_3,'^',mfc='None',label="Experiment 3")
 plt.loglog(Re[:n_dat],Np,'-',label="Lethe",color="black")
 
-plt.xlabel("$N$ [RPS]")
+plt.xlabel("$Re$")
 plt.ylabel("$N_p$")
 plt.legend()
 
