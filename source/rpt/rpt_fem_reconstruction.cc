6d78e3da8d5fc6880774f3a41a06433a0665a6e3
Amishga <amishga1999@gmail.com>
2022-08-09 09:36:34 -0400

    clang format

--- source/rpt/rpt_fem_reconstruction.cc
+++ source/rpt/rpt_fem_reconstruction.cc
@@ -310,10 +310,10 @@ RPTFEMReconstruction<dim>::output_raw_results()
         myfile << it->second[p] << sep;
 
       for (unsigned int d = 0; d < n_detector; ++d)
-      {
-        myfile << nodal_counts[d][it->first] << sep;
-        std::cout << nodal_counts[d][it->first] << sep;
-      }
+        {
+          myfile << nodal_counts[d][it->first] << sep;
+          std::cout << nodal_counts[d][it->first] << sep;
+        }
 
       myfile << "\n";
       std::cout << std::endl;
