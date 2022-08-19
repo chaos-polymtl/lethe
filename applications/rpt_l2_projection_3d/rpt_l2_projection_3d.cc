73ac7064d6da952acc81481ff41e4a07bcf3d90a
Amishga <amishga1999@gmail.com>
2022-08-19 08:07:27 -0400

    Homogenize all rpt headers

--- applications/rpt_l2_projection_3d/rpt_l2_projection_3d.cc
+++ applications/rpt_l2_projection_3d/rpt_l2_projection_3d.cc
@@ -1,20 +1,3 @@
-/* ---------------------------------------------------------------------
- *
- * Copyright (C) 2019 - by the Lethe authors
- *
- * This file is part of the Lethe library
- *
- * The Lethe library is free software; you can use it, redistribute
- * it, and/or modify it under the terms of the GNU Lesser General
- * Public License as published by the Free Software Foundation; either
- * version 3.1 of the License, or (at your option) any later version.
- * The full text of the license can be found in the file LICENSE at
- * the top level of the Lethe distribution.
- *
- * ---------------------------------------------------------------------
- *
- */
-
 #include <rpt/rpt_calculating_parameters.h>
 #include <rpt/rpt_fem_reconstruction.h>
 
--- include/rpt/rpt_fem_reconstruction.h
+++ include/rpt/rpt_fem_reconstruction.h
@@ -1,13 +1,13 @@
 /* ---------------------------------------------------------------------
  *
- * Copyright (C) 2022 - by the Lethe authors
+ * Copyright (C) 2022 - 2022 by the Lethe authors
  *
  * This file is part of the Lethe library
  *
  * The Lethe library is free software; you can use it, redistribute
  * it, and/or modify it under the terms of the GNU Lesser General
  * Public License as published by the Free Software Foundation; either
- * version 3.1 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  * The full text of the license can be found in the file LICENSE at
  * the top level of the Lethe distribution.
  *
