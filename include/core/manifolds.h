698333078e8f3f23b4b386032d109819842fa833
Bruno Blais <blais.bruno@gmail.com>
2022-08-26 17:18:50 -0400

    Fix indent

--- include/core/manifolds.h
+++ include/core/manifolds.h
@@ -188,20 +188,17 @@ attach_manifolds_to_triangulation(
  *
  * @param manifold_id Identifier of the manifold
  */
-void
-attach_cad_to_manifold(
+void attach_cad_to_manifold(
   std::shared_ptr<parallel::DistributedTriangulationBase<2>> triangulation,
   std::string                                                cad_name,
   unsigned int                                               manifold_id);
 
-void
-attach_cad_to_manifold(
+void attach_cad_to_manifold(
   std::shared_ptr<parallel::DistributedTriangulationBase<2, 3>> triangulation,
   std::string                                                   cad_name,
   unsigned int                                                  manifold_id);
 
-void
-attach_cad_to_manifold(
+void attach_cad_to_manifold(
   std::shared_ptr<parallel::DistributedTriangulationBase<3>> triangulation,
   std::string                                                cad_name,
   unsigned int                                               manifold_id);
