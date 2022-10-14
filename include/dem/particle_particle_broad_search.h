65788e559623361662be268413e7f972fe2b46af
Audrey Collard-Daigneault <71884806+acdaigneault@users.noreply.github.com>
2022-10-14 10:41:14 -0400

    Refactor the particle-particle broad search code in DEM (#612)
    Description of the problem
    PR n°1 of a series of refactoring PRs for the DEM code.
    Enhancement of the particle-particle broad search code.
    
    Description of the solution
    Addition of the private method particle-particle broad search code that allows to fill containers of contact pair candidates with a range of particles and the first iterator of this range (prior skipping the first particle when used for particles in main cell).
    
    Former-commit-id: 091cf28c313bb5041326cfd4182e0c755efb7550

--- include/dem/particle_particle_broad_search.h
+++ include/dem/particle_particle_broad_search.h
@@ -43,9 +43,6 @@ using namespace dealii;
 template <int dim>
 class ParticleParticleBroadSearch
 {
-private:
-  // std::vector<int> particle_candidate_container;
-
 public:
   ParticleParticleBroadSearch<dim>();
 
@@ -58,10 +55,10 @@ public:
    * search
    * @param cells_local_neighbor_list This vector is the output of
    * find_cell_neighbors class and shows the local neighbor cells of all local
-   * celsl in the triangulation
+   * cells in the triangulation
    * @param cells_ghost_neighbor_list This vector is the output of
    * find_cell_neighbors class and shows the ghost neighbor cells of all local
-   * celsl in the triangulation
+   * cells in the triangulation
    * @param local_contact_pair_candidates A map of vectors which contains all
    * the local-local particle pairs in adjacent cells which are collision
    * candidates
@@ -81,6 +78,44 @@ public:
       dim>::particle_particle_candidates &local_contact_pair_candidates,
     typename dem_data_containers::dem_data_structures<
       dim>::particle_particle_candidates &ghost_contact_pair_candidates);
+
+  /**
+   * Stores the candidate particle-particle collision pairs with a given
+   * particle iterator. particle_begin iterator is useful to skip storage of the
+   * first particle in main cell (particle_begin will be the iterator after the
+   * particles_to_evaluate.begin() in that case). When particle_begin is
+   * particles_to_evaluate.begin(), it stores all the particle id in
+   * contact_pair_candidates_container.
+   *
+   * @param particle_begin The particle iterator to start storing particle id
+   * @param particles_to_evaluate The particle range to evaluate with the
+   * particle iterator prior storing ids
+   * @param contact_pair_candidates_container A vector which will contain all
+   * the particle pairs which are collision candidates
+   */
+  inline void
+  store_candidates(
+    const typename Particles::ParticleHandler<
+      dim>::particle_iterator_range::iterator &particle_begin,
+    const typename Particles::ParticleHandler<dim>::particle_iterator_range
+      &                                 particles_to_evaluate,
+    std::vector<types::particle_index> &contact_pair_candidates_container)
+  {
+    // Create a arbitrary temporary empty container
+    if (contact_pair_candidates_container.empty())
+      {
+        contact_pair_candidates_container.reserve(40);
+      }
+
+    // Store particle ids from the selected particle iterator
+    for (auto particle_iterator = particle_begin;
+         particle_iterator != particles_to_evaluate.end();
+         ++particle_iterator)
+      {
+        contact_pair_candidates_container.emplace_back(
+          particle_iterator->get_id());
+      }
+  }
 };
 
 #endif /* particle_particle_broad_search_h */
