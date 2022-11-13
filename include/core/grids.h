a4a7a4baeea30101663fe74bc743d8d9e1f68ea3
Bruno Blais <blais.bruno@gmail.com>
2022-11-13 17:13:00 -0500

    Unique mesh reader (#639)
    Description of the problem
    The DEM module contained a lot of code duplication from the CFD module. The entire part related to the attachment of the grid to the triangulation was fully duplicated. This was a total mess.
    
    Description of the solution
    Refactor the reader to at least use the same attachment mechanism
    I also took the opportunity to clean the interface to use references instead of pointers. This make the code more portable and easier to use everywhere.
    
    How Has This Been Tested?
    All tests pass without a change
    
    Comments
    Ongoing process to clean the DEM code
    Closes Move mesh reading in DEM to the core grid utilities #561
    
    Co-authored-by: Victor Oliveira Ferreira <65647639+voferreira@users.noreply.github.com>
    Former-commit-id: 96cfdf85a824ab87b0bee82d9ed5a9ac733f52c5

--- include/core/grids.h
+++ include/core/grids.h
@@ -41,9 +41,21 @@ using namespace dealii;
 template <int dim, int spacedim = dim>
 void
 attach_grid_to_triangulation(
-  std::shared_ptr<parallel::DistributedTriangulationBase<dim, spacedim>>
-                                                          triangulation,
-  const Parameters::Mesh &                                mesh_parameters,
+  parallel::DistributedTriangulationBase<dim, spacedim> &triangulation,
+  const Parameters::Mesh &                               mesh_parameters);
+
+
+/**
+ * @brief Modifies the triangulation to setup periodic boundary conditions in the case of CFD simulations
+ *
+ * @param triangulation The triangulation to which a grid is attached
+ *
+ * @param boundary_conditions The information about the boundary conditions id. This is used to set-up the periodicity of the domain
+ */
+template <int dim, int spacedim = dim>
+void
+setup_periodic_boundary_conditions(
+  parallel::DistributedTriangulationBase<dim, spacedim> & triangulation,
   const BoundaryConditions::BoundaryConditions<spacedim> &boundary_conditions);
 
 /**
@@ -60,8 +72,7 @@ attach_grid_to_triangulation(
 template <int dim, int spacedim = dim>
 void
 read_mesh_and_manifolds(
-  std::shared_ptr<parallel::DistributedTriangulationBase<dim, spacedim>>
-                                                          triangulation,
+  parallel::DistributedTriangulationBase<dim, spacedim> & triangulation,
   const Parameters::Mesh &                                mesh_parameters,
   const Parameters::Manifolds &                           manifolds_parameters,
   const bool &                                            restart,
