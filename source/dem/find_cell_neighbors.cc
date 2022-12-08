94d025ba13de62dca3b9f82679ddd2e50337216e
Bruno Blais <blais.bruno@gmail.com>
2022-12-08 00:31:03 -0500

    Fix indent

--- source/dem/find_cell_neighbors.cc
+++ source/dem/find_cell_neighbors.cc
@@ -122,7 +122,7 @@ FindCellNeighbors<dim>::find_cell_periodic_neighbors(
   typename DEM::dem_data_structures<dim>::cell_vector
     ghost_local_periodic_neighbor_vector;
 
-  typename DEM::dem_data_structures<dim>::cell_set       total_cell_list;
+  typename DEM::dem_data_structures<dim>::cell_set    total_cell_list;
   typename DEM::dem_data_structures<dim>::cell_vector total_ghost_cell_list;
 
   // For each cell, the cell vertices are found and used to find adjacent cells.
@@ -323,12 +323,11 @@ void
 FindCellNeighbors<dim>::get_periodic_neighbor_list(
   const typename Triangulation<dim>::active_cell_iterator &cell,
   const std::map<unsigned int, std::vector<unsigned int>>
-                                             &coinciding_vertex_groups,
+    &                                         coinciding_vertex_groups,
   const std::map<unsigned int, unsigned int> &vertex_to_coinciding_vertex_group,
   const std::vector<std::set<typename Triangulation<dim>::active_cell_iterator>>
-    &v_to_c,
-  typename DEM::dem_data_structures<dim>::cell_vector
-    &periodic_neighbor_list)
+    &                                                  v_to_c,
+  typename DEM::dem_data_structures<dim>::cell_vector &periodic_neighbor_list)
 {
   // Loop over vertices of the cell
   for (unsigned int vertex = 0; vertex < cell->n_vertices(); ++vertex)
