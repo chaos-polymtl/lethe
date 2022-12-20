ce2205e91239e4450936397023a6b9ad48683c2a
Lucka <luckabarbeau@gmail.com>
2022-12-20 10:38:19 -0500

    indent all

--- include/core/shape.h
+++ include/core/shape.h
@@ -26,10 +26,11 @@
 #include <deal.II/dofs/dof_handler.h>
 #include <deal.II/dofs/dof_tools.h>
 
-#include <deal.II/grid/manifold_lib.h>
 
 #include <deal.II/physics/transformations.h>
 #include <deal.II/grid/manifold_lib.h>
+
+#include <deal.II/physics/transformations.h>
 
 #include <cfloat>
 #include <memory>
