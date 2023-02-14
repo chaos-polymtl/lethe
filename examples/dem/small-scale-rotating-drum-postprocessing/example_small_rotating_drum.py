48a75504a3152092db3788daf28dfb2318397181
Victor Oliveira Ferreira <victor@LAPTOP-F2KIN6BL>
2023-02-14 12:30:43 -0300

    Comment on example python file

--- examples/dem/small-scale-rotating-drum-postprocessing/example_small_rotating_drum.py
+++ examples/dem/small-scale-rotating-drum-postprocessing/example_small_rotating_drum.py
@@ -1,30 +1,53 @@
 #Post-processing tool to track mixing in small rotating drum
 
-# importing sys
+# import sys
 import sys
 
-#Append path to the module to sys path list to be able to import it
-path_to_module = '../../../contrib/postprocessing/' #Folder where the module is
+#Path to the module
+path_to_module = '../../../contrib/postprocessing/'
 sys.path.append(path_to_module)
-
-print(sys.path)
 
 #Import tools from module
 from lethe_pyvista_tools import *
 
 
 #Create object named "particles"
-particles = lethe_pyvista_tools(case_path = ".", prm_file_name = "small-rotating-drum-dem.prm")
+particles = lethe_pyvista_tools(".", "small-rotating-drum-dem.prm")
 
 #Read files and store data to object named "particles"
 #The packing of particles is finished after 40th time step
 #So, it is important to set first to 40
-particles.read_lethe_to_pyvista(pvd_name = "out.pvd", first = 40)
+particles.read_lethe_to_pyvista("out.pvd", first = 40)
 
+
+#State condition for particle_color array creation
 condition = "(y**2 + z**2)**(1/2) > 0.025"
 
-particles.array_modifier(new_array_name = "particle_color", condition = condition, array_values = 1, restart_array = True, write_new_vtu = False)
+#Create array named particle_color and give the value of 1 to all particles within the given condition
+particles.array_modifier(array_name = "particle_color", condition = condition, array_values = 1, restart_array = True)
 
+
+#State a second condition to modify particle_color
 condition = "(y**2 + z**2)**(1/2) > 0.04"
 
-particles.array_modifier(new_array_name = "particle_color", condition = condition, array_values = 2, restart_array = False, write_new_vtu = True)
+#Modify the array to give the value of 2 to all particles withing the new condition
+particles.array_modifier(array_name = "particle_color", condition = condition, array_values = 2, restart_array = False)
+
+
+#VISUALIZE DATA USING PYVISTA:
+
+#Create a sphere with diameter 1
+sphere = pv.Sphere(theta_resolution=50, phi_resolution=50)
+
+#Use sphere as basis to create sheric representation of particles
+particle_glyph = particles.df[0].glyph(scale='Diameter', geom=sphere)
+
+#Create a plotter object
+plt = pv.Plotter()
+#Add particles to object and color them by particle_color array
+plt.add_mesh(particle_glyph, scalars = "particle_color")
+#Open plot window
+plt.show()
+
+#Save results to be able to open them on ParaView
+particles.write_vtu(prefix = "mod_")
