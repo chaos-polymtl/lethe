df4947b4d51c14385373011ac919428f5c82441b
Victor Oliveira Ferreira <65647639+voferreira@users.noreply.github.com>
2023-02-15 20:51:17 -0300

    Update contrib/postprocessing/example.py
    Co-authored-by: Audrey Collard-Daigneault <71884806+acdaigneault@users.noreply.github.com>

--- contrib/postprocessing/example.py
+++ contrib/postprocessing/example.py
@@ -48,15 +48,31 @@ example.sort_by_array("ID")
 
 #It is also possible to modify or create new arrays using
 example.array_modifier()
-#The arguments (and their default values) are:
-#reference_array_name = "ID" -> array to be used as reference to create or modify the other. All arrays will be sorted and written according to this one.
-#array_name = "new_array" -> name of the new array. If there is an array with the same name, it will be rewritten according to the other arguments.
-#restart_array = False -> if True, zeroes the entire array before modifying it. If you want to modify part of the array keeping the rest intact, set it to False
-#condition = "" -> takes a string and uses it in an if condition to modify the array. Variables accepted include x, y, z, u, v, w, t, and any other array. It also accepts a combination of them, such as: "x*w**2 + t > 2"
-#array_values = 0 -> new values to the array. This argument accepts a single value (which will be repeated to all data respecting the given condition), an numpy array or python list (with the same len of all other arrays), or a string such as "2*x + t" (working just like the condition argument)
-#standard_value = 0 -> if restart array is True, the standard_value will be the one plugged to the entire array before modifying it.
-#reference_time_step = 0 -> reference time step to which the modification will be applied. The others will follow this given one
-#time_dependent = False -> the modifier can be time dependent or not. If set True, the condition will be tested to each of the time-steps, while if it is False, it will be applied using the reference_time_step instead, and the modification will be just replicated to the other time steps
+# The arguments (and their default values) are:
+# reference_array_name = "ID"        -> array to be used as reference to create or modify the other. 
+#                                       All arrays will be sorted and written according to this one.
+# array_name           = "new_array" -> name of the new array. 
+#                                       If there is an array with the same name, it will be rewritten according to the 
+#                                       other arguments.
+# restart_array        = False       -> if True, zeroes the entire array before modifying it. 
+#                                       If you want to modify part of the array keeping the rest intact, set it to False
+# condition            = ""          -> takes a string and uses it in an if condition to modify the array. 
+#                                       Variables accepted include x, y, z, u, v, w, t, and any other array. 
+#                                       It also accepts a combination of them, such as: "x*w**2 + t > 2"
+# array_values         = 0           -> new values to the array. 
+#                                       This argument accepts a single value (which will be repeated to all data 
+#                                       respecting the given condition), an numpy array or python list (with the same 
+#                                       len of all other arrays), or a string such as "2*x + t" (working just like the 
+#                                       condition argument)
+# standard_value       = 0           -> if restart array is True, the standard_value will be the one plugged to the 
+#                                       entire array before modifying it.
+# reference_time_step  = 0           -> reference time step to which the modification will be applied. 
+#                                       The others will follow this given one
+# time_dependent       = False       -> the modifier can be time dependent or not. 
+#                                       If set True, the condition will be tested to each of the time-steps, while if 
+#                                       it is False, it will be applied using the reference_time_step instead, and the 
+#                                       modification will be just replicated to the other time steps
+
 
 #Other modifications can be made without using the array_modified function, using the same structure as in PyVista documentation (https://docs.pyvista.org/)
 #To write those, the following can be used:
