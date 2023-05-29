579711de08c6bc4ae5a13d89f24edc8309539718
Bruno Blais <blais.bruno@gmail.com>
2023-05-29 10:07:35 -0400

    WIP VOF stabilization

--- source/solvers/vof.cc
+++ source/solvers/vof.cc
@@ -100,8 +100,8 @@ template <int dim>
 void
 VolumeOfFluid<dim>::assemble_local_system_matrix(
   const typename DoFHandler<dim>::active_cell_iterator &cell,
-  VOFScratchData<dim> &                                 scratch_data,
-  StabilizedMethodsCopyData &                           copy_data)
+  VOFScratchData<dim>                                  &scratch_data,
+  StabilizedMethodsCopyData                            &copy_data)
 {
   copy_data.cell_is_local = cell->is_locally_owned();
   if (!cell->is_locally_owned())
@@ -152,10 +152,9 @@ VolumeOfFluid<dim>::copy_local_matrix_to_global_matrix(
   if (!copy_data.cell_is_local)
     return;
 
-  const AffineConstraints<double> &constraints_used = this->zero_constraints;
-  constraints_used.distribute_local_to_global(copy_data.local_matrix,
-                                              copy_data.local_dof_indices,
-                                              this->system_matrix);
+  this->zero_constraints.distribute_local_to_global(copy_data.local_matrix,
+                                                    copy_data.local_dof_indices,
+                                                    this->system_matrix);
 }
 
 
@@ -194,8 +193,8 @@ template <int dim>
 void
 VolumeOfFluid<dim>::assemble_local_system_rhs(
   const typename DoFHandler<dim>::active_cell_iterator &cell,
-  VOFScratchData<dim> &                                 scratch_data,
-  StabilizedMethodsCopyData &                           copy_data)
+  VOFScratchData<dim>                                  &scratch_data,
+  StabilizedMethodsCopyData                            &copy_data)
 {
   copy_data.cell_is_local = cell->is_locally_owned();
   if (!cell->is_locally_owned())
@@ -347,7 +346,7 @@ template <typename VectorType>
 std::pair<Tensor<1, dim>, Tensor<1, dim>>
 VolumeOfFluid<dim>::calculate_barycenter(
   const TrilinosWrappers::MPI::Vector &solution,
-  const VectorType &                   solution_fd)
+  const VectorType                    &solution_fd)
 {
   const MPI_Comm mpi_communicator = this->triangulation->get_communicator();
 
@@ -440,13 +439,13 @@ VolumeOfFluid<3>::calculate_barycenter<TrilinosWrappers::MPI::Vector>(
 
 template std::pair<Tensor<1, 2>, Tensor<1, 2>>
 VolumeOfFluid<2>::calculate_barycenter<TrilinosWrappers::MPI::BlockVector>(
-  const TrilinosWrappers::MPI::Vector &     solution,
+  const TrilinosWrappers::MPI::Vector      &solution,
   const TrilinosWrappers::MPI::BlockVector &current_solution_fd);
 
 
 template std::pair<Tensor<1, 3>, Tensor<1, 3>>
 VolumeOfFluid<3>::calculate_barycenter<TrilinosWrappers::MPI::BlockVector>(
-  const TrilinosWrappers::MPI::Vector &     solution,
+  const TrilinosWrappers::MPI::Vector      &solution,
   const TrilinosWrappers::MPI::BlockVector &current_solution_fd);
 
 
@@ -492,8 +491,7 @@ VolumeOfFluid<dim>::calculate_volume_and_mass(
             {
               switch (monitored_fluid)
                 {
-                  case Parameters::FluidIndicator::fluid0:
-                    {
+                    case Parameters::FluidIndicator::fluid0: {
                       this->volume_monitored +=
                         fe_values_vof.JxW(q) * (1 - phase_values[q]);
                       this->mass_monitored += fe_values_vof.JxW(q) *
@@ -501,8 +499,7 @@ VolumeOfFluid<dim>::calculate_volume_and_mass(
                                               density_0[q];
                       break;
                     }
-                  case Parameters::FluidIndicator::fluid1:
-                    {
+                    case Parameters::FluidIndicator::fluid1: {
                       this->volume_monitored +=
                         fe_values_vof.JxW(q) * phase_values[q];
                       this->mass_monitored +=
@@ -528,7 +525,7 @@ template <typename VectorType>
 double
 VolumeOfFluid<dim>::find_monitored_fluid_avg_pressure(
   const TrilinosWrappers::MPI::Vector &solution,
-  const VectorType &                   current_solution_fd,
+  const VectorType                    &current_solution_fd,
   const Parameters::FluidIndicator     monitored_fluid)
 {
   QGauss<dim>    quadrature_formula(this->cell_quadrature->size());
@@ -613,14 +610,14 @@ VolumeOfFluid<3>::find_monitored_fluid_avg_pressure<
 template double
 VolumeOfFluid<2>::find_monitored_fluid_avg_pressure<
   TrilinosWrappers::MPI::BlockVector>(
-  const TrilinosWrappers::MPI::Vector &     solution,
+  const TrilinosWrappers::MPI::Vector      &solution,
   const TrilinosWrappers::MPI::BlockVector &current_solution_fd,
   const Parameters::FluidIndicator          monitored_fluid);
 
 template double
 VolumeOfFluid<3>::find_monitored_fluid_avg_pressure<
   TrilinosWrappers::MPI::BlockVector>(
-  const TrilinosWrappers::MPI::Vector &     solution,
+  const TrilinosWrappers::MPI::Vector      &solution,
   const TrilinosWrappers::MPI::BlockVector &current_solution_fd,
   const Parameters::FluidIndicator          monitored_fluid);
 
@@ -1024,9 +1021,8 @@ VolumeOfFluid<dim>::find_sharpening_threshold()
           st_min             = st_avg;
           mass_deviation_min = mass_deviation_avg;
         }
-    }
-  while (std::abs(mass_deviation_avg) > mass_deviation_tol &&
-         nb_search_ite < max_iterations);
+  } while (std::abs(mass_deviation_avg) > mass_deviation_tol &&
+           nb_search_ite < max_iterations);
 
   // Take minimum deviation in between the two endpoints of the last
   // interval searched, if out of the do-while loop because max_iterations is
@@ -2002,10 +1998,9 @@ VolumeOfFluid<dim>::setup_dofs()
 
   // Sparse matrices initialization
   DynamicSparsityPattern dsp(this->dof_handler.n_dofs());
-  DoFTools::make_sparsity_pattern(this->dof_handler,
-                                  dsp,
-                                  this->nonzero_constraints,
-                                  /*keep_constrained_dofs = */ true);
+  DoFTools::make_sparsity_pattern(
+    this->dof_handler, dsp, this->nonzero_constraints, false
+    /*keep_constrained_dofs =  true*/);
 
   SparsityTools::distribute_sparsity_pattern(dsp,
                                              this->locally_owned_dofs,
@@ -2836,9 +2831,9 @@ VolumeOfFluid<3>::apply_peeling_wetting<TrilinosWrappers::MPI::BlockVector>(
 template <int dim>
 void
 VolumeOfFluid<dim>::change_cell_phase(
-  const PhaseChange &                         type,
-  const double &                              new_phase,
-  TrilinosWrappers::MPI::Vector &             solution_pw,
+  const PhaseChange                          &type,
+  const double                               &new_phase,
+  TrilinosWrappers::MPI::Vector              &solution_pw,
   const std::vector<types::global_dof_index> &dof_indices_vof)
 {
   if (type == PhaseChange::wetting)
