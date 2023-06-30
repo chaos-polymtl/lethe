648d98d3f756ed8ef8b93ddd3ea2a24493a9d5f0
Amishga Alphonius <amishga1999@gmail.com>
2023-06-30 17:34:23 -0400

    Remove unused pressure scaling variable

--- include/core/dimensionality.h
+++ include/core/dimensionality.h
@@ -95,7 +95,6 @@ namespace Parameters
      * everywhere.
      */
     double density_scaling;
-    double pressure_scaling;
     double specific_gas_constant_scaling;
     double viscosity_scaling;
     double specific_heat_scaling;
