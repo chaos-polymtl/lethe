1c982c84d71b33c99d7f404259faf7aceb2a7490
OGaboriault <102625061+OGaboriault@users.noreply.github.com>
2023-07-06 10:48:42 -0400

    Augment the maximum number of solid object to 10 (#788)

--- include/core/solid_objects_parameters.h
+++ include/core/solid_objects_parameters.h
@@ -375,7 +375,7 @@ namespace Parameters
     // DEM solid objects
     std::vector<std::shared_ptr<RigidSolidObject<dim>>> solids;
     unsigned int                                        number_solids;
-    static const unsigned int max_number_of_solids = 5;
+    static const unsigned int max_number_of_solids = 10;
   };
 
   template <int dim>
