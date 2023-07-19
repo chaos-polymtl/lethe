de034346e8093386cdbd03af80b34a441d0d3cf6
OGaboriault <oliviergabo@gmail.com>
2023-07-19 15:43:15 -0400

    Better broad-search for solid-object

--- source/dem/dem.cc
+++ source/dem/dem.cc
@@ -1123,7 +1123,7 @@ DEMSolver<dim>::solve()
   // Find the smallest cell size and use this as the floating mesh mapping
   // criterion
   smallest_floating_mesh_mapping_criterion =
-    GridTools::minimal_cell_diameter(triangulation);
+    0.5 * GridTools::minimal_cell_diameter(triangulation);
 
   if (has_periodic_boundaries)
     {
