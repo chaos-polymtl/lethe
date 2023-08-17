4b845ac95b20f609bc54cf0033482efd5368a363
Bruno Blais <blais.bruno@gmail.com>
2023-08-17 07:13:40 -0400

    Add timer per physics (#849)
    Description of the problem
    Only the Fluid Dynamics physics had a timer
    
    Description of the solution
    Add one individual timer per physics
    
    How Has This Been Tested?
    tested on a few examples and it works well
    
    Co-authored-by: Amishga Alphonius <107414376+AmishgaAlphonius@users.noreply.github.com>

--- include/solvers/heat_transfer.h
+++ include/solvers/heat_transfer.h
@@ -35,6 +35,7 @@
 
 #include <deal.II/base/convergence_table.h>
 #include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/timer.h>
 
 #include <deal.II/distributed/solution_transfer.h>
 #include <deal.II/distributed/tria_base.h>
@@ -46,6 +47,7 @@
 
 #include <deal.II/lac/trilinos_sparse_matrix.h>
 #include <deal.II/lac/trilinos_vector.h>
+
 
 
 template <int dim>
@@ -60,6 +62,10 @@ public:
     : AuxiliaryPhysics<dim, TrilinosWrappers::MPI::Vector>(
         p_simulation_parameters.non_linear_solver)
     , multiphysics(multiphysics_interface)
+    , computing_timer(p_triangulation->get_communicator(),
+                      this->pcout,
+                      TimerOutput::summary,
+                      TimerOutput::wall_times)
     , simulation_parameters(p_simulation_parameters)
     , triangulation(p_triangulation)
     , simulation_control(p_simulation_control)
@@ -105,6 +111,11 @@ public:
             SolutionTransfer<dim, TrilinosWrappers::MPI::Vector>(
               this->dof_handler));
       }
+
+    // Change the behavior of the timer for situations when you don't want
+    // outputs
+    if (simulation_parameters.timer.type == Parameters::Timer::Type::none)
+      this->computing_timer.disable_output();
   }
 
   /**
@@ -466,7 +477,10 @@ private:
                         const double                     phase_value_q);
 
 
-  MultiphysicsInterface<dim> *     multiphysics;
+  MultiphysicsInterface<dim> *multiphysics;
+
+  TimerOutput computing_timer;
+
   const SimulationParameters<dim> &simulation_parameters;
 
 
