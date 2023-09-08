0b08c9cc407cd9f7fd99fefde5120c392bdd02bd
hepap <helene.papillon-laroche@polymtl.ca>
2023-09-08 09:14:33 -0400

    Add SurfaceTensionPhaseChange class

--- include/core/surface_tension_model.h
+++ include/core/surface_tension_model.h
@@ -18,6 +18,7 @@
 #define lethe_surface_tension_model_h
 
 #include <core/interface_property_model.h>
+#include <core/phase_change.h>
 
 /**
  * @brief SurfaceTensionModel. Abstract class that allows to calculate the
@@ -186,6 +187,139 @@ public:
     for (unsigned int i = 0; i < property_vector.size(); ++i)
       property_vector[i] = surface_tension_coefficient +
                            surface_tension_gradient * (temperature[i] - T_0);
+  }
+
+  /**
+   * @brief jacobian Calculates the jacobian (the partial derivative) of the
+   * surface tension coefficient with respect to a field
+   * @param field_values Value of the various fields on which the property may
+   * depend.
+   * @param id Indicator of the field with respect to which the jacobian
+   * should be calculated.
+   * @return value of the partial derivative of the surface tension coefficient
+   * with respect to the field.
+   */
+  double
+  jacobian(const std::map<field, double> & /*field_values*/, field id) override
+  {
+    if (id == field::temperature)
+      return surface_tension_gradient;
+    else
+      return 0;
+  }
+
+  /**
+   * @brief vector_jacobian Calculates the derivative of the surface tension
+   * coefficient with respect to a field.
+   * @param field_vectors Vector for the values of the fields used to evaluate
+   * the property.
+   * @param id Identifier of the field with respect to which a derivative should
+   * be calculated.
+   * @param jacobian vector of the value of the derivative of the surface
+   * tension coefficient with respect to the field id.
+   */
+  void
+  vector_jacobian(
+    const std::map<field, std::vector<double>> & /*field_vectors*/,
+    const field          id,
+    std::vector<double> &jacobian_vector) override
+  {
+    if (id == field::temperature)
+      std::fill(jacobian_vector.begin(),
+                jacobian_vector.end(),
+                surface_tension_gradient);
+    else
+      std::fill(jacobian_vector.begin(), jacobian_vector.end(), 0);
+  }
+
+private:
+  const double surface_tension_coefficient;
+  const double T_0;
+  const double surface_tension_gradient;
+};
+
+/**
+ * @brief Linear surface tension. The surface tension is given by:
+ * sigma_0 + dsimga/dT * (T-T_0).
+ */
+class SurfaceTensionPhaseChange : public SurfaceTensionLinear
+{
+public:
+  /**
+   * @brief Default constructor
+   */
+  SurfaceTensionPhaseChange(
+    const Parameters::SurfaceTensionParameters &p_surface_tension_parameters,
+    const Parameters::PhaseChange phase_change_params)
+    : surface_tension_coefficient(
+        p_surface_tension_parameters.surface_tension_coefficient)
+    , T_0(p_surface_tension_parameters.T_0)
+    , surface_tension_gradient(
+        p_surface_tension_parameters.surface_tension_gradient)
+    , p_phase_change_params(phase_change_params)
+    , T_solidus(phase_change_params.T_solidus)
+    , T_liquidus(phase_change_params.T_liquidus)
+  {
+    this->model_depends_on[field::temperature] = true;
+  }
+
+  /**
+   * @brief value Calculates the surface tension coefficient
+   * @param field_vectors Vectors of the fields on which the surface tension
+   * depends. In this case, it depends on the temperature.
+   * @return value of the physical property calculated with the fields_value
+   */
+  double
+  value(const std::map<field, double> &fields_value) override
+  {
+    const double temperature = fields_value.at(field::temperature);
+
+    double surface_tension;
+
+    if (fields_value.at(field::temperature) < T_solidus)
+      surface_tension = 0.0;
+    else if (fields_value.at(field::temperature) > T_liquidus)
+      surface_tension = surface_tension_coefficient +
+             surface_tension_gradient * (temperature - T_0);
+    else
+      {
+        const double l_frac =
+          calculate_liquid_fraction(fields_value.at(field::temperature),
+                                    p_phase_change_params);
+
+        surface_tension = (surface_tension_coefficient +
+               surface_tension_gradient * (temperature - T_0)) * l_frac;
+      }
+    return surface_tension;
+  }
+
+  /**
+   * @brief vector_value Calculates the vector of surface tension coefficient.
+   * @param field_vectors Vectors of the fields on which the surface tension
+   * depends. In this case, it depends on the temperature.
+   * @param property_vector Vectors of the surface tension coefficient values
+   */
+  void
+  vector_value(const std::map<field, std::vector<double>> &field_vectors,
+               std::vector<double> &property_vector) override
+  {
+    const std::vector<double> &temperature =
+      field_vectors.at(field::temperature);
+    for (unsigned int i = 0; i < property_vector.size(); ++i)
+      if (fields_value.at(field::temperature) < T_solidus)
+        property_vector[i] = 0.0;
+      else if (fields_value.at(field::temperature) > T_liquidus)
+        property_vector[i] = surface_tension_coefficient +
+               surface_tension_gradient * (temperature - T_0);
+      else
+        {
+          const double l_frac =
+            calculate_liquid_fraction(fields_value.at(field::temperature),
+                                      p_phase_change_params);
+
+          property_vector[i] = (surface_tension_coefficient +
+                 surface_tension_gradient * (temperature - T_0)) * l_frac;
+        }
   }
 
   /**
