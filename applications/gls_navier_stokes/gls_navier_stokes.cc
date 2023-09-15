4b4ff6f7485cdaed81f079c5b1a7d55e99460766
Bruno Blais <blais.bruno@gmail.com>
2023-09-15 07:29:12 -0400

    Fix error I added

--- applications/gls_navier_stokes/gls_navier_stokes.cc
+++ applications/gls_navier_stokes/gls_navier_stokes.cc
@@ -1,26 +1,26 @@
-i /* ---------------------------------------------------------------------
-  *
-  * Copyright (C) 2019 - by the Lethe authors
-  *
-  * This file is part of the Lethe library
-  *
-  * The Lethe library is free software; you can use it, redistribute
-  * it, and/or modify it under the terms of the GNU Lesser General
-  * Public License as published by the Free Software Foundation; either
-  * version 3.1 of the License, or (at your option) any later version.
-  * The full text of the license can be found in the file LICENSE at
-  * the top level of the Lethe distribution.
-  *
-  * ---------------------------------------------------------------------
-
+/* ---------------------------------------------------------------------
  *
- * Author: Bruno Blais, Polytechnique Montreal, 2019-
- */
+ * Copyright (C) 2019 - by the Lethe authors
+ *
+ * This file is part of the Lethe library
+ *
+ * The Lethe library is free software; you can use it, redistribute
+ * it, and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either
+ * version 3.1 of the License, or (at your option) any later version.
+ * The full text of the license can be found in the file LICENSE at
+ * the top level of the Lethe distribution.
+ *
+ * ---------------------------------------------------------------------
+
+*
+* Author: Bruno Blais, Polytechnique Montreal, 2019-
+*/
 
 #include "solvers/gls_navier_stokes.h"
 
-  int
-  main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
   try
     {
