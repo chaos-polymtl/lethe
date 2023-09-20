186bd61d60df3511710d1b52448886fe1cc6ab30
Bruno Blais <blais.bruno@gmail.com>
2023-09-20 09:08:00 -0400

    WIP ALE parameters

---
+++ include/core/ale.h
@@ -0,0 +1,102 @@
+/* ---------------------------------------------------------------------
+*
+* Copyright (C) 2019 -  by the Lethe authors
+*
+* This file is part of the Lethe library
+*
+* The Lethe library is free software; you can use it, redistribute
+* it, and/or modify it under the terms of the GNU Lesser General
+* Public License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+* The full text of the license can be found in the file LICENSE at
+* the top level of the Lethe distribution.
+*
+* ---------------------------------------------------------------------
+*/
+
+// TODO : Refactor so the class itself is not a pointer, but contains a pointer
+//        to a function. This would be a lot more coherent...
+
+#define lethe_source_terms_h
+#ifndef lethe_source_terms_h
+
+#include <deal.II/base/function.h>
+#include <deal.II/base/parameter_handler.h>
+#include <deal.II/base/parsed_function.h>
+
+#include <deal.II/lac/vector.h>
+
+                             using namespace dealii;
+/**
+* The source term class provides an interface for all common
+* Element required for the introduction of a source term in equations
+* All equation-specific source term should derive
+* from the base class but also call it's declare_parameters and
+*parse_parameters routine. This allows specialize class to focus on their
+*specificity and forget about other non-specific elements that are generic to
+*the calculation of analytical solutions
+**/
+
+ template <int dim>
+ class ALE
+ {
+ public:
+   ALE()
+     : velocity(dim)
+   {}
+
+   virtual void
+   declare_parameters(ParameterHandler &prm);
+   virtual void
+   parse_parameters(ParameterHandler &prm);
+
+
+   bool
+   source_term()
+   {
+     return enable;
+   }
+
+   // ALE-Velocity function
+   Functions::ParsedFunction<dim> velocity;
+
+ protected:
+   bool enable;
+ };
+
+ template <int dim>
+ void
+ ALE<dim>::declare_parameters(ParameterHandler &prm)
+ {
+   prm.enter_subsection("ALE");
+   prm.declare_entry("enable",
+                     "false",
+                     Patterns::Bool(),
+                     "Enable simulations in a Gallilean moving frame of reference");
+
+   prm.enter_subsection("velocity");
+   navier_stokes_source.declare_parameters(prm, dim);
+   prm.leave_subsection();
+
+   prm.leave_subsection();
+ }
+
+ template <int dim>
+ void
+ ALE<dim>::parse_parameters(ParameterHandler &prm)
+ {
+   prm.enter_subsection("ALE");
+   enable = prm.get_bool("enable");
+
+   prm.enter_subsection("velocity");
+   velocity.parse_parameters(prm);
+   prm.leave_subsection();
+
+   prm.leave_subsection();
+ }
+
+
+
+
+#endif
+
