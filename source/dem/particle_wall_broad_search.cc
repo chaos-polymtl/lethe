90cb068953a49f708942b084e215326f1c69733b
Olivier Guévremont <guevremont.o@gmail.com>
2023-10-04 18:46:16 -0400

    Attempt to fix 64bit compilation 3

--- source/dem/particle_wall_broad_search.cc
+++ source/dem/particle_wall_broad_search.cc
@@ -279,7 +279,7 @@ template <int dim>
 void
 ParticleWallBroadSearch<dim>::find_particle_floating_wall_contact_pairs(
   const std::unordered_map<
-    unsigned int,
+    types::global_dof_index,
     std::set<typename Triangulation<dim>::active_cell_iterator>>
                                         &boundary_cells_for_floating_walls,
   const Particles::ParticleHandler<dim> &particle_handler,
