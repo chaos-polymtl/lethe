ddf7844440c7a9bb3f24d10ed78022c614fe46f7
OGaboriault <oliviergabo@gmail.com>
2023-11-21 10:20:00 -0500

    Change de name of the string in the parsing function to be more consistent with the parsing of other parameters

--- source/core/parameters_lagrangian.cc
+++ source/core/parameters_lagrangian.cc
@@ -65,14 +65,15 @@ namespace Parameters
       const unsigned int &particle_type,
       ParameterHandler   &prm)
     {
-      const std::string size_distribution = prm.get("size distribution type");
-      if (size_distribution == "uniform")
+      const std::string size_distribution_type_str =
+        prm.get("size distribution type");
+      if (size_distribution_type_str == "uniform")
         {
           size_distribution_type = size_distribution_type::uniform;
           particle_average_diameter.at(particle_type) =
             prm.get_double("diameter");
         }
-      else if (size_distribution == "normal")
+      else if (size_distribution_type_str == "normal")
         {
           size_distribution_type = size_distribution_type::normal;
           particle_average_diameter.at(particle_type) =
