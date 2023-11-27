b43c5615349da4a5fc0328fe97acf19078642c0e
OGaboriault <102625061+OGaboriault@users.noreply.github.com>
2023-11-27 10:59:18 -0500

    Bugfix, dem fixing critical rayleigh time step (#939)
    Description of the problem
    The inspection of the DEM time step was wrong. It should be searching for the more restrictive rayleigh time, that is the minimum rayleigh time, not the maximum.
    Description of the solution
    The rayleigh time step is initialize to infinity.
    We use std::min instead of std::max.
    How Has This Been Tested?
    One test has been updated.
    All the other test are still succeeding
    Documentation
    N/A

--- source/dem/input_parameter_inspection.cc
+++ source/dem/input_parameter_inspection.cc
@@ -12,14 +12,14 @@ input_parameter_inspection(const DEMSolverParameters<dim> &dem_parameters,
   // Getting the input parameters as local variable
   auto   parameters          = dem_parameters;
   auto   physical_properties = dem_parameters.lagrangian_physical_properties;
-  double rayleigh_time_step  = 0;
+  double rayleigh_time_step  = 1. / DBL_MIN;
 
   for (unsigned int i = 0; i < physical_properties.particle_type_number; ++i)
     {
       double shear_modulus =
         physical_properties.youngs_modulus_particle[i] /
         (2.0 * (1.0 + physical_properties.poisson_ratio_particle[i]));
-      rayleigh_time_step = std::max(
+      rayleigh_time_step = std::min(
         M_PI_2 * physical_properties.particle_average_diameter[i] *
           sqrt(physical_properties.density_particle[i] / shear_modulus) /
           (0.1631 * physical_properties.poisson_ratio_particle[i] + 0.8766),
