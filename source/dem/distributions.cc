480713885bee17f02646dda0f72a21a0d81e05eb
OGaboriault <oliviergabo@gmail.com>
2023-12-08 15:14:03 -0500

    Everything works

--- source/dem/distributions.cc
+++ source/dem/distributions.cc
@@ -1,3 +1,5 @@
+#include <core/utilities.h>
+
 #include <dem/distributions.h>
 
 #include <algorithm>
@@ -71,14 +73,27 @@ UniformDistribution::find_max_diameter()
 CustomDistribution::CustomDistribution(
   const std::vector<double> &d_list,
   const std::vector<double> &d_probabilities)
-  : diameter_custom_cumm_prob(d_list)
+  : diameter_custom_values(d_list)
 {
-  std::vector<double> cumulative_probability_vector;
-  cumulative_probability_vector.reserve(d_probabilities.size());
+  std::vector<double> cumulative_probability_vector, n_i_vector;
+  double              n_tot = 0.;
 
-  double cumulative_value;
-  for (double i_probability : d_probabilities)
+  cumulative_probability_vector.reserve(d_probabilities.size());
+  n_i_vector.reserve(d_probabilities.size());
+
+  for (unsigned int i = 0; i < diameter_custom_values.size(); ++i)
     {
+      n_i_vector.push_back(d_probabilities[i] / Utilities::fixed_power<3>(
+                                                  diameter_custom_values[i]));
+      n_tot += n_i_vector[i];
+    }
+
+  double i_probability;
+  double cumulative_value = 0.;
+
+  for (unsigned int i = 0; i < diameter_custom_values.size(); ++i)
+    {
+      i_probability = n_i_vector[i] / n_tot;
       cumulative_value += i_probability;
       cumulative_probability_vector.push_back(cumulative_value);
     }
