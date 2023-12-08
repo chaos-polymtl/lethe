0b92f981b2fe90d50b088d798ed9acf7f63d479e
OGaboriault <oliviergabo@gmail.com>
2023-12-08 15:28:50 -0500

    Added runtime_error with the right bool operator...

--- source/core/parameters_lagrangian.cc
+++ source/core/parameters_lagrangian.cc
@@ -83,7 +83,7 @@ namespace Parameters
                     particle_custom_probability.at(particle_type).end());
 
       // We make sure that the cumulative probability is equal to 1.
-      if (std::abs(probability_sum - 1.0) < 1.e-14)
+      if (std::abs(probability_sum - 1.0) > 1.e-12)
         {
           throw(std::runtime_error(
             "Invalid custom probabilities. The sum of probabilities should be equal to 1.0 "));
