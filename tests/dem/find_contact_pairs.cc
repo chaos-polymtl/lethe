2a09bc44864f2b92ce8d5f3cfa4e6c1bcce13b2e
Peter Munch <peterrmuench@gmail.com>
2024-01-08 04:24:17 +0100

    Be explicit regarding namespace (#981)

--- tests/dem/find_contact_pairs.cc
+++ tests/dem/find_contact_pairs.cc
@@ -148,7 +148,7 @@ main(int argc, char **argv)
     {
       initlog();
       Utilities::MPI::MPI_InitFinalize mpi_initialization(
-        argc, argv, numbers::invalid_unsigned_int);
+        argc, argv, dealii::numbers::invalid_unsigned_int);
       test<3>();
     }
   catch (std::exception &exc)
