bf38db3a392cfb111399f9242432f961808932da
Olivier Guévremont <guevremont.o@gmail.com>
2024-02-09 21:41:05 -0500

    Doxygen ib stencil (#1025)
    Description of the problem
    Some comments needed to be added or completed for Doxygen.
    Description of the solution
    It is done for IBStencil class.
    Documentation
    Yes
    
    
    Co-authored-by: Bruno Blais <blais.bruno@gmail.com>

--- include/core/ib_stencil.h
+++ include/core/ib_stencil.h
@@ -29,38 +29,52 @@
 
 using namespace dealii;
 
-
+/**
+ * @brief Define where the sharp immersed-boundaries are imposed on Eulerian grid.
+ *
+ * @tparam dim An integer that denotes the dimension of the space in which
+ * the flow is solved.
+ */
 template <int dim>
 class IBStencil
 {
 public:
   /**
-  * @brief
-  Return the number of points used by the interpolation stencil excluding the
-  dof itself for a stencil of a given order.
-  *
-  * @param order, the stencil order.
-  */
+   * @brief Return the number of points used by the interpolation stencil (excluding the
+   *  dof itself) for a stencil of a given order.
+   *
+   * @param[in] order Stencil order.
+   *
+   * @return Number of points used by the interpolation stencil.
+   */
   virtual unsigned int
   number_of_interpolation_support_points(const unsigned int order);
 
   /**
-   * @brief
+   * @brief Find the DOF at which we try to apply the IB by interpolation
+   * and the location of the support points to do so.
+   *
    * Define the points for the IB stencil, based on the order and the particle
-   * position as well as the DOF position Depending on the order, the output
+   * position as well as the DOF position. Depending on the order, the output
    * variable "point" change definition. In the case of stencil orders 1 to 4
    * the variable point returns the position of the DOF directly. In the case of
    * high order stencil, it returns the position of the point that is on the IB.
    * The variable "interpolation points" return the points used to define the
-   * cell used for the stencil definition and the locations of the points use in
-   * the stencil calculation.
+   * cell used for the stencil definition and the locations of the points used
+   * in the stencil calculation.
    *
-   * @param order, the stencil order.
-   * @param length_ratio the length ratio between the 2 side of the stencil.
-   * @param p, the IB particle that cuts the cell.
-   * @param dof_point, the support point of the DOF.
-   *  @param cell_guess A guess of the cell containing the evaluation point, which
-   *  is useful to reduce computation time
+   * @param[in] order Stencil order.
+   *
+   * @param[in] length_ratio Length ratio between the 2 side of the stencil.
+   *
+   * @param[in] p IB particle that cuts the cell.
+   *
+   * @param[in] dof_point Support point of the DOF.
+   *
+   * @param[in] cell_guess Guess of the cell containing the evaluation point,
+   * which is useful to reduce computation time.
+   *
+   * @return Tuple containing (point, interpolation_points).
    */
   virtual std::tuple<Point<dim>, std::vector<Point<dim>>>
   support_points_for_interpolation(
@@ -71,7 +85,7 @@ public:
     const typename DoFHandler<dim>::active_cell_iterator &cell_guess);
 
   /**
-   * See overloaded function
+   * @brief See overloaded function.
    */
   virtual std::tuple<Point<dim>, std::vector<Point<dim>>>
   support_points_for_interpolation(const unsigned int order,
@@ -80,13 +94,14 @@ public:
                                    const Point<dim>  &dof_point);
 
   /**
-   * @brief
-   * Define the point used to define the cell used for the stencil calculation.
+   * @brief Define the point used to define the cell used for the stencil calculation.
    *
-   * @param p, the IB particle that cuts the cell.
-   * @param dof_point, the support point of the DOF.
-   *  @param cell_guess A guess of the cell containing the evaluation point, which
-   *  is useful to reduce computation time
+   * @param[in] p IB particle that cuts the cell.
+   *
+   * @param[in] dof_point support point of the DOF.
+   *
+   * @param[in] cell_guess Guess of the cell containing the evaluation point,
+   * which is useful to reduce computation time.
    */
   virtual Point<dim>
   point_for_cell_detection(
@@ -95,35 +110,38 @@ public:
     const typename DoFHandler<dim>::active_cell_iterator &cell_guess);
 
   /**
-   * See overloaded function
+   * @brief See overloaded function.
    */
   virtual Point<dim>
   point_for_cell_detection(IBParticle<dim> &p, const Point<dim> &dof_point);
 
   /**
-   * @brief
-   * Return the coefficient of the stencil based on the order.
-   * The coefficients of the IB stencil assume a ratio of length between the
-   * distance of the farthest interpolation point and the DOF and the distance
-   * between the IB and the DOF of 1/8. The coefficient order goes from the
-   * coefficient of the DOF to the coefficient of the farthest interpolation
-   * point.
+   * @brief Return the coefficients (weights) of the stencil.
    *
-   * @param order, the stencil order.
-   * @param length_ratio the length ratio between the 2 side of the stencil.
+   * The coefficients of the IB stencil assume a length between the
+   * distance of the farthest interpolation point to the DOF, and the distance
+   * between the IB and the DOF of 1/8. The coefficients are defined from the
+   * coefficient of the DOF to the coefficient of the farthest interpolation
+   * point (sorted by increasing distance).
+   *
+   * @param[in] order Stencil order.
+   *
+   * @param[in] length_ratio Length ratio between the 2 side of the stencil.
    */
   virtual std::vector<double>
   coefficients(const unsigned int order, const double length_ratio);
 
   /**
-   * @brief
-   * Return the velocity of the IB used in the RHS of the equation
+   * @brief Return the velocity of the IB used in the RHS of the equation.
    *
-   * @param p, the IB particle that cuts the cell.
-   * @param dof_point, the support point of the DOF.
-   * @param component, the stencil component of the dof (vx=0,vy=1,vz=2).
-   *  @param cell_guess A guess of the cell containing the evaluation point, which
-   *  is useful to reduce computation time
+   * @param[in] p IB particle that cuts the cell.
+   *
+   * @param[in] dof_point Support point of the DOF.
+   *
+   * @param[in] component Stencil component of the dof (vx=0,vy=1,vz=2).
+   *
+   * @param[in] cell_guess Guess of the cell containing the evaluation point,
+   * which is useful to reduce computation time.
    */
   virtual double
   ib_velocity(IBParticle<dim>                                      &p,
@@ -132,7 +150,7 @@ public:
               const typename DoFHandler<dim>::active_cell_iterator &cell_guess);
 
   /**
-   * See overloaded function
+   * @brief See overloaded function.
    */
   virtual double
   ib_velocity(IBParticle<dim>   &p,
@@ -141,16 +159,16 @@ public:
 
 private:
   /**
-   * @brief
-   * Return the points on the reference 1D element for the polynomial base
+   * @brief Return the points on the reference 1D element for the polynomial base.
    *
-   * @param order, the order of the stencil.
+   * @param[in] order Order of the stencil.
    */
-
   void
   p_base(unsigned int order);
 
-  // the points of the reference 1D stencil
+  /**
+   * @brief Points of the reference 1D stencil.
+   */
   std::vector<double> reference_points;
 };
 
