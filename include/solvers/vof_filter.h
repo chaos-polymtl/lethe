5c4a514d9bdf2b4b3e4f66742d85b988fe24f0d4
Amishga Alphonius <107414376+AmishgaAlphonius@users.noreply.github.com>
2024-02-11 12:17:08 -0500

    Update comments in vof_filter.h (#1029)
    Former-commit-id: 0fcebc5d6ec49ec19960a986e932a884b0768737

--- include/solvers/vof_filter.h
+++ include/solvers/vof_filter.h
@@ -21,46 +21,61 @@
 #include <core/parameters_multiphysics.h>
 
 /**
- * @brief Class that allows phase fraction filtering
+ * @brief Abstract class for filtering phase fraction in volume of fluid (VOF)
+ * simulations.
  */
 class VolumeOfFluidFilterBase
 {
 public:
-  VolumeOfFluidFilterBase()
-  {}
-
   /**
-   * @brief Instantiates and returns a pointer to a VolumeOfFluidFilterBase
-   * object by casting it to the proper child class
+   * @brief Instantiate and return a pointer to a VolumeOfFluidFilterBase
+   * object by casting it to the proper child class.
    *
-   * @param phase_filter_parameters VOF model parameters
+   * @param[in] phase_filter_parameters VOF filtration parameters.
+   *
+   * @return Casted VolumeOfFluidFilterBase child class object.
    */
   static std::shared_ptr<VolumeOfFluidFilterBase>
   model_cast(const Parameters::VOF_PhaseFilter &phase_filter_parameters);
 
   /**
-   * @brief filter_phase calculates the value of the filtered phase fraction
-   * @param unfiltered_phase Value of the phase fraction before applying the filter
-   * @return Value of the phase fraction after applying the filter
+   * @brief Compute the value of the filtered phase fraction.
+   *
+   * @param[in] unfiltered_phase Value of the phase fraction before applying the
+   * filter.
+   *
+   * @return Value of the computed phase fraction after applying the filter.
    */
   virtual double
   filter_phase(const double &unfiltered_phase) = 0;
 };
 
 /**
- * @brief Class that is used as a default when no
- * filter is applied to the phase fraction
+ * @brief Default filter applied to phase fraction. The VolumeOfFluidNoFilter
+ * filter does not modify the phase fraction value and returns it as it is.
  */
 class VolumeOfFluidNoFilter : public VolumeOfFluidFilterBase
 {
 public:
+  /**
+   * @brief Constructor of the transparent filter.
+   *
+   * No filter is applied to the computed filtered phase fraction and the
+   * unfiltered phase fraction is returned.
+   */
   VolumeOfFluidNoFilter()
   {}
 
   /**
-   * @brief filter_phase calculates the value of the filtered phase fraction
-   * @param unfiltered_phase Value of the phase fraction before applying the filter
-   * @return unfiltered_phase
+   * @brief Computes the value of the filtered phase fraction.
+   *
+   * Here, the filtered phase fraction value corresponds to the unfiltered one
+   * (@p unfiltered_phase).
+   *
+   * @param[in] unfiltered_phase Value of the phase fraction before applying the
+   * filter.
+   *
+   * @return Value of @p unfiltered_phase.
    */
   virtual double
   filter_phase(const double &unfiltered_phase) override
@@ -70,21 +85,39 @@ public:
 };
 
 /**
- * @brief Class used to calculate a filtered phase for VOF simulations.
- * The filtered phase is defined as
- * \f$\phi_f = 0.5 \tanh(\beta(\phi-0.5)) + 0.5\f$.
+ * @brief Filter phase fraction with a hyperbolic tangent function
+ * for VOF simulations.
+ *
+ * The filtered phase is defined as:
+ *
+ * \f$\phi' = 0.5 \tanh(\beta(\phi-0.5)) + 0.5\f$
+ *
+ * where \f$\phi'\f$ is the filtered phase fraction value and \f$\beta\f$
+ * is a model parameter that enables sharper definition when increased.
+ *
+ * The filter allows a sharper definition of the interface and clamps the value
+ * of the phase fraction between 0 and 1.
  */
 class VolumeOfFluidTanhFilter : public VolumeOfFluidFilterBase
 {
 public:
+  /**
+   * @brief Constructor of the hyperbolic tangent phase fraction filter.
+   *
+   * @param[in] beta Value of the \f$\beta\f$ parameter modulating the
+   * interface sharpness.
+   */
   VolumeOfFluidTanhFilter(const double beta)
     : beta(beta)
   {}
 
   /**
-   * @brief filter_phase calculates the value of the filtered phase fraction
-   * @param unfiltered_phase Value of the phase fraction before applying the filter
-   * @return Value of the phase fraction after applying the tanh filter
+   * @brief Computes the value of the filtered phase fraction.
+   *
+   * @param[in] unfiltered_phase Value of the phase fraction before applying the
+   * filter.
+   *
+   * @return Value of the computed phase fraction after applying the filter.
    */
   virtual double
   filter_phase(const double &unfiltered_phase) override
@@ -93,7 +126,11 @@ public:
   }
 
 private:
-  // User-defined parameter that influences the definition of the interface
+  /**
+   * User-defined parameter that influences the sharpness of the interface
+   * between fluids. A greater value leads to a sharper interface. The
+   * recommended value is \f$\beta=20\f$.
+   */
   const double beta;
 };
 
