chaos-polymtl/lethe

commit 38a056e13defc6e7b54cc90961af930db0801c16

Introduce alias GlobalVectorType (#1001)
Introduce an Alias for the vector type to enable porting to use deal.II parallel vectors.


Co-authored-by: Bruno Blais <blais.bruno@gmail.com>
Former-commit-id: c10170824f6dcf5dca82012da36679bc828ddddc

prototypes/matrix_based_advection_diffusion/matrix_based_advection_diffusion.cc
@@ -59,6 +59,8 @@
 #include <deal.II/numerics/solution_transfer.h>
 #include <deal.II/numerics/vector_tools.h>
 
+#include <core/vector.h>
+
 #include <fstream>
 #include <iostream>
 
@@ -455,7 +457,7 @@ private:
   void
   output_results(const unsigned int cycle) const;
 
-  using VectorType = TrilinosWrappers::MPI::Vector;
+  using VectorType = GlobalVectorType;
   using MatrixType = TrilinosWrappers::SparseMatrix;
 
   parallel::distributed::Triangulation<dim> triangulation;
@@ -1351,7 +1353,7 @@ MatrixBasedAdvectionDiffusion<dim, fe_degree>::compute_update()
 {
   TimerOutput::Scope t(computing_timer, "compute update");
 
-  TrilinosWrappers::MPI::Vector completely_distributed_solution(
+  GlobalVectorType completely_distributed_solution(
     locally_owned_dofs, mpi_communicator);
 
   SolverControl solver_control(200, 1.e-8 * system_rhs.l2_norm(), true, true);

prototypes/matrix_based_non_linear_poisson/matrix_based_non_linear_poisson.cc
@@ -59,6 +59,8 @@
 #include <deal.II/numerics/solution_transfer.h>
 #include <deal.II/numerics/vector_tools.h>
 
+#include <core/vector.h>
+
 #include <fstream>
 #include <iostream>
 
@@ -348,7 +350,7 @@ private:
   void
   output_results(const unsigned int cycle) const;
 
-  using VectorType = TrilinosWrappers::MPI::Vector;
+  using VectorType = GlobalVectorType;
   using MatrixType = TrilinosWrappers::SparseMatrix;
 
   parallel::distributed::Triangulation<dim> triangulation;
@@ -994,7 +996,7 @@ MatrixBasedPoissonProblem<dim, fe_degree>::compute_update()
 {
   TimerOutput::Scope t(computing_timer, "compute update");
 
-  TrilinosWrappers::MPI::Vector completely_distributed_solution(
+  GlobalVectorType completely_distributed_solution(
     locally_owned_dofs, mpi_communicator);
 
   SolverControl              solver_control(100, 1.e-12);

CMakeLists.txt
@@ -88,6 +88,12 @@ if(CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME)
   option(BUILD_PROTOTYPES "Build the prototype applications")
   mark_as_advanced(BUILD_PROTOTYPES)
 
+  option(LETHE_USE_LDV "Use dealii::LinearAlgebra::distributed::Vector")
+  mark_as_advanced(LETHE_USE_LDV)
+  if(LETHE_USE_LDV)
+    add_compile_definitions(LETHE_USE_LDV)
+  endif()
+
   include(GNUInstallDirs)
   set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
   list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES

include/core/solid_base.h
@@ -25,6 +25,7 @@
 // Lethe Includes
 #include <core/parameters.h>
 #include <core/solid_objects_parameters.h>
+#include <core/vector.h>
 
 // Dealii Includes
 
@@ -140,7 +141,7 @@ public:
   /**
    * @return the reference to the displacement vector
    */
-  TrilinosWrappers::MPI::Vector &
+  GlobalVectorType &
   get_displacement_vector();
 
   /**
@@ -255,8 +256,8 @@ private:
 
   DoFHandler<dim, spacedim>                displacement_dh;
   std::shared_ptr<FESystem<dim, spacedim>> displacement_fe;
-  TrilinosWrappers::MPI::Vector            displacement;
-  TrilinosWrappers::MPI::Vector            displacement_relevant;
+  GlobalVectorType                         displacement;
+  GlobalVectorType                         displacement_relevant;
 
 
   std::shared_ptr<Parameters::NitscheObject<spacedim>> &param;

include/core/vector.h
@@ -1,0 +1,39 @@
+/* ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2020 -  by the Lethe authors
+ *
+ * This file is part of the Lethe library
+ *
+ * The Lethe library is free software; you can use it, redistribute
+ * it, and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * The full text of the license can be found in the file LICENSE at
+ * the top level of the Lethe distribution.
+ *
+ * ---------------------------------------------------------------------
+
+ *
+ * Author: Bruno Blais, Polytechnique Montreal, 2020 -
+ */
+
+#ifndef lethe_vector_h
+#define lethe_vector_h
+
+#include <deal.II/lac/la_parallel_block_vector.h>
+#include <deal.II/lac/la_parallel_vector.h>
+#include <deal.II/lac/trilinos_parallel_block_vector.h>
+#include <deal.II/lac/trilinos_vector.h>
+
+// #define LETHE_USE_LDV is defined using the CMAKE flag -DLETHE_USE_LDV=TRUE
+
+#ifndef LETHE_USE_LDV
+using GlobalVectorType      = dealii::TrilinosWrappers::MPI::Vector;
+using GlobalBlockVectorType = dealii::TrilinosWrappers::MPI::BlockVector;
+#else
+using GlobalVectorType = dealii::LinearAlgebra::distributed::Vector<double>;
+using GlobalBlockVectorType =
+  dealii::LinearAlgebra::distributed::BlockVector<double>;
+#endif
+
+#endif

include/fem-dem/gls_sharp_navier_stokes.h
@@ -20,6 +20,7 @@
 #include <core/ib_particle.h>
 #include <core/ib_stencil.h>
 #include <core/shape.h>
+#include <core/vector.h>
 
 #include <solvers/gls_navier_stokes.h>
 
@@ -603,8 +604,8 @@ private:
   /*
    * These vectors are used to keep track of the DOFs that are overconstrained
    */
-  TrilinosWrappers::MPI::Vector local_dof_overconstrained;
-  TrilinosWrappers::MPI::Vector dof_overconstrained;
+  GlobalVectorType local_dof_overconstrained;
+  GlobalVectorType dof_overconstrained;
 
   std::map<typename DoFHandler<dim>::active_cell_iterator,
            std::tuple<bool, unsigned int>>

include/fem-dem/gls_vans.h
@@ -339,21 +339,21 @@ protected:
   IndexSet locally_relevant_dofs_voidfraction;
 
   // Solution of the void fraction at previous time steps
-  std::vector<TrilinosWrappers::MPI::Vector> previous_void_fraction;
+  std::vector<GlobalVectorType> previous_void_fraction;
 
-  TrilinosWrappers::MPI::Vector nodal_void_fraction_relevant;
-  TrilinosWrappers::MPI::Vector nodal_void_fraction_owned;
+  GlobalVectorType nodal_void_fraction_relevant;
+  GlobalVectorType nodal_void_fraction_owned;
 
   // Assemblers for the particle_fluid interactions
   std::vector<std::shared_ptr<ParticleFluidAssemblerBase<dim>>>
     particle_fluid_assemblers;
 
   TrilinosWrappers::SparseMatrix system_matrix_void_fraction;
-  TrilinosWrappers::MPI::Vector  system_rhs_void_fraction;
+  GlobalVectorType               system_rhs_void_fraction;
   TrilinosWrappers::SparseMatrix complete_system_matrix_void_fraction;
-  TrilinosWrappers::MPI::Vector  complete_system_rhs_void_fraction;
+  GlobalVectorType               complete_system_rhs_void_fraction;
   TrilinosWrappers::SparseMatrix mass_matrix;
-  TrilinosWrappers::MPI::Vector  diagonal_of_mass_matrix;
+  GlobalVectorType               diagonal_of_mass_matrix;
   IndexSet                       active_set;
 
   std::shared_ptr<TrilinosWrappers::PreconditionILU> ilu_preconditioner;

include/solvers/cahn_hilliard.h
@@ -31,6 +31,7 @@
 
 #include <core/bdf.h>
 #include <core/simulation_control.h>
+#include <core/vector.h>
 
 #include <solvers/auxiliary_physics.h>
 #include <solvers/cahn_hilliard_assemblers.h>
@@ -55,7 +56,7 @@
 
 
 template <int dim>
-class CahnHilliard : public AuxiliaryPhysics<dim, TrilinosWrappers::MPI::Vector>
+class CahnHilliard : public AuxiliaryPhysics<dim, GlobalVectorType>
 {
 public:
   CahnHilliard(MultiphysicsInterface<dim>      *multiphysics_interface,
@@ -63,7 +64,7 @@ public:
                std::shared_ptr<parallel::DistributedTriangulationBase<dim>>
                                                   p_triangulation,
                std::shared_ptr<SimulationControl> p_simulation_control)
-    : AuxiliaryPhysics<dim, TrilinosWrappers::MPI::Vector>(
+    : AuxiliaryPhysics<dim, GlobalVectorType>(
         p_simulation_parameters.non_linear_solver.at(PhysicsID::cahn_hilliard))
     , multiphysics(multiphysics_interface)
     , computing_timer(p_triangulation->get_communicator(),
@@ -130,10 +131,9 @@ public:
       }
 
     // Allocate solution transfer
-    solution_transfer =
-      std::make_shared<parallel::distributed::
-                         SolutionTransfer<dim, TrilinosWrappers::MPI::Vector>>(
-        dof_handler);
+    solution_transfer = std::make_shared<
+      parallel::distributed::SolutionTransfer<dim, GlobalVectorType>>(
+      dof_handler);
 
     // Set size of previous solutions using BDF schemes information
     previous_solutions.resize(maximum_number_of_previous_solutions());
@@ -143,9 +143,8 @@ public:
     for (unsigned int i = 0; i < previous_solutions.size(); ++i)
       {
         previous_solutions_transfer.emplace_back(
-          parallel::distributed::
-            SolutionTransfer<dim, TrilinosWrappers::MPI::Vector>(
-              this->dof_handler));
+          parallel::distributed::SolutionTransfer<dim, GlobalVectorType>(
+            this->dof_handler));
       }
 
     // Change the behavior of the timer for situations when you don't want
@@ -274,27 +273,27 @@ public:
    * NB : dof_handler and present_solution are passed to the multiphysics
    * interface at the end of the setup_dofs method
    */
-  TrilinosWrappers::MPI::Vector &
+  GlobalVectorType &
   get_evaluation_point() override
   {
     return evaluation_point;
   }
-  TrilinosWrappers::MPI::Vector &
+  GlobalVectorType &
   get_local_evaluation_point() override
   {
     return local_evaluation_point;
   }
-  TrilinosWrappers::MPI::Vector &
+  GlobalVectorType &
   get_newton_update() override
   {
     return newton_update;
   }
-  TrilinosWrappers::MPI::Vector &
+  GlobalVectorType &
   get_present_solution() override
   {
     return present_solution;
   }
-  TrilinosWrappers::MPI::Vector &
+  GlobalVectorType &
   get_system_rhs() override
   {
     return system_rhs;
@@ -405,8 +404,8 @@ private:
    */
   template <typename VectorType>
   std::pair<Tensor<1, dim>, Tensor<1, dim>>
-  calculate_barycenter(const TrilinosWrappers::MPI::Vector &solution,
-                       const VectorType &current_solution_fd);
+  calculate_barycenter(const GlobalVectorType &solution,
+                       const VectorType       &current_solution_fd);
 
 
   MultiphysicsInterface<dim> *multiphysics;
@@ -435,25 +434,24 @@ private:
   IndexSet locally_owned_dofs;
   IndexSet locally_relevant_dofs;
 
-  TrilinosWrappers::MPI::Vector  evaluation_point;
-  TrilinosWrappers::MPI::Vector  local_evaluation_point;
-  TrilinosWrappers::MPI::Vector  newton_update;
-  TrilinosWrappers::MPI::Vector  present_solution;
-  TrilinosWrappers::MPI::Vector  system_rhs;
+  GlobalVectorType               evaluation_point;
+  GlobalVectorType               local_evaluation_point;
+  GlobalVectorType               newton_update;
+  GlobalVectorType               present_solution;
+  GlobalVectorType               system_rhs;
   AffineConstraints<double>      nonzero_constraints;
   AffineConstraints<double>      zero_constraints;
   TrilinosWrappers::SparseMatrix system_matrix;
 
 
   // Previous solutions vectors
-  std::vector<TrilinosWrappers::MPI::Vector> previous_solutions;
+  std::vector<GlobalVectorType> previous_solutions;
 
   // Solution transfer classes
   std::shared_ptr<
-    parallel::distributed::SolutionTransfer<dim, TrilinosWrappers::MPI::Vector>>
+    parallel::distributed::SolutionTransfer<dim, GlobalVectorType>>
     solution_transfer;
-  std::vector<
-    parallel::distributed::SolutionTransfer<dim, TrilinosWrappers::MPI::Vector>>
+  std::vector<parallel::distributed::SolutionTransfer<dim, GlobalVectorType>>
     previous_solutions_transfer;
 
   // Assemblers for the matrix and rhs

commit 3d2ffd2eacd111fc8598eb0a79451f1171865db5

Remove global vector type from prototypes (#1063)
Description of the problem
The GlobalVectorType was introduced in two prototypes in PR #1001.

Description of the solution
This change was reverted as this is just a prototype and we do not need it to work with Trilinos and L::d::V vectors.

prototypes/matrix_based_advection_diffusion/matrix_based_advection_diffusion.cc
@@ -59,8 +59,6 @@
 #include <deal.II/numerics/solution_transfer.h>
 #include <deal.II/numerics/vector_tools.h>
 
-#include <core/vector.h>
-
 #include <fstream>
 #include <iostream>
 
@@ -457,7 +455,7 @@ private:
   void
   output_results(const unsigned int cycle) const;
 
-  using VectorType = GlobalVectorType;
+  using VectorType = TrilinosWrappers::MPI::Vector;
   using MatrixType = TrilinosWrappers::SparseMatrix;
 
   parallel::distributed::Triangulation<dim> triangulation;
@@ -1353,7 +1351,7 @@ MatrixBasedAdvectionDiffusion<dim, fe_degree>::compute_update()
 {
   TimerOutput::Scope t(computing_timer, "compute update");
 
-  GlobalVectorType completely_distributed_solution(
+  TrilinosWrappers::MPI::Vector completely_distributed_solution(
     locally_owned_dofs, mpi_communicator);
 
   SolverControl solver_control(200, 1.e-8 * system_rhs.l2_norm(), true, true);

prototypes/matrix_based_non_linear_poisson/matrix_based_non_linear_poisson.cc
@@ -59,8 +59,6 @@
 #include <deal.II/numerics/solution_transfer.h>
 #include <deal.II/numerics/vector_tools.h>
 
-#include <core/vector.h>
-
 #include <fstream>
 #include <iostream>
 
@@ -350,7 +348,7 @@ private:
   void
   output_results(const unsigned int cycle) const;
 
-  using VectorType = GlobalVectorType;
+  using VectorType = TrilinosWrappers::MPI::Vector;
   using MatrixType = TrilinosWrappers::SparseMatrix;
 
   parallel::distributed::Triangulation<dim> triangulation;
@@ -996,7 +994,7 @@ MatrixBasedPoissonProblem<dim, fe_degree>::compute_update()
 {
   TimerOutput::Scope t(computing_timer, "compute update");
 
-  GlobalVectorType completely_distributed_solution(
+  TrilinosWrappers::MPI::Vector completely_distributed_solution(
     locally_owned_dofs, mpi_communicator);
 
   SolverControl              solver_control(100, 1.e-12);