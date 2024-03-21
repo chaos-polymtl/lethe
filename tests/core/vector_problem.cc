1cf37588f37ec04aec4479653f2ccbc43063cb98
hepap <helene.papillon-laroche@polymtl.ca>
2024-03-21 09:00:36 -0400

    Remove commented lines

--- tests/core/vector_problem.cc
+++ tests/core/vector_problem.cc
@@ -11,14 +11,10 @@
  * The full text of the license can be found in the file LICENSE at
  * the top level of the Lethe distribution.
  *
- * ---------------------------------------------------------------------
-
-*
-* Author: Audrey Collard-Daigneault, Polytechnique Montreal, 2020-
-*/
+ * ---------------------------------------------------------------------*/
 
 /**
- * @brief This code tests averaging values in time with Trilinos vectors.
+ * @brief This code tests dividing a solution vector in terms of the velocity and pressure components.
  */
 
 // Deal.II includes
@@ -26,9 +22,9 @@
 
 #include <deal.II/dofs/dof_tools.h>
 
-#include <deal.II/fe/component_mask.h>
 #include <deal.II/fe/fe_q.h>
 #include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/component_mask.h>
 
 #include <deal.II/grid/grid_generator.h>
 
@@ -49,95 +45,92 @@
 void
 test()
 {
-  Triangulation<3> tria(typename Triangulation<3>::MeshSmoothing(
-    Triangulation<3>::smoothing_on_refinement |
-    Triangulation<3>::smoothing_on_coarsening));
-
+  Triangulation<3> tria(
+    typename Triangulation<3>::MeshSmoothing(
+      Triangulation<3>::smoothing_on_refinement |
+      Triangulation<3>::smoothing_on_coarsening));
+      
   GridGenerator::hyper_cube(tria, -1, 1);
   DoFHandler<3> dof_handler(tria);
-
+    
   FESystem<3> fe(FE_Q<3>(1), 3, FE_Q<3>(1), 1);
-
+  
   dof_handler.distribute_dofs(fe);
-
+  
   Vector<double> dummy_solution;
-
+  
   dummy_solution.reinit(dof_handler.n_dofs());
-
+  
   const unsigned int dofs_per_cell = fe.n_dofs_per_cell();
-
-  Vector<double>                       cell_dummy_solution(dofs_per_cell);
+  
+  Vector<double> cell_dummy_solution(dofs_per_cell);
   std::vector<types::global_dof_index> local_dof_indices(dofs_per_cell);
-
+  
   for (const auto &cell : dof_handler.active_cell_iterators())
     {
-      cell_dummy_solution = 0;
-
-      for (unsigned int i = 0; i < dofs_per_cell; ++i)
+        cell_dummy_solution = 0;
+        
+        for (unsigned int i = 0; i < dofs_per_cell; ++i)
         {
           const auto comp_i = fe.system_to_component_index(i).first;
 
-          cell_dummy_solution(i) = 1.0 * (float(comp_i) + 1);
+          cell_dummy_solution(i) = 1.0*(float(comp_i)+1);
+          
         }
-
-      cell->get_dof_indices(local_dof_indices);
-
-      for (unsigned int i = 0; i < dofs_per_cell; ++i)
-        dummy_solution(local_dof_indices[i]) += cell_dummy_solution(i);
+        
+       cell->get_dof_indices(local_dof_indices);
+       
+       for (unsigned int i = 0; i < dofs_per_cell; ++i)
+          dummy_solution(local_dof_indices[i]) += cell_dummy_solution(i);
+      
     }
-
+    
   FEValuesExtractors::Vector velocities(0);
   FEValuesExtractors::Scalar pressure(3);
-
+  
   ComponentMask velocity_mask = fe.component_mask(velocities);
   ComponentMask pressure_mask = fe.component_mask(pressure);
-
-
-  std::vector<IndexSet> index_set_velocity =
-    DoFTools::locally_owned_dofs_per_component(dof_handler, velocity_mask);
-  std::vector<IndexSet> index_set_pressure =
-    DoFTools::locally_owned_dofs_per_component(dof_handler, pressure_mask);
-
-  // Vector<double> velocity_solution(dof_handler.n_locally_owned_dofs());
-  // Vector<double> pressure_solution(dof_handler.n_locally_owned_dofs());
-
+  
+  
+  std::vector<IndexSet> index_set_velocity = DoFTools::locally_owned_dofs_per_component(dof_handler, velocity_mask); 
+  std::vector<IndexSet> index_set_pressure = DoFTools::locally_owned_dofs_per_component(dof_handler, pressure_mask); 
+  
   double correction_norm = 0.0;
-  double max_correction  = DBL_MIN;
-
+  double max_correction = DBL_MIN;
+  
   for (unsigned int d = 0; d < 3; ++d)
+  {  
+    
+    for (auto j = index_set_velocity[d].begin(); j !=index_set_velocity[d].end(); j++)
     {
-      for (auto j = index_set_velocity[d].begin();
-           j != index_set_velocity[d].end();
-           j++)
-        {
-          correction_norm += dummy_solution[*j] * dummy_solution[*j];
-
-          if (dummy_solution[*j] > max_correction)
-            {
-              max_correction = dummy_solution[*j];
-            }
-        }
+      correction_norm += dummy_solution[*j]*dummy_solution[*j];
+      
+      if (dummy_solution[*j]>max_correction)
+      {
+        max_correction = dummy_solution[*j];
+      }
     }
-
+  }
+  
   deallog << "||u||_L2 : " << std::sqrt(correction_norm) << std::endl;
   deallog << "||u||_Linfty : " << max_correction << std::endl;
-
+  
   correction_norm = 0.0;
-  max_correction  = DBL_MIN;
-
-  for (auto j = index_set_pressure[3].begin(); j != index_set_pressure[3].end();
-       j++)
+  max_correction = DBL_MIN;
+  
+  for (auto j = index_set_pressure[3].begin(); j !=index_set_pressure[3].end(); j++)
     {
-      correction_norm += dummy_solution[*j] * dummy_solution[*j];
-
-      if (dummy_solution[*j] > max_correction)
-        {
-          max_correction = dummy_solution[*j];
-        }
+      correction_norm += dummy_solution[*j]*dummy_solution[*j];
+      
+      if (dummy_solution[*j]>max_correction)
+      {
+        max_correction = dummy_solution[*j];
+      }
     }
-
+  
   deallog << "||p||_L2 : " << std::sqrt(correction_norm) << std::endl;
   deallog << "||p||_Linfty : " << max_correction << std::endl;
+
 }
 
 int
