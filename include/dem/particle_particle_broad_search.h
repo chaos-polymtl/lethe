b1405801d57cc414205037f496911e46b5dd1709
Bruno Blais <blais.bruno@gmail.com>
2024-05-13 13:13:16 -0400

    Reapply "Change [] to .find() for storing candidates in p-w broad search"
    This reverts commit c7a558ef4ac04b169d19b8d5a92e491df0ed2dd5.

--- include/dem/particle_particle_broad_search.h
+++ include/dem/particle_particle_broad_search.h
@@ -133,13 +133,13 @@ private:
    * first particle in main cell (particle_begin will be the iterator after the
    * particles_to_evaluate.begin() in that case). When particle_begin is
    * particles_to_evaluate.begin(), it stores all the particle id in
-   * contact_pair_candidates_container.
+   * contact_pair_candidates.
    *
-   * @param particle_begin The particle iterator to start storing particle id
+   * @param particle_begin The particle iterator to start storing particle ids.
    * @param particles_to_evaluate The particle range to evaluate with the
-   * particle iterator prior storing ids
-   * @param contact_pair_candidates_container A vector which will contain all
-   * the particle pairs which are collision candidates
+   * particle iterator prior storing ids..
+   * @param contact_pair_candidates A map which will contain all the particle
+   * pairs candidate.
    */
   inline void
   store_candidates(
