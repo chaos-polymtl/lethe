75f2fd4d1a6ec679fb69d08c74b49f311a1b9e1c
cleodeletre <cleo.deletre@gmail.com>
2024-06-04 14:24:33 -0400

    modified version based on PR comments

--- include/dem/force_chains_visualization.h
+++ include/dem/force_chains_visualization.h
@@ -17,44 +17,110 @@
 using namespace dealii;
 using namespace DEM;
 
-#ifndef particles_force_chains_v2_h
-#  define particles_force_chains__v2_h
+#ifndef particles_force_chains_h
+#define particles_force_chains_h
 
+/**
+ * Base class for the particles force chains contact force models.
+ * This class does not implement any of the models, but ensures that
+ * an interface without template specialization is available. All of the
+ * actual implementation of the models are carried out in the
+ * ParticlesForceChains class which is templated by the contact model
+ * type.
+ */
+template <int dim>
+class ParticlesForceChainsBase
+{
+public:
+   /**
+   * @brief Use a ParticleParticleContactForce object to calculate normal forces between 
+   * all touching particles. Stock normal forces and particles position in vectors.
+   *
+   * @param container_manager The container manager object that contains
+   * containers to modify of contact pair periodic candidates with other
+   * containers with periodic neighbors lists
+   * @param dt DEM time step
+   */
+  virtual void
+  calculate_force_chains(DEMContactManager<dim> &container_manager,
+                         const double            dt) = 0;
+  /**
+   * @brief Output the force chains in a single vtu file for each iteration.
+   *
+   * @param mpi_communicator The mpi communicator   
+   * @param folder a string that contains the path where the results are to be saved
+   * @param iter the iteration number associated with the file
+   */
+  virtual void
+  write_force_chains(const MPI_Comm           mpi_communicator,
+                     const std::string  folder,
+                     const unsigned int iter) = 0;
+};
 
-
+/**
+ * @brief Class that carries out the calculation of
+ * particle-particle contact force and the visualization of force chains 
+ * by writing vtu files. Instead of using a inheritance hiearchy to distinguish between
+ * the contact model, the class is templated with the type of force model
+ * and rolling friction model. Consequently, the code for each
+ * combination of force model is generated at compile time.
+ *
+ * @tparam dim The dimension of the problem
+ * @tparam force_model The particle-particle contact force model
+ * @tparam rolling_friction_model The rolling resistance model used
+ */
 template <
   int                                                       dim,
   Parameters::Lagrangian::ParticleParticleContactForceModel force_model,
   Parameters::Lagrangian::RollingResistanceMethod rolling_friction_model>
 class ParticlesForceChains
-  : public ParticleParticleContactForce<dim,
-                                        force_model,
-                                        rolling_friction_model>
+  : public ParticlesForceChainsBase<dim>, 
+  public ParticleParticleContactForce<dim, force_model, rolling_friction_model>
 {
 public:
   ParticlesForceChains(const DEMSolverParameters<dim> &dem_parameters);
 
   virtual ~ParticlesForceChains()
   {}
-
+  /**
+   * @brief Create a triangulation with a unique cell (represented by a line) 
+   * for each normal force between two particles.
+   *
+   * @param tria Empty triangulation used to create a triangulation with all 
+   * the vertices needed.
+   * @param vertices Vector of points used to create a triangulation
+   */
   void
   multi_general_cell(Triangulation<1, 3>         &tria,
                      const std::vector<Point<3>> &vertices);
-
+   /**
+   * @brief Use a ParticleParticleContactForce object to calculate normal forces between 
+   * all touching particles. Stock normal forces and particles position in vectors.
+   *
+   * @param container_manager The container manager object that contains
+   * containers to modify of contact pair periodic candidates with other
+   * containers with periodic neighbors lists
+   * @param dt DEM time step
+   */
   void
   calculate_force_chains(DEMContactManager<dim> &container_manager,
-                         const double            dt);
-
-
+                         const double            dt) override;
+  /**
+   * @brief Output the force chains in a single vtu file for each iteration.
+   *
+   * @param mpi_communicator The mpi communicator   
+   * @param folder a string that contains the path where the results are to be saved
+   * @param iter the iteration number associated with the file
+   */
   void
-  write_force_chains(MPI_Comm           mpi_communicator,
+  write_force_chains(const MPI_Comm           mpi_communicator,
                      const std::string  folder,
-                     const unsigned int iter);
-
+                     const unsigned int iter) override;
 private:
+  // vector of normal forces between each touching particles.
   std::vector<double>             force_normal;
+  // vector of positions of touching particles.
   std::vector<Point<3>>           vertices;
-  const DEMSolverParameters<dim> &dem_parameters;
 };
 
 
--- source/dem/set_particle_particle_contact_force_model.cc
+++ source/dem/set_particle_particle_contact_force_model.cc
@@ -1,4 +1,5 @@
 #include <dem/set_particle_particle_contact_force_model.h>
+#include <dem/force_chains_visualization.h>
 
 using namespace dealii;
 
@@ -295,9 +296,308 @@ set_particle_particle_contact_force_model(
   return particle_particle_contact_force_object;
 }
 
+template <int dim>
+std::shared_ptr<ParticlesForceChainsBase<dim>>
+set_force_chains_contact_force_model(
+  const DEMSolverParameters<dim> &dem_parameters)
+{
+  std::shared_ptr<ParticlesForceChainsBase<dim>>
+    particles_force_chains_object;
+
+  if (dem_parameters.model_parameters.particle_particle_contact_force_model ==
+      Parameters::Lagrangian::ParticleParticleContactForceModel::linear)
+    {
+      switch (dem_parameters.model_parameters.rolling_resistance_method)
+        {
+          case Parameters::Lagrangian::RollingResistanceMethod::no_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    linear,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    no_resistance>>(dem_parameters);
+              break;
+            }
+          case Parameters::Lagrangian::RollingResistanceMethod::
+            constant_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    linear,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    constant_resistance>>(dem_parameters);
+              break;
+            }
+          case Parameters::Lagrangian::RollingResistanceMethod::
+            viscous_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    linear,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    viscous_resistance>>(dem_parameters);
+              break;
+            }
+          default:
+            throw std::runtime_error(
+              "Invalid contact force model and rolling resistance method combination");
+        }
+    }
+  else if (dem_parameters.model_parameters
+             .particle_particle_contact_force_model ==
+           Parameters::Lagrangian::ParticleParticleContactForceModel::
+             hertz_mindlin_limit_overlap)
+    {
+      switch (dem_parameters.model_parameters.rolling_resistance_method)
+        {
+          case Parameters::Lagrangian::RollingResistanceMethod::no_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    hertz_mindlin_limit_overlap,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    no_resistance>>(dem_parameters);
+              break;
+            }
+          case Parameters::Lagrangian::RollingResistanceMethod::
+            constant_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    hertz_mindlin_limit_overlap,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    constant_resistance>>(dem_parameters);
+              break;
+            }
+          case Parameters::Lagrangian::RollingResistanceMethod::
+            viscous_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    hertz_mindlin_limit_overlap,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    viscous_resistance>>(dem_parameters);
+              break;
+            }
+          default:
+            throw std::runtime_error(
+              "Invalid contact force model and rolling resistance method combination");
+        }
+    }
+  else if (dem_parameters.model_parameters
+             .particle_particle_contact_force_model ==
+           Parameters::Lagrangian::ParticleParticleContactForceModel::
+             hertz_mindlin_limit_force)
+    {
+      switch (dem_parameters.model_parameters.rolling_resistance_method)
+        {
+          case Parameters::Lagrangian::RollingResistanceMethod::no_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    hertz_mindlin_limit_force,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    no_resistance>>(dem_parameters);
+              break;
+            }
+          case Parameters::Lagrangian::RollingResistanceMethod::
+            constant_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    hertz_mindlin_limit_force,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    constant_resistance>>(dem_parameters);
+              break;
+            }
+          case Parameters::Lagrangian::RollingResistanceMethod::
+            viscous_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    hertz_mindlin_limit_force,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    viscous_resistance>>(dem_parameters);
+              break;
+            }
+          default:
+            throw std::runtime_error(
+              "Invalid contact force model and rolling resistance method combination");
+        }
+    }
+  else if (dem_parameters.model_parameters
+             .particle_particle_contact_force_model ==
+           Parameters::Lagrangian::ParticleParticleContactForceModel::hertz)
+    {
+      switch (dem_parameters.model_parameters.rolling_resistance_method)
+        {
+          case Parameters::Lagrangian::RollingResistanceMethod::no_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    hertz,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    no_resistance>>(dem_parameters);
+              break;
+            }
+          case Parameters::Lagrangian::RollingResistanceMethod::
+            constant_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    hertz,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    constant_resistance>>(dem_parameters);
+              break;
+            }
+          case Parameters::Lagrangian::RollingResistanceMethod::
+            viscous_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    hertz,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    viscous_resistance>>(dem_parameters);
+              break;
+            }
+          default:
+            throw std::runtime_error(
+              "Invalid contact force model and rolling resistance method combination");
+        }
+    }
+  else if (dem_parameters.model_parameters
+             .particle_particle_contact_force_model ==
+           Parameters::Lagrangian::ParticleParticleContactForceModel::hertz_JKR)
+    {
+      switch (dem_parameters.model_parameters.rolling_resistance_method)
+        {
+          case Parameters::Lagrangian::RollingResistanceMethod::no_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    hertz_JKR,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    no_resistance>>(dem_parameters);
+              break;
+            }
+          case Parameters::Lagrangian::RollingResistanceMethod::
+            constant_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    hertz_JKR,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    constant_resistance>>(dem_parameters);
+              break;
+            }
+          case Parameters::Lagrangian::RollingResistanceMethod::
+            viscous_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    hertz_JKR,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    viscous_resistance>>(dem_parameters);
+              break;
+            }
+          default:
+            throw std::runtime_error(
+              "Invalid contact force model and rolling resistance method combination");
+        }
+    }
+  else if (dem_parameters.model_parameters
+             .particle_particle_contact_force_model ==
+           Parameters::Lagrangian::ParticleParticleContactForceModel::DMT)
+    {
+      switch (dem_parameters.model_parameters.rolling_resistance_method)
+        {
+          case Parameters::Lagrangian::RollingResistanceMethod::no_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    DMT,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    no_resistance>>(dem_parameters);
+              break;
+            }
+          case Parameters::Lagrangian::RollingResistanceMethod::
+            constant_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    DMT,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    constant_resistance>>(dem_parameters);
+              break;
+            }
+          case Parameters::Lagrangian::RollingResistanceMethod::
+            viscous_resistance:
+            {
+              particles_force_chains_object =
+                std::make_shared<ParticlesForceChains<
+                  dim,
+                  Parameters::Lagrangian::ParticleParticleContactForceModel::
+                    DMT,
+                  Parameters::Lagrangian::RollingResistanceMethod::
+                    viscous_resistance>>(dem_parameters);
+              break;
+            }
+          default:
+            throw std::runtime_error(
+              "Invalid contact force model and rolling resistance method combination");
+        }
+    }
+  else
+    {
+      throw std::runtime_error(
+        "The chosen particle-particle contact force model is invalid");
+    }
+  return particles_force_chains_object;
+}
+
 template std::shared_ptr<ParticleParticleContactForceBase<2>>
 set_particle_particle_contact_force_model(
   const DEMSolverParameters<2> &dem_parameters);
 template std::shared_ptr<ParticleParticleContactForceBase<3>>
 set_particle_particle_contact_force_model(
+  const DEMSolverParameters<3> &dem_parameters);
+template std::shared_ptr<ParticlesForceChainsBase<2>>
+set_force_chains_contact_force_model(
+  const DEMSolverParameters<2> &dem_parameters);
+template std::shared_ptr<ParticlesForceChainsBase<3>>
+set_force_chains_contact_force_model(
   const DEMSolverParameters<3> &dem_parameters);
