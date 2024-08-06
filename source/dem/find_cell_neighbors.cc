6c99e1a60a4f4450b576e663685977bd445fb29f
Audrey Collard-Daigneault <acdaigneault@gmail.com>
2024-08-05 21:29:34 -0400

    Modify find_cell_neighbors from class to functions
    Former-commit-id: 28969ac2fbe91d45df42dd60144beccb195f0b7d

--- source/dem/find_cell_neighbors.cc
+++ source/dem/find_cell_neighbors.cc
@@ -369,26 +369,27 @@ find_cell_neighbors<3>(
 template void
 find_cell_periodic_neighbors<2>(
   const parallel::distributed::Triangulation<2> &triangulation,
-  const typename dem_data_structures<2>::periodic_boundaries_cells_info
+  const typename DEM::dem_data_structures<2>::periodic_boundaries_cells_info
     &periodic_boundaries_cells_information,
-  typename dem_data_structures<2>::cells_neighbor_list
+  typename DEM::dem_data_structures<2>::cells_neighbor_list
     &cells_local_periodic_neighbor_list,
-  typename dem_data_structures<2>::cells_neighbor_list
+  typename DEM::dem_data_structures<2>::cells_neighbor_list
     &cells_ghost_periodic_neighbor_list,
-  typename dem_data_structures<2>::cells_neighbor_list
+  typename DEM::dem_data_structures<2>::cells_neighbor_list
     &cells_ghost_local_periodic_neighbor_list);
 
 template void
 find_cell_periodic_neighbors<3>(
   const parallel::distributed::Triangulation<3> &triangulation,
-  const typename dem_data_structures<3>::periodic_boundaries_cells_info
+  const typename DEM::dem_data_structures<3>::periodic_boundaries_cells_info
     &periodic_boundaries_cells_information,
-  typename dem_data_structures<3>::cells_neighbor_list
+  typename DEM::dem_data_structures<3>::cells_neighbor_list
     &cells_local_periodic_neighbor_list,
-  typename dem_data_structures<3>::cells_neighbor_list
+  typename DEM::dem_data_structures<3>::cells_neighbor_list
     &cells_ghost_periodic_neighbor_list,
-  typename dem_data_structures<3>::cells_neighbor_list
+  typename DEM::dem_data_structures<3>::cells_neighbor_list
     &cells_ghost_local_periodic_neighbor_list);
+
 
 template void
 find_full_cell_neighbors<2>(
@@ -401,3 +402,23 @@ find_full_cell_neighbors<3>(
   const parallel::distributed::Triangulation<3> &triangulation,
   typename dem_data_structures<3>::cells_total_neighbor_list
     &cells_total_neighbor_list);
+
+template void
+get_periodic_neighbor_list<2>(
+  const typename Triangulation<2>::active_cell_iterator &cell,
+  const std::map<unsigned int, std::vector<unsigned int>>
+                                             &coinciding_vertex_groups,
+  const std::map<unsigned int, unsigned int> &vertex_to_coinciding_vertex_group,
+  const std::vector<std::set<typename Triangulation<2>::active_cell_iterator>>
+                                                    &v_to_c,
+  typename DEM::dem_data_structures<2>::cell_vector &periodic_neighbor_list);
+
+template void
+get_periodic_neighbor_list<3>(
+  const typename Triangulation<3>::active_cell_iterator &cell,
+  const std::map<unsigned int, std::vector<unsigned int>>
+                                             &coinciding_vertex_groups,
+  const std::map<unsigned int, unsigned int> &vertex_to_coinciding_vertex_group,
+  const std::vector<std::set<typename Triangulation<3>::active_cell_iterator>>
+                                                    &v_to_c,
+  typename DEM::dem_data_structures<3>::cell_vector &periodic_neighbor_list);
