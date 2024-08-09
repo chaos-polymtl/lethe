6b771f4869a65d1f155356b543bbe2375cb8e4c7
Bruno Blais <blais.bruno@gmail.com>
2024-08-09 17:23:44 -0400

    Upgrade line count plot
    Former-commit-id: 8377b50fc565f7101259fc5409d0e0ab6f347e66

--- contrib/utilities/plot_lines.py
+++ contrib/utilities/plot_lines.py
@@ -8,7 +8,7 @@ from datetime import datetime
 colors=['#1b9e77','#d95f02','#7570b3','#e7298a','#66a61e','#e6ab02']
 #Plot font and colors
 font = {'weight' : 'normal',
-        'size'   : 12}
+        'size'   : 14}
 
 plt.rc('font', **font)
 
@@ -24,13 +24,13 @@ for j in df["Date"]:
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-plt.plot_date(date_list,df["Code"],ms=4,color=colors[0],label="Code")
-plt.plot_date(date_list,df["Test"],ms=4,color=colors[1],label="Tests")
+plt.plot_date(date_list,df["Code"]/1000,ms=4,color=colors[0],label="Code")
+plt.plot_date(date_list,df["Test"]/1000,ms=4,color=colors[1],label="Tests")
 plt.xlabel("Date")
-plt.ylabel("Number of lines")
+plt.ylabel("Number of lines $\\cdot 10^3$")
 
 plt.legend()
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%y-%m-%d'))
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%y-%m'))
 plt.tight_layout()
-
+plt.savefig("lines.png",dpi=300)
 plt.show()
