81085da0782b9e844c7e2e23cf6b63cf0da73bfd
Bruno Blais <blais.bruno@gmail.com>
2024-09-13 16:15:51 -0400

    Starting to add interface for assemblers
    Former-commit-id: 94cc417bf869b543507f03f965144f11dc1f86a2

--- include/solvers/tracer_assemblers.h
+++ include/solvers/tracer_assemblers.h
@@ -174,5 +174,84 @@ public:
 };
 
 
+/**
+ * @brief A pure virtual class that serves as an interface for boundary and internal faces that occurs when using a discontinuous Galerkin discretization.
+ * The main difference between the TracerFaceAssembler and the TracerAssembler
+ * is that the TracerFaceAssembler assembles the matrix and rhs for internal
+ * faces and thus requires the StabilizedDGMethodsCopyData class.
+ *
+ * @tparam dim An integer that denotes the number of spatial dimensions
+ *
+ * @ingroup assemblers
+ */
+template <int dim>
+class TracerFaceAssembler
+{
+public:
+  /**
+   * @brief assemble_matrix Interface for the call to matrix assembly
+   * @param scratch_data Scratch data containing the Tracer information.
+   * It is important to note that the scratch data has to have been re-inited
+   * before calling for matrix assembly.
+   * @param copy_data Destination where the local_rhs and loc
+   */
+
+  virtual void
+  assemble_matrix(TracerScratchData<dim>      &scratch_data,
+                  StabilizedDGMethodsCopyData &copy_data) = 0;
+
+
+  /**
+   * @brief assemble_matrix Interface for the call to rhs
+   * @param scratch_data Scratch data containing the Tracer information.
+   * It is important to note that the scratch data has to have been re-inited
+   * before calling for matrix assembly.
+   * @param copy_data Destination where the local_rhs and loc
+   */
+
+  virtual void
+  assemble_rhs(TracerScratchData<dim>      &scratch_data,
+               StabilizedDGMethodsCopyData &copy_data) = 0;
+};
+
+
+/**
+ * @brief Assembles the symmetric interior penalty (SIPG) method (or Nitche's method) for internal faces. This assembler is only required when solving the Tracer equation using a discontinuous Galerkin discretization.
+ *
+ * @tparam dim An integer that denotes the number of spatial dimensions
+ *
+ * @ingroup assemblers
+ */
+template <int dim>
+class TracerAssemblerSIPG : public TracerFaceAssembler<dim>
+{
+public:
+  /**
+   * @brief assemble_matrix Interface for the call to matrix assembly
+   * @param scratch_data Scratch data containing the Tracer information.
+   * It is important to note that the scratch data has to have been re-inited
+   * before calling for matrix assembly.
+   * @param copy_data Destination where the local_rhs and loc
+   */
+
+  virtual void
+  assemble_matrix(TracerScratchData<dim>      &scratch_data,
+                  StabilizedDGMethodsCopyData &copy_data);
+
+
+  /**
+   * @brief assemble_matrix Interface for the call to rhs
+   * @param scratch_data Scratch data containing the Tracer information.
+   * It is important to note that the scratch data has to have been re-inited
+   * before calling for matrix assembly.
+   * @param copy_data Destination where the local_rhs and loc
+   */
+
+  virtual void
+  assemble_rhs(TracerScratchData<dim>      &scratch_data,
+               StabilizedDGMethodsCopyData &copy_data);
+};
+
+
 
 #endif
