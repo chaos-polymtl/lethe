e19470b3540d34fa9475c47de3f1378bb991e532
Bruno Blais <blais.bruno@gmail.com>
2024-10-01 10:49:19 -0400

    Add a test with tracer dynamic mesh adapt

--- source/solvers/tracer.cc
+++ source/solvers/tracer.cc
@@ -25,7 +25,9 @@
 
 #include <deal.II/meshworker/mesh_loop.h>
 
+#include <deal.II/numerics/error_estimator.h>
 #include <deal.II/numerics/vector_tools.h>
+
 
 template <int dim>
 void
@@ -1093,10 +1095,10 @@ Tracer<dim>::postprocess_tracer_flow_rate(const VectorType &current_solution_fd)
                            normal_vector_tracer) *
                         fe_face_values_tracer.JxW(q);
                     } // end loop on quadrature points
-                } // end face is a boundary face
-            } // end loop on faces
-        } // end condition cell at boundary
-    } // end loop on cells
+                }     // end face is a boundary face
+            }         // end loop on faces
+        }             // end condition cell at boundary
+    }                 // end loop on cells
 
 
   // Sum across all cores
@@ -1479,7 +1481,7 @@ Tracer<dim>::compute_kelly(
       const FEValuesExtractors::Scalar tracer(0);
 
       KellyErrorEstimator<dim>::estimate(
-        *this->temperature_mapping,
+        *this->mapping,
         this->dof_handler,
         *this->face_quadrature,
         typename std::map<types::boundary_id, const Function<dim, double> *>(),
