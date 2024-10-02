5f53e6d41b891989b3133a00228aeabaaac4953e
Bruno Blais <blais.bruno@gmail.com>
2024-10-02 16:57:11 -0400

    Remove bug

--- source/solvers/fluid_dynamics_matrix_free.cc
+++ source/solvers/fluid_dynamics_matrix_free.cc
@@ -523,7 +523,7 @@ namespace dealii
         dst = dst_;
       }
     } // namespace MGCoarseGridApplyPreconditioner
-  }   // namespace internal
+  } // namespace internal
 
 
   template <class VectorType, class PreconditionerType>
@@ -2222,29 +2222,34 @@ template <int dim>
 void
 FluidDynamicsMatrixFree<dim>::update_boundary_conditions()
 {
-  if (this->simulation_parameters.boundary_conditions.time_dependent)
+  if (!this->simulation_parameters.boundary_conditions.time_dependent)
+    return;
+
+  // We can never assume in the code anywhere that the local_evaluation_point is
+  // at the right value its value must always be reinitialized from the present
+  // solution. This may appear trivial, but this is extremely important when we
+  // are checkpointing. Trust me future Bruno.
+  this->local_evaluation_point = this->present_solution;
+
+  double time = this->simulation_control->get_current_time();
+  for (unsigned int i_bc = 0;
+       i_bc < this->simulation_parameters.boundary_conditions.size;
+       ++i_bc)
     {
-      double time = this->simulation_control->get_current_time();
-      for (unsigned int i_bc = 0;
-           i_bc < this->simulation_parameters.boundary_conditions.size;
-           ++i_bc)
-        {
-          this->simulation_parameters.boundary_conditions.bcFunctions[i_bc]
-            .u.set_time(time);
-          this->simulation_parameters.boundary_conditions.bcFunctions[i_bc]
-            .v.set_time(time);
-          this->simulation_parameters.boundary_conditions.bcFunctions[i_bc]
-            .w.set_time(time);
-          this->simulation_parameters.boundary_conditions
-            .bcPressureFunction[i_bc]
-            .p.set_time(time);
-        }
-      define_non_zero_constraints();
-      // Distribute constraints
-      auto &nonzero_constraints = this->nonzero_constraints;
-      nonzero_constraints.distribute(this->local_evaluation_point);
-      this->present_solution = this->local_evaluation_point;
+      this->simulation_parameters.boundary_conditions.bcFunctions[i_bc]
+        .u.set_time(time);
+      this->simulation_parameters.boundary_conditions.bcFunctions[i_bc]
+        .v.set_time(time);
+      this->simulation_parameters.boundary_conditions.bcFunctions[i_bc]
+        .w.set_time(time);
+      this->simulation_parameters.boundary_conditions.bcPressureFunction[i_bc]
+        .p.set_time(time);
     }
+  define_non_zero_constraints();
+  // Distribute constraints
+  auto &nonzero_constraints = this->nonzero_constraints;
+  nonzero_constraints.distribute(this->local_evaluation_point);
+  this->present_solution = this->local_evaluation_point;
 }
 
 template <int dim>
