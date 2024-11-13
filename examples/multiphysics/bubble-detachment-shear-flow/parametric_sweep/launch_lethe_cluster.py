a7b5f8d3e58872933dbad74c6c672ca63469416c
Pierre Laurentin <81017102+PierreLaurentinCS@users.noreply.github.com>
2024-11-13 10:46:48 -0500

    CHNS example : bubble detachment in a shear flow (#1334)
    Description
    This is the second example of the Cahn-Hilliard-Navier-Stokes solver. The solver is used to simulate the detachment of bubbles in a shear flow in three dimensions. The quantities of interest (detachment time and volume) are compared to values from the literature. An excellent agreement is observed, which confirms that the CHNS solver is able to describe complex interface physics, involving break-up and jet formation.
    
    All the necessary files to run the parametric sweep are included (only on the shear rate) and python scripts are provided to reproduce the figures if needed (see the previous PR : #1307).

---
+++ examples/multiphysics/bubble-detachment-shear-flow/parametric_sweep/launch_lethe_cluster.py
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023-2024 The Lethe Authors
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
+
+"""
+Summary: Script to launch all the Lethe simulations on a cluster, one job for every cases.
+"""
+
+import os
+
+PATH = os.getcwd()
+
+# File to look for in each case folder
+SHELL_FILE = 'launch_lethe.sh'
+case_count = 0
+
+# Walk through the directory structure
+for root, directories, files in os.walk(PATH):
+    if SHELL_FILE in files:
+        case_count+=1
+        os.chdir(root)
+        os.system(f'sbatch {SHELL_FILE}')
+        os.chdir(PATH)
+
+print(f'Number of cases launched = {case_count}')
+
+
