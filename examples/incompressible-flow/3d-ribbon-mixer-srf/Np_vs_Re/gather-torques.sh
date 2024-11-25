8897e3a9cf411bbe72e5c509c10cc75ce09e2b1e
mivaia <59896845+mivaia@users.noreply.github.com>
2024-11-24 20:46:21 -0500

    3D ribbon mixer. Modified the gather-torques.sh script to take the 1st boundary instead of the boundary 0. (convention was changed a couple of commits ago).  Everything works except there is no experimental.dat file for the plot_Np_vs_re.py script (#1381)
    Modified mesh section in the doc.
    Modified the gather-torques because of the new boundary convention.
    ALSO, the experimental.dat file is missing and the reference is not in the doc.

--- examples/incompressible-flow/3d-ribbon-mixer-srf/Np_vs_Re/gather-torques.sh
+++ examples/incompressible-flow/3d-ribbon-mixer-srf/Np_vs_Re/gather-torques.sh
@@ -3,5 +3,5 @@ for i in $(cat case_index.txt) ;
 do 
 echo "Gathering $i"
 echo "-------------"
-tail -1 ./$i/output/torque.00.dat   >> gather.dat
+tail -1 ./$i/output/torque.01.dat   >> gather.dat
 done
