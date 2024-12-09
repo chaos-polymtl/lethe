e7e842319cf6b9f0988e75c49e32da7db4139dbf
Mikael Vaillant <vaillantmikael@yahoo.ca>
2024-12-09 15:45:35 -0500

    Add a new object to calculate the time average of a scalar. Started adding it to the heat transfer physics

---
+++ include/solvers/postprocessing_scalar.h
@@ -0,0 +1,200 @@
+// SPDX-FileCopyrightText: Copyright (c) 2020-2024 The Lethe Authors
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
+
+#ifndef lethe_average_scalar_in_time_h
+#define lethe_average_scalar_in_time_h
+
+// Dofs
+#include <deal.II/dofs/dof_handler.h>
+
+// Distributed
+#include <deal.II/distributed/solution_transfer.h>
+
+// Lethe Includes
+#include <core/parameters.h>
+
+using namespace dealii;
+
+/**
+ * @brief Calculates the time-averaged scalar field.
+ *
+ * @tparam dim An integer that denotes the dimension of the space in which
+ * the flow is solved.
+ *
+ * @tparam VectorType The vector type used for the solvers (Trilinos (normal or block)
+ * or deal.II vectors).
+ *
+ * @tparam DofsType The type of storage of indices which differs according to vector.
+ *
+ */
+
+template <int dim>
+class AverageScalarInTime
+{
+    public:
+        /**
+         * @brief Constructor that initializes the solution transfer objects for the scalar field.
+         * 
+         * @param[in] dof_handler Used to initialize the solution transfer objects.
+         */
+        AverageScalarInTime(DoFHandler<dim> &dof_handler);
+
+
+        /**
+         * @brief Calculate time-averaged scalar field using vector with no ghost
+         * cells.
+         * 
+         * @param[in] local_evaluation_point The solution vector with no ghost cells.
+         * 
+         * @param[in] post_processing The postprocessing parameters to definie initial
+         * 
+         * @param[in] current_time The current time.
+         * 
+         * @param[in] time_step The time step.
+         */
+        void 
+        calculate_average_scalar(const GlobalVectorType &local_evaluation_point,
+                                 const Parameters::PostProcessing &post_processing,
+                                 const double current_time,
+                                 const double time_step);
+
+        void
+        initialize_vectors(const IndexSet &locally_owned_dofs,
+                           const IndexSet &locally_relevant_dofs,
+                           const MPI_Comm        &mpi_communicator);
+
+        /**
+         * @brief   Use the inverse of the time since the beginning of the time averaging to reevaluate the average scalar field.
+         *
+         */
+        GlobalVectorType
+        get_average_scalar()
+        {
+            return average_scalar;
+        }
+
+        /**
+         * @brief Prepare average velocity object for dynamic mesh adaptation.
+         */
+        void
+        update_average_scalar();
+
+        /**
+         * @brief Prepare average velocity object for dynamic mesh adaptation.
+         */
+        void
+        prepare_for_mesh_adaptation();
+
+        /**
+         * @brief Reestablish solution vectors after dynamic mesh adaptation.
+         */
+        void
+        post_mesh_adaptation();
+
+        /**
+         * @brief Save checkpoints to continuing averaging after restart.
+         *
+         * @param[in] prefix Name for checkpointing files.
+         *
+         * @return Vector with average values.
+         */
+        std::vector<const GlobalVectorType *>
+        save(const std::string &prefix);
+
+        /**
+         * @brief Read checkpoints to continuing averaging after restart.
+         *
+         * @param[in] prefix Name for checkpoint files.
+         *
+         * @return Vector with average values.
+         */
+        std::vector<GlobalVectorType *>
+        read(const std::string &prefix);
+
+
+        /**
+         * @brief Sanitize the average scalar object after a checkpoint has been read by
+         * resetting all of the locally_owned vectors using the locally_relevant
+         * vectors. This is necessary because only the locally_relevant_vectors are
+         * saved, but the calculation routines expect that the content of the
+         * locally_owned vectors match that of the locally_relevant vectors.
+         */
+        void
+        sanitize_after_restart()
+        {
+            sum_scalar_dt = sum_scalar_dt_with_ghost_cells;
+        }
+
+    private:
+        /**
+         * @brief Vector to store the scalar field multiplied by time step.
+         *
+         */
+        GlobalVectorType scalar_dt;
+
+        /**
+         * @brief Ghosted vector to store the sum of all the scalar multiplied by time step.
+         *
+         */
+        GlobalVectorType sum_scalar_dt_with_ghost_cells;
+
+        /**
+         * @brief Vector to store the sum of all the scalar multiplied by time step.
+         *
+         */
+        GlobalVectorType sum_scalar_dt;
+
+        /**
+         * @brief Object to transfer the averaged scalar in case of mesh adaptation.
+         *
+         */
+        parallel::distributed::SolutionTransfer<dim, GlobalVectorType> solution_transfer_sum_scalar_dt;
+
+        /**
+         * @brief Time averaged scalar.
+         */
+        GlobalVectorType average_scalar;
+
+        /**
+         * @brief Time averaged scalar containing the locally owned and locally relevant dofs for all processes.
+         */
+        GlobalVectorType average_scalar_with_ghost_cells;
+
+        /**
+         * @brief Difference between the simulation starting time and the initial averaging time.
+         */
+        double real_initial_time;
+
+        /**
+         * @brief Vector to store the sum of all the scalar field multiplied by time step.
+         *
+         */
+        double total_time_for_average;
+
+        /**
+         * @brief Time step.
+         *
+         */
+        double dt;
+
+        /**
+         * @brief First time step is stored in dt_0 and added to total_time_for_average to ensure the first average calculation does not result in a division by zero. 
+         * 
+         */
+        double dt_0;
+
+        /**
+         * @brief Inverse of the total time for averaging.
+         *
+         */
+        double inv_range_time;
+
+        /**
+         * @brief Track whether we are within the averaging time period.
+         *
+         */
+        bool average_calculation;
+
+};
+
+#endif
---
+++ source/solvers/postprocessing_scalar.cc
@@ -0,0 +1,149 @@
+// SPDX-FileCopyrightText: Copyright (c) 2020-2024 The Lethe Authors
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
+
+#include <core/vector.h>
+
+#include <solvers/postprocessing_scalar.h>
+
+#include <fstream>
+
+template <int dim>
+AverageScalarInTime<dim>::AverageScalarInTime(
+  DoFHandler<dim> &dof_handler)
+  : solution_transfer_sum_scalar_dt(dof_handler)
+  , total_time_for_average(0.0)
+  , average_calculation(false)
+{}
+
+template <int dim>
+void
+AverageScalarInTime<dim>::calculate_average_scalar(
+  const GlobalVectorType           &local_evaluation_point,
+  const Parameters::PostProcessing &post_processing,
+  const double                      current_time,
+  const double                      time_step)
+{
+  const double epsilon      = 1e-6;
+  const double initial_time = post_processing.initial_time;
+  dt                        = time_step;
+
+  std::cout<<"allo 0"<<std::endl;
+  // When averaging velocities begins
+  if (current_time >= (initial_time - epsilon) && !average_calculation)
+    {
+      average_calculation = true;
+      real_initial_time   = current_time;
+
+      // Store the first dt value in case dt varies.
+      dt_0 = dt;
+    }
+  std::cout<< dt <<std::endl;
+
+  std::cout<<local_evaluation_point.size()<<std::endl;
+  // Calculate (scalar*dt) at each time step and accumulates the values
+  scalar_dt.equ(dt, local_evaluation_point);
+
+  sum_scalar_dt += scalar_dt;
+
+  // Get the inverse of the time since the beginning of the time averaging
+  total_time_for_average = (current_time - real_initial_time) + dt_0;
+  inv_range_time         = 1. / total_time_for_average;
+
+  // Calculate the average velocities.
+  average_scalar.equ(inv_range_time, sum_scalar_dt);
+
+}
+
+template <int dim>
+void
+AverageScalarInTime<dim>::initialize_vectors(
+  const IndexSet  &locally_owned_dofs,
+  const IndexSet  &locally_relevant_dofs,
+  const MPI_Comm         &mpi_communicator)
+{
+  // Reinitialisation of the average velocity and reynolds stress vectors
+  // to get the right length.
+  scalar_dt.reinit(locally_owned_dofs, mpi_communicator);
+  sum_scalar_dt.reinit(locally_owned_dofs, mpi_communicator);
+  average_scalar.reinit(locally_owned_dofs, mpi_communicator);
+
+  average_scalar_with_ghost_cells.reinit(locally_owned_dofs,
+                                         locally_relevant_dofs,
+                                         mpi_communicator);
+}
+
+
+// CHECK FROM HERE
+
+template <int dim>
+void
+AverageScalarInTime<dim>::update_average_scalar()
+{
+  if (total_time_for_average > 1e-16)
+    {
+      inv_range_time = 1.0 / total_time_for_average;
+      // Calculate the average scalar.
+      average_scalar.equ(inv_range_time, sum_scalar_dt);
+    }
+}
+
+template <int dim>
+void
+AverageScalarInTime<dim>::prepare_for_mesh_adaptation()
+{
+  sum_scalar_dt_with_ghost_cells = sum_scalar_dt;
+  solution_transfer_sum_scalar_dt.prepare_for_coarsening_and_refinement(sum_scalar_dt_with_ghost_cells);
+}
+
+template <int dim>
+void
+AverageScalarInTime<dim>::post_mesh_adaptation()
+{
+  solution_transfer_sum_scalar_dt.interpolate(sum_scalar_dt);
+
+  sum_scalar_dt_with_ghost_cells = sum_scalar_dt;
+
+  update_average_scalar();
+}
+
+template <int dim>
+std::vector<const GlobalVectorType *>
+AverageScalarInTime<dim>::save(const std::string &prefix)
+{
+  sum_scalar_dt_with_ghost_cells = sum_scalar_dt;
+
+  std::vector<const GlobalVectorType *> avg_scalar_set_transfer;
+  avg_scalar_set_transfer.push_back(&sum_scalar_dt_with_ghost_cells);
+
+  std::string   filename = prefix + ".averagescalar";
+  std::ofstream output(filename.c_str());
+  output << "Average scalar" << std::endl;
+  output << "dt_0 " << dt_0 << std::endl;
+  output << "Average_calculation_boolean " << average_calculation << std::endl;
+  output << "Real_initial_time " << real_initial_time << std::endl;
+
+  return avg_scalar_set_transfer;
+}
+
+template <int dim>
+std::vector<GlobalVectorType *>
+AverageScalarInTime<dim>::read(const std::string &prefix)
+{
+  std::vector<GlobalVectorType *> sum_vectors;
+  sum_vectors.push_back(&sum_scalar_dt_with_ghost_cells);
+
+  std::string   filename = prefix + ".averagescalar";
+  std::ifstream input(filename.c_str());
+  AssertThrow(input, ExcFileNotOpen(filename));
+
+  std::string buffer;
+  std::getline(input, buffer);
+  input >> buffer >> dt_0;
+  input >> buffer >> average_calculation;
+  input >> buffer >> real_initial_time;
+
+  return sum_vectors;
+}
+
+template class AverageScalarInTime<2>;
+template class AverageScalarInTime<3>;
