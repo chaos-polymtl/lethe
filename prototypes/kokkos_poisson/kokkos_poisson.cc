231c9fdc874a95e265479dcc5d985ea388081244
Peter Munch <peterrmuench@gmail.com>
2025-01-30 20:10:50 +0100

    Add multigrid

--- prototypes/kokkos_poisson/kokkos_poisson.cc
+++ prototypes/kokkos_poisson/kokkos_poisson.cc
@@ -23,12 +23,144 @@
 #include <deal.II/matrix_free/portable_matrix_free.h>
 #include <deal.II/matrix_free/tools.h>
 
+#include <deal.II/multigrid/mg_base.h>
+#include <deal.II/multigrid/mg_base.templates.h>
+#include <deal.II/multigrid/mg_coarse.h>
+#include <deal.II/multigrid/mg_matrix.h>
+#include <deal.II/multigrid/mg_smoother.h>
+#include <deal.II/multigrid/mg_transfer_global_coarsening.h>
+#include <deal.II/multigrid/multigrid.h>
+#include <deal.II/multigrid/multigrid.templates.h>
+
 #include <deal.II/numerics/data_out.h>
 #include <deal.II/numerics/vector_tools.h>
 
 using namespace dealii;
 
 static unsigned int counter = 0;
+
+
+
+template <int dim, typename Number, typename MemorySpace>
+class MGTransferMFWrapper
+  : public MGTransferBase<
+      LinearAlgebra::distributed::Vector<Number, MemorySpace>>
+{
+public:
+  using VectorType = LinearAlgebra::distributed::Vector<Number, MemorySpace>;
+  using VectorTypeHost =
+    LinearAlgebra::distributed::Vector<Number, dealii::MemorySpace::Host>;
+
+  MGTransferMFWrapper(
+    const MGLevelObject<MGTwoLevelTransfer<dim, VectorTypeHost>> &mg_transfers)
+    : transfer(mg_transfers)
+  {}
+
+  template <typename Number2>
+  void
+  copy_to_mg(
+    const DoFHandler<dim>                                          &dof_handler,
+    MGLevelObject<VectorType>                                      &dst,
+    const LinearAlgebra::distributed::Vector<Number2, MemorySpace> &src) const
+  {
+    MGLevelObject<VectorTypeHost> dst_host(dst.min_level(), dst.max_level());
+    LinearAlgebra::distributed::Vector<Number2, dealii::MemorySpace::Host>
+      src_host;
+
+    copy_to_host(src_host, src);
+    for (unsigned int l = dst.min_level(); l < dst.max_level(); ++l)
+      copy_to_host(dst_host[l], dst[l]);
+
+    transfer.copy_to_mg(dof_handler, dst_host, src_host);
+
+    for (unsigned int l = dst.min_level(); l <= dst.max_level(); ++l)
+      copy_from_host(dst[l], dst_host[l]);
+  }
+
+  template <typename Number2>
+  void
+  copy_from_mg(const DoFHandler<dim> &dof_handler,
+               LinearAlgebra::distributed::Vector<Number2, MemorySpace> &dst,
+               const MGLevelObject<VectorType> &src) const
+  {
+    LinearAlgebra::distributed::Vector<Number2, dealii::MemorySpace::Host>
+                                  dst_host;
+    MGLevelObject<VectorTypeHost> src_host(src.min_level(), src.max_level());
+
+    copy_to_host(dst_host, dst);
+    for (unsigned int l = src.min_level(); l <= src.max_level(); ++l)
+      copy_to_host(src_host[l], src[l]);
+
+    transfer.copy_from_mg(dof_handler, dst_host, src_host);
+
+    copy_from_host(dst, dst_host);
+  }
+
+  void
+  prolongate(const unsigned int to_level,
+             VectorType        &dst,
+             const VectorType  &src) const override
+  {
+    VectorTypeHost dst_host;
+    VectorTypeHost src_host;
+
+    copy_to_host(dst_host, dst);
+    copy_to_host(src_host, src);
+
+    transfer.prolongate(to_level, dst_host, src_host);
+
+    copy_from_host(dst, dst_host);
+  }
+
+  void
+  restrict_and_add(const unsigned int from_level,
+                   VectorType        &dst,
+                   const VectorType  &src) const override
+  {
+    VectorTypeHost dst_host;
+    VectorTypeHost src_host;
+
+    copy_to_host(dst_host, dst);
+    copy_to_host(src_host, src);
+
+    transfer.restrict_and_add(from_level, dst_host, src_host);
+
+    copy_from_host(dst, dst_host);
+  }
+
+private:
+  const MGTransferMF<dim, Number, dealii::MemorySpace::Host> transfer;
+
+  template <typename Number2>
+  void
+  copy_to_host(
+    LinearAlgebra::distributed::Vector<Number2, dealii::MemorySpace::Host> &dst,
+    const LinearAlgebra::distributed::Vector<Number2, MemorySpace> &src) const
+  {
+    LinearAlgebra::ReadWriteVector<Number2> rw_vector(
+      src.get_partitioner()->locally_owned_range());
+    rw_vector.import(src, VectorOperation::insert);
+
+    dst.reinit(src.get_partitioner());
+    dst.import(rw_vector, VectorOperation::insert);
+  }
+
+  template <typename Number2>
+  void
+  copy_from_host(
+    LinearAlgebra::distributed::Vector<Number2, MemorySpace> &dst,
+    const LinearAlgebra::distributed::Vector<Number2, dealii::MemorySpace::Host>
+      &src) const
+  {
+    LinearAlgebra::ReadWriteVector<Number2> rw_vector(
+      src.get_partitioner()->locally_owned_range());
+    rw_vector.import(src, VectorOperation::insert);
+
+    if (dst.size() == 0)
+      dst.reinit(src.get_partitioner());
+    dst.import(rw_vector, VectorOperation::insert);
+  }
+};
 
 template <int dim,
           int fe_degree,
@@ -39,6 +171,7 @@ class LaplaceOperator;
 
 template <int dim, int fe_degree, int n_components, typename Number>
 class LaplaceOperator<dim, fe_degree, n_components, Number, MemorySpace::Host>
+  : public EnableObserverPointer
 {
 public:
   using VectorType =
@@ -59,6 +192,19 @@ public:
       mapping, dof_handler, constraints, quadrature, additional_data);
   }
 
+  types::global_dof_index
+  m() const
+  {
+    return matrix_free.get_dof_handler().n_dofs();
+  }
+
+  Number
+  el(unsigned int, unsigned int) const
+  {
+    DEAL_II_NOT_IMPLEMENTED();
+    return 0;
+  }
+
   void
   initialize_dof_vector(VectorType &vec) const
   {
@@ -69,6 +215,15 @@ public:
   vmult(VectorType &dst, const VectorType &src) const
   {
     matrix_free.cell_loop(&LaplaceOperator::local_apply, this, dst, src, true);
+  }
+
+  void
+  Tvmult(VectorType &dst, const VectorType &src) const
+  {
+    AssertThrow(false, ExcNotImplemented());
+
+    (void)dst;
+    (void)src;
   }
 
   void
@@ -176,7 +331,7 @@ class LaplaceOperator<dim,
                       fe_degree,
                       n_components,
                       Number,
-                      MemorySpace::Default>
+                      MemorySpace::Default> : public EnableObserverPointer
 {
 public:
   using VectorType =
@@ -197,6 +352,19 @@ public:
       mapping, dof_handler, constraints, quadrature, additional_data);
   }
 
+  types::global_dof_index
+  m() const
+  {
+    return matrix_free.get_dof_handler().n_dofs();
+  }
+
+  Number
+  el(unsigned int, unsigned int) const
+  {
+    DEAL_II_NOT_IMPLEMENTED();
+    return 0;
+  }
+
   void
   initialize_dof_vector(VectorType &vec) const
   {
@@ -210,6 +378,15 @@ public:
     LaplaceOperatorLocal<dim, fe_degree, n_components, Number> local_operator;
     matrix_free.cell_loop(local_operator, src, dst);
     matrix_free.copy_constrained_values(src, dst); // TODO: annoying
+  }
+
+  void
+  Tvmult(VectorType &dst, const VectorType &src) const
+  {
+    AssertThrow(false, ExcNotImplemented());
+
+    (void)dst;
+    (void)src;
   }
 
   void
@@ -276,6 +453,9 @@ run(const unsigned int n_refinements, ConvergenceTable &table)
 
   using Number     = double;
   using VectorType = LinearAlgebra::distributed::Vector<Number, MemorySpace>;
+  using VectorTypeHost = LinearAlgebra::distributed::Vector<Number>;
+
+  const bool use_multigrid = true;
 
   parallel::distributed::Triangulation<dim> tria(comm);
 
@@ -308,7 +488,7 @@ run(const unsigned int n_refinements, ConvergenceTable &table)
   laplace_operator.initialize_dof_vector(dst);
 
   {
-    LinearAlgebra::distributed::Vector<Number> src_host(src.get_partitioner());
+    VectorTypeHost src_host(src.get_partitioner());
 
     VectorTools::create_right_hand_side<dim, dim>(
       mapping,
@@ -326,16 +506,121 @@ run(const unsigned int n_refinements, ConvergenceTable &table)
     dst = 0.0;
   }
 
-  // PreconditionIdentity preconditioner;
-  DiagonalMatrix<VectorType> preconditioner;
-  laplace_operator.compute_inverse_diagonal(preconditioner.get_vector());
-
   ReductionControl     solver_control;
   SolverCG<VectorType> solver(solver_control);
-  solver.solve(laplace_operator, dst, src, preconditioner);
+
+  if (!use_multigrid)
+    {
+      DiagonalMatrix<VectorType> preconditioner;
+      laplace_operator.compute_inverse_diagonal(preconditioner.get_vector());
+
+      solver.solve(laplace_operator, dst, src, preconditioner);
+    }
+  else
+    {
+      using LevelMatrixType =
+        LaplaceOperator<dim, degree, n_components, Number, MemorySpace>;
+      using SmootherPreconditionerType = DiagonalMatrix<VectorType>;
+      using SmootherType               = PreconditionChebyshev<LevelMatrixType,
+                                                 VectorType,
+                                                 SmootherPreconditionerType>;
+      using MGTransferType             = typename std::conditional<
+        std::is_same_v<MemorySpace, dealii::MemorySpace::Host>,
+        MGTransferMF<dim, Number>,
+        MGTransferMFWrapper<dim, Number, MemorySpace>>::type;
+
+      const auto coarse_grid_triangulations =
+        MGTransferGlobalCoarseningTools::create_geometric_coarsening_sequence(
+          dof_handler.get_triangulation());
+
+      const unsigned int min_level = 0;
+      const unsigned int max_level = coarse_grid_triangulations.size() - 1;
+
+      MGLevelObject<DoFHandler<dim>> mg_dof_handlers(min_level, max_level);
+      MGLevelObject<AffineConstraints<Number>> mg_constraints(min_level,
+                                                              max_level);
+      MGLevelObject<LevelMatrixType> mg_matrices(min_level, max_level);
+
+      MGLevelObject<MGTwoLevelTransfer<dim, VectorTypeHost>> mg_transfers(
+        min_level, max_level);
+
+      // level operators
+      for (unsigned int level = min_level; level <= max_level; ++level)
+        {
+          auto &dof_handler = mg_dof_handlers[level];
+          auto &constraint  = mg_constraints[level];
+
+          dof_handler.reinit(*coarse_grid_triangulations[level]);
+          dof_handler.distribute_dofs(fe);
+
+          constraint.reinit(dof_handler.locally_owned_dofs(),
+                            DoFTools::extract_locally_relevant_dofs(
+                              dof_handler));
+
+          DoFTools::make_zero_boundary_constraints(dof_handler, constraint);
+          constraint.close();
+
+          mg_matrices[level].reinit(mapping,
+                                    dof_handler,
+                                    constraint,
+                                    quadrature.get_tensor_basis()[0]);
+        }
+
+      mg::Matrix<VectorType> mg_matrix(mg_matrices);
+
+      // transfer operator
+      for (unsigned int level = min_level; level < max_level; ++level)
+        mg_transfers[level + 1].reinit(mg_dof_handlers[level + 1],
+                                       mg_dof_handlers[level],
+                                       mg_constraints[level + 1],
+                                       mg_constraints[level]);
+
+      MGTransferType mg_transfer(mg_transfers);
+
+      // smoother
+      MGLevelObject<typename SmootherType::AdditionalData> smoother_data(
+        min_level, max_level);
+
+      for (unsigned int level = min_level; level <= max_level; ++level)
+        {
+          smoother_data[level].preconditioner =
+            std::make_shared<SmootherPreconditionerType>();
+          mg_matrices[level].compute_inverse_diagonal(
+            smoother_data[level].preconditioner->get_vector());
+          smoother_data[level].smoothing_range     = 20;
+          smoother_data[level].degree              = 5;
+          smoother_data[level].eig_cg_n_iterations = 20;
+          smoother_data[level].constraints.copy_from(mg_constraints[level]);
+        }
+
+      MGSmootherPrecondition<LevelMatrixType, SmootherType, VectorType>
+        mg_smoother;
+      mg_smoother.initialize(mg_matrices, smoother_data);
+
+      for (unsigned int level = min_level; level <= max_level; ++level)
+        {
+          VectorType vec;
+          mg_matrices[level].initialize_dof_vector(vec);
+          mg_smoother.smoothers[level].estimate_eigenvalues(vec);
+        }
+
+      // coarse-grid solver
+      MGCoarseGridApplySmoother<VectorType> mg_coarse;
+      mg_coarse.initialize(mg_smoother);
+
+      // put everything together
+      Multigrid<VectorType> mg(
+        mg_matrix, mg_coarse, mg_transfer, mg_smoother, mg_smoother);
+
+      PreconditionMG<dim, VectorType, MGTransferType> preconditioner(
+        dof_handler, mg, mg_transfer);
+
+      // solve
+      solver.solve(laplace_operator, dst, src, preconditioner);
+    }
 
   {
-    LinearAlgebra::distributed::Vector<Number> dst_host(dst.get_partitioner());
+    VectorTypeHost dst_host(dst.get_partitioner());
 
     LinearAlgebra::ReadWriteVector<Number> rw_vector(
       src.get_partitioner()->locally_owned_range());
