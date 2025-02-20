c7acadc75a156c6f75ea9dcec5afdfd05e29690e
hepap <helene.papillon-laroche@polymtl.ca>
2025-02-20 09:47:05 -0500

    Add interface tools and unit test

---
+++ tests/core/interface_tools_sphere_volume.cc
@@ -0,0 +1,100 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025 The Lethe Authors
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
+
+// Deal.II includes
+#include <deal.II/base/function_signed_distance.h>
+
+#include <deal.II/distributed/tria.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/mapping.h>
+#include <deal.II/numerics/vector_tools.h>
+
+// Lethe
+#include <core/interface_tools.h>
+
+#include <../tests/tests.h>
+
+void
+test()
+{
+  Triangulation<3> triangulation;
+  DoFHandler<3>    dof_handler;
+  FE_Q<3> fe(1);
+  MappingQ<3> mapping(1);
+  
+  const Point<3> p_0 = Point<3>({0, 0, 0});
+  const Point<3> p_1 = Point<3>({1, 1, 1});
+
+  GridGenerator::hyper_rectangle(triangulation, p_0, p_1);
+  triangulation.refine_global(4);
+  
+  const Point<3> sphere_center = Point<3>({0.5, 0.5, 0.5});
+  const double sphere_radius = 0.25;
+  
+  Vector<double> error_volume(3);
+  
+  for (unsigned int n = 0; n < 3; n++)
+  {
+    triangulation.refine_global(n);
+  
+  dof_handler.reinit(triangulation);
+  dof_handler.distribute_dofs(fe);
+  
+  Vector<double> signed_distance;
+
+  signed_distance.reinit(dof_handler.n_dofs());
+  
+  VectorTools::interpolate(mapping, dof_handler, Functions::SignedDistance::Sphere<3>(sphere_center, sphere_radius), signed_distance);
+  
+  const double volume = InterfaceTools::compute_volume(mapping, dof_handler,fe, signed_distance, triangulation.get_communicator());
+  
+  error_volume[n] = abs(4.0*M_PI*std::pow(sphere_radius, 3)/3.0 - volume);
+  
+  deallog << "The volume error for ref. lev. " << n + 3 << " is : " << error_volume[n] << std::endl;
+
+}
+
+const double convergence_order = log(error_volume[2]/error_volume[1])/log(error_volume[1]/error_volume[0]);
+
+deallog << "The convergence is : " << convergence_order << std::endl;
+
+}
+
+int
+main(int argc, char *argv[])
+{
+  try
+    {
+      initlog();
+      Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
+      test();
+    }
+  catch (std::exception &exc)
+    {
+      std::cerr << std::endl
+                << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Exception on processing: " << std::endl
+                << exc.what() << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
+    }
+  catch (...)
+    {
+      std::cerr << std::endl
+                << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Unknown exception!" << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
+    }
+  return 0;
+}
