de2545855f0a486bbe02175a6827688abe8c263a
hepap <helene.papillon-laroche@polymtl.ca>
2025-02-20 09:47:05 -0500

    Add interface tools and unit test

--- include/core/interface_tools.h
+++ include/core/interface_tools.h
@@ -8,50 +8,356 @@
 
 #include <deal.II/fe/fe.h>
 
+#include <deal.II/grid/grid_tools.h>
+
+#include <deal.II/matrix_free/fe_point_evaluation.h>
+
+#include <deal.II/non_matching/quadrature_generator.h>
+
 using namespace dealii;
+
 namespace InterfaceTools
 {
   /**
    * @brief Scalar function defined by the DOF values of a single cell. Based on the CellWiseFunction and RefSpaceFEFieldFunction of dealii.
    */
-  template <int dim, typename VectorType = Vector<double>>
+  template <int dim, typename VectorType = Vector<double>, typename FEType = FE_Q<dim>>
   class CellWiseFunction : public Function<dim>
   {
   public:
     /**
      * @brief Constructor.
      *
-     * @param[in] p_fe Finite element discretizing the field we want to convert to
-     * a CellWiseFunction.
+     * @param[in] p_fe Finite element discretizing the field we want to convert
+     * to a CellWiseFunction.
      *
      */
-    CellWiseFunction(const FiniteElement<dim, dim> &p_fe);
-  
+    CellWiseFunction(const unsigned int p_fe_degree);
+
     /**
-     * @brief Set the cell that the function should be evaluated on. 
+     * @brief Set the cell that the function should be evaluated on.
      *
      * @param[in] in_local_dof_values Cell's DOF values
      *
      */
-      void
+    void
     set_active_cell(const VectorType &in_local_dof_values);
+
+    /**
+     * @brief Return the value of the function at the given point in the reference cell.
+     *
+     * @param[in] point Coordinates of the point in the reference cell
+     *
+     * @param[in] component Index of the component of the function for which the
+     * value is computed
+     *
+     * @return Value of the function (or the component of interest) at the given point
+     *
+     */
+    double
+    value(const Point<dim>  &point,
+          const unsigned int component = 0) const override;
+
+    /**
+     * @brief Return the function gradient of the specified component  at the given point in the reference cell.
+     *
+     * @param[in] point Coordinates of the point in the reference cell
+     *
+     * @param[in] component Index of the component of the function for which the
+     * gradient is computed
+     *
+     * @return Value of the gradient of the specified component at the given point
+     *
+     */
+    Tensor<1, dim>
+    gradient(const Point<dim>  &point,
+             const unsigned int component = 0) const override;
+
+    /**
+     * @brief Return the function Hessian of the specified component  at the given point in the reference cell.
+     *
+     * @param[in] point Coordinates of the point in the reference cell
+     *
+     * @param[in] component Index of the component of the function for which the
+     * Hessian is computed
+     *
+     * @return Value of the Hessian of the specified component at the given point
+     *
+     */
+    SymmetricTensor<2, dim>
+    hessian(const Point<dim>  &point,
+            const unsigned int component = 0) const override;
+
   private:
     /// Finite element discretizing the field of interest
-    FiniteElement<dim> fe;
-    
+    FEType fe;
+
     /// Number of dofs per element
     unsigned int n_cell_wise_dofs;
-    
+
+    ///
+    VectorType cell_dof_values;
   };
-  
-  template <int dim, typename VectorType>
-  CellWiseFunction<dim, VectorType>::CellWiseFunction(
-    const FiniteElement<dim, dim> &p_fe)
-    : fe(p_fe)
+
+  template <int dim, typename VectorType, typename FEType>
+  CellWiseFunction<dim, VectorType, FEType>::CellWiseFunction(
+    const unsigned int p_fe_degree)
+    : fe(p_fe_degree)
   {
     n_cell_wise_dofs = fe.dofs_per_cell;
   }
 
+  template <int dim, typename VectorType, typename FEType>
+  void
+  CellWiseFunction<dim, VectorType, FEType>::set_active_cell(
+    const VectorType &in_local_dof_values)
+  {
+    cell_dof_values = in_local_dof_values;
+  }
+
+  template <int dim, typename VectorType, typename FEType>
+  double
+  CellWiseFunction<dim, VectorType, FEType>::value(const Point<dim>  &point,
+                                           const unsigned int component) const
+  {
+    double value = 0;
+    for (unsigned int i = 0; i < n_cell_wise_dofs; ++i)
+      value +=
+        cell_dof_values[i] * fe.shape_value_component(i, point, component);
+
+    return value;
+  }
+
+  template <int dim, typename VectorType, typename FEType>
+  Tensor<1, dim>
+  CellWiseFunction<dim, VectorType, FEType>::gradient(
+    const Point<dim>  &point,
+    const unsigned int component) const
+  {
+    Tensor<1, dim> gradient;
+    for (unsigned int i = 0; i < n_cell_wise_dofs; ++i)
+      gradient +=
+        cell_dof_values[i] * fe.shape_grad_component(i, point, component);
+
+    return gradient;
+  }
+
+  template <int dim, typename VectorType, typename FEType>
+  SymmetricTensor<2, dim>
+  CellWiseFunction<dim, VectorType, FEType>::hessian(const Point<dim>  &point,
+                                             const unsigned int component) const
+  {
+    Tensor<2, dim> hessian;
+    for (unsigned int i = 0; i < n_cell_wise_dofs; ++i)
+      hessian +=
+        cell_dof_values[i] * fe.shape_grad_grad_component(i, point, component);
+
+    return symmetrize(hessian);
+  }
+
+  /**
+   * @brief
+   * Compute the volume enclosed by the 0 level of a level-set field
+   * inside a cell. The inside volume is computed, defined by negative values of
+   * the level-set field.
+   *
+   * @param[in] fe_point_evaluation FePointEvaluation
+   *
+   * @param[in] cell Cell for which the volume is computed
+   *
+   * @param[in] cell_dof_values cell DOFs value of the level set field
+   *
+   * @param[in] corr correction to apply to the DOF values (constant for all
+   * DOFs)
+   *
+   * @param[in] n_quad_points number of quadrature points for the volume
+   * integration faces
+   *
+   * @return cell-wise volume
+   */
+  template <int dim>
+  double
+  compute_cell_wise_volume(
+    FEPointEvaluation<1, dim>                            &fe_point_evaluation,
+    const typename DoFHandler<dim>::active_cell_iterator &cell,
+    Vector<double>     cell_dof_level_set_values,
+    const double       corr,
+    const unsigned int n_quad_points)
+  {
+    const unsigned int n_dofs = cell_dof_level_set_values.size();
+
+    // Initialize required variables to compute local volume
+    const BoundingBox<dim> unit_box = create_unit_bounding_box<dim>();
+    CellWiseFunction<dim>  signed_distance_function =
+      CellWiseFunction<dim>(cell->get_fe().degree);
+
+    hp::QCollection<1> q_collection;
+    q_collection.push_back(QGauss<1>(n_quad_points));
+
+    NonMatching::QuadratureGenerator<dim> quadrature_generator =
+      NonMatching::QuadratureGenerator<dim>(q_collection);
+
+    for (unsigned int j = 0; j < n_dofs; j++)
+      {
+        cell_dof_level_set_values[j] += corr;
+      }
+
+    signed_distance_function.set_active_cell(cell_dof_level_set_values);
+    quadrature_generator.generate(signed_distance_function, unit_box);
+
+    const Quadrature<dim> inside_quadrature =
+      quadrature_generator.get_inside_quadrature();
+
+    if (inside_quadrature.size() == 0)
+      return 0.0;
+
+    fe_point_evaluation.reinit(cell, inside_quadrature.get_points());
+
+    double inside_cell_volume = 0.0;
+    for (unsigned int q = 0; q < inside_quadrature.size(); q++)
+      {
+        /* Compute the volume int 1*JxW*dOmega. FEPointEvaluation.JxW() does not
+        return the right thing.*/
+        inside_cell_volume += fe_point_evaluation.jacobian(q).determinant() *
+                              inside_quadrature.weight(q);
+      }
+
+    return inside_cell_volume;
+  }
+
+  /**
+   * @brief
+   * Compute the volume enclosed by the 0 level of a level set field
+   * in the domain. The inside volume is computed, defined by negative values of
+   * the level-set field.
+   *
+   * @param[in] mapping Mapping of the domain
+   *
+   * @param[in] dof_handler DofHandler associated to the triangulation on which
+   * the volume is computed
+   *
+   * @param[in] level_set_vector Level-set vector
+   *
+   * @param[in] mpi_communicator MPI communicator
+   *
+   * @return Volume enclosed by the 0 level
+   */
+  template <int dim, typename VectorType>
+  double
+  compute_volume(const Mapping<dim>       &mapping,
+                 const DoFHandler<dim>    &dof_handler,
+                 const FiniteElement<dim> &fe,
+                 const VectorType         &level_set_vector,
+                 const MPI_Comm           &mpi_communicator)
+  {
+    FEPointEvaluation<1, dim> fe_point_evaluation(
+      mapping, fe, update_jacobians | update_JxW_values);
+
+    double volume = 0.0;
+    for (const auto &cell : dof_handler.active_cell_iterators())
+      {
+        if (cell->is_locally_owned())
+          {
+            const unsigned int n_dofs_per_cell =
+              cell->get_fe().n_dofs_per_cell();
+            Vector<double> cell_dof_level_set_values(n_dofs_per_cell);
+
+            cell->get_dof_values(level_set_vector,
+                                 cell_dof_level_set_values.begin(),
+                                 cell_dof_level_set_values.end());
+
+            volume += compute_cell_wise_volume(fe_point_evaluation,
+                                               cell,
+                                               cell_dof_level_set_values,
+                                               0.0,
+                                               cell->get_fe().degree + 1);
+          }
+      }
+    volume = Utilities::MPI::sum(volume, mpi_communicator);
+
+    return volume;
+  }
+
+  /**
+   * @brief
+   * Reconstruct the interface defined by the 0 level of a level set field
+   * in the domain.
+   *
+   * @param[in] mapping Mapping of the domain
+   *
+   * @param[in] dof_handler DofHandler associated to the triangulation for which
+   * the interface is reconstructed
+   *
+   * @param[in] fe Finite element
+   *
+   * @param[in] level_set_vector Level-set vector
+   *
+   * @param[in,out] interface_reconstruction_vertices Cell-wise map of the
+   * reconstructed surface vertices. The map contains vectors storing the
+   * vertices of the reconstructed surface for each intersected volume cell
+   * (dim).
+   *
+   * @param[in,out] interface_reconstruction_cells Cell-wise map of the
+   * reconstructed surface cells. The map contains vectors storing the cell
+   * (dim-1) of the reconstructed surface for each intersected volume cell
+   * (dim).
+   *
+   * @param[in,out] intersected_dofs Set of DOFs that belong to intersected
+   * volume cell (dim).
+   *
+   */
+  template <int dim, typename VectorType>
+  void
+  reconstruct_interface(
+    const Mapping<dim>       &mapping,
+    const DoFHandler<dim>    &dof_handler,
+    const FiniteElement<dim> &fe,
+    const VectorType         &level_set_vector,
+    std::map<types::global_cell_index, std::vector<Point<dim>>>
+      &interface_reconstruction_vertices,
+    std::map<types::global_cell_index, std::vector<CellData<dim - 1>>>
+                                      &interface_reconstruction_cells,
+    std::set<types::global_dof_index> &intersected_dofs)
+  {
+    GridTools::MarchingCubeAlgorithm<dim, VectorType> marching_cube(mapping,
+                                                                    fe,
+                                                                    1,
+                                                                    1e-10);
+    const unsigned int dofs_per_cell = fe.n_dofs_per_cell();
+
+    for (const auto &cell : dof_handler.active_cell_iterators())
+      {
+        if (cell->is_locally_owned())
+          {
+            std::vector<Point<dim>>        surface_vertices;
+            std::vector<CellData<dim - 1>> surface_cells;
+
+            marching_cube.process_cell(
+              cell, level_set_vector, 0.0, surface_vertices, surface_cells);
+
+            // If the cell is intersected, reconstruct the interface in it
+            if (surface_vertices.size() != 0)
+              {
+                const unsigned int cell_index =
+                  cell->global_active_cell_index();
+
+                // Store the interface reconstruction vertices and cells
+                interface_reconstruction_vertices[cell_index] =
+                  surface_vertices;
+                interface_reconstruction_cells[cell_index] = surface_cells;
+
+                // Store the dofs of the intersected volume cell
+                std::vector<types::global_dof_index> dof_indices(dofs_per_cell);
+                cell->get_dof_indices(dof_indices);
+
+                for (unsigned int i = 0; i < dofs_per_cell; ++i)
+                  {
+                    intersected_dofs.insert(dof_indices[i]);
+                  }
+              }
+          }
+      }
+  }
 
 } // namespace InterfaceTools
 
