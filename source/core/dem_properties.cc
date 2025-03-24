4ae9d940552c3db64c03dfc8898ac694f4124a37
marineharel <marine.harel@ensta-paris.fr>
2025-03-24 13:19:48 -0400

    undo last commit

--- source/core/dem_properties.cc
+++ source/core/dem_properties.cc
@@ -54,8 +54,10 @@ namespace DEM
     return properties;
   }
 
+  template class ParticleProperties<2, DEM::DEMProperties::PropertiesIndex>;
   template class ParticleProperties<2, DEM::CFDDEMProperties::PropertiesIndex>;
   template class ParticleProperties<2, DEM::DEMMPProperties::PropertiesIndex>;
+  template class ParticleProperties<3, DEM::DEMProperties::PropertiesIndex>;
   template class ParticleProperties<3, DEM::CFDDEMProperties::PropertiesIndex>;
   template class ParticleProperties<3, DEM::DEMMPProperties::PropertiesIndex>;
 
