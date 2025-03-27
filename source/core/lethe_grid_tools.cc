76d340267d4f89b32c359604dfb19eb1b2915c9c
marineharel <marine.harel@ensta-paris.fr>
2025-03-27 09:08:58 -0400

    correct template

--- source/core/lethe_grid_tools.cc
+++ source/core/lethe_grid_tools.cc
@@ -1484,13 +1484,6 @@ template std::
 template std::
   tuple<std::vector<bool>, std::vector<Point<3>>, std::vector<Tensor<1, 3>>>
   LetheGridTools::
-    find_particle_triangle_projection<2, DEM::DEMMPProperties::PropertiesIndex>(
-      const std::vector<Point<2>>                       &triangle,
-      const std::vector<Particles::ParticleIterator<2>> &particles,
-      const unsigned int &n_particles_in_base_cell);
-template std::
-  tuple<std::vector<bool>, std::vector<Point<3>>, std::vector<Tensor<1, 3>>>
-  LetheGridTools::
     find_particle_triangle_projection<3, DEM::DEMProperties::PropertiesIndex>(
       const std::vector<Point<3>>                       &triangle,
       const std::vector<Particles::ParticleIterator<3>> &particles,
