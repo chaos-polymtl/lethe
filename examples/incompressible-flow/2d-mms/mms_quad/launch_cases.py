46c41ac0dbfb3eb02707824d845c585dc1c4cd10
antonellasuccar <antonella.succar@polymtl.ca>
2025-03-31 17:54:18 -0400

    Part of the comments

--- examples/incompressible-flow/2d-mms/mms_quad/launch_cases.py
+++ examples/incompressible-flow/2d-mms/mms_quad/launch_cases.py
@@ -6,12 +6,27 @@ Script to launch different cases (as folders) of the same problem
 with different polynomial degrees for the velocity and the pressure (mesh is adapted automatically).
 """
 import os
+import argparse
+import re
 
 PATH = os.getcwd()
 PRM_FILE = 'mms_2d_steady.prm'
-LETHE_EXEC = 'mpirun -np 6 lethe-fluid'
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-n", "--num_cores", type=int, default=1, help="Number of cores to use (default: 1)")
+args = parser.parse_args()
+print(f"Using {args.num_cores} cores.")
+
+LETHE_EXEC = f'mpirun -np {args.num_cores} lethe-fluid'
 
 for root, directories, files in os.walk(PATH):
-    if PRM_FILE in files and root != PATH:
-                os.chdir(root)
-                os.system(f'{LETHE_EXEC} {PRM_FILE}' + '> output.txt')
+    pattern = r'degu_(\d+)_degp_(\d+)'
+    match = re.search(pattern, os.path.basename(root))  # Extract folder name details
+    if match:
+       os.chdir(root)
+       velocity_approx_degree = int(match.group(1))
+       pressure_approx_degree = int(match.group(2))
+       # Print the message with the simulation parameters
+       print(f'Running simulation with velocity approximation degree {velocity_approx_degree}, '
+             f'pressure approximation degree {pressure_approx_degree},  at increasing refinement levels')         
+       os.system(f'{LETHE_EXEC} {PRM_FILE}')
