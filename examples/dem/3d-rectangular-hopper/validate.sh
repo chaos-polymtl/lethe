51c29ba0ac7b96de8edcbaade707bffd820b02f5
Olivier Gaboriault <oliviergabo@gmail.com>
2025-04-08 13:10:37 -0400

    validate.sh

--- examples/dem/3d-rectangular-hopper/validate.sh
+++ examples/dem/3d-rectangular-hopper/validate.sh
@@ -50,6 +50,16 @@ wait $pid
 
 python3 hopper_post_processing.py -f . --prm hopper.prm --validate
 
+# Copy the information to the log folder
+cp $plots $folder
+cp $data $folder
+
+# Append the information to the report
+magick -density 300 -pointsize 12 text:mass-and-discharge-rate.txt mass-and-discharge-rate.pdf
+
+magick -density 300  $output_root/report.pdf mass-and-discharge-rate.pdf $plots  -quality 100 $output_root/temporary.pdf
+cp $output_root/temporary.pdf $output_root/report.pdf
+
 
 folder="$output_root/3d-rectangular-hopper-periodic"
 action="mpirun -np $n_proc lethe-particles hopper_periodic.prm" 
@@ -62,7 +72,7 @@ gmsh -3 hopper_structured_periodic.geo > "$folder/log-mesh"
 show_animation $pid "---> Running periodic BC simulation" 
 wait $pid
 
-python3 hopper_post_processing.py -f . --prm hopper.prm --validate
+python3 hopper_post_processing.py -f . --prm hopper_periodic.prm --validate
 
 # Copy the information to the log folder
 cp $plots $folder
