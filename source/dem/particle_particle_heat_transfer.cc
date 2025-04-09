ad4405b4b068fc1b01543fd5797d677dbb9e53f6
Marine Harel <165046707+marineharel@users.noreply.github.com>
2025-04-09 07:34:04 -0400

    Particle particle heat transfer (#1478)
    Description
    This PR adds the functions that will be used for thermal DEM including the calculation of the thermal resistances, the total conductance and two functions to apply heat transfer to particles for local contacts or ghost-local contacts, in the same way it is done for the contact force.
    
    Testing
    A test of the calculation of the thermal resistances is added.
    
    Documentation
    Miscellaneous (will be removed when merged)
    For now, these functions can only be used directly in tests.
    I plan on using them in the particle_particle_contact_force files next.
    
    * split into .cc and .h
    
    * correct
    
    * corect error
    
    * add const
    
    * modify erfc-1 function
    
    * add comments and change erfc-1 function
    
    * adjust test output
    
    * apply comments
    
    * replace sqrt

---
+++ source/dem/particle_particle_heat_transfer.cc
@@ -0,0 +1,228 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025 The Lethe Authors
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
+
+#include <dem/particle_particle_heat_transfer.h>
+
+double
+calculate_corrected_contact_radius(const double effective_radius,
+                                   const double effective_youngs_modulus,
+                                   const double effective_real_youngs_modulus,
+                                   const double normal_force_norm)
+{
+  const double contact_radius = pow((3 * normal_force_norm * effective_radius) /
+                                      (4 * effective_youngs_modulus),
+                                    (1.0 / 3.0));
+
+  // In certain cases, the simulation effective Young’s modulus
+  // can be chosen a few orders lower than the real effective Young’s modulus
+  // to ensure simulation stability (related to Rayleigh's time).
+  // As a consequence, the contact radius is overestimated, so a correctional
+  // factor is applied here.
+  return contact_radius *
+         pow(effective_youngs_modulus / effective_real_youngs_modulus,
+             1.0 / 5.0);
+}
+
+double
+calculate_macrocontact_resistance(const double harmonic_particle_conductivity,
+                                  const double contact_radius)
+{
+  return 0.5 / (contact_radius * harmonic_particle_conductivity + DBL_MIN);
+}
+
+double
+calculate_microcontact_resistance(const double equivalent_surface_slope,
+                                  const double equivalent_surface_roughness,
+                                  const double effective_microhardness,
+                                  const double contact_radius_squared,
+                                  const double harmonic_particle_conductivity,
+                                  const double maximum_pressure)
+{
+  // 1.184 = (1+0.96/2)/1.25
+  return 1.184 /
+         (M_PI * harmonic_particle_conductivity * contact_radius_squared) *
+         (equivalent_surface_roughness / equivalent_surface_slope) *
+         pow(effective_microhardness / (maximum_pressure + DBL_MIN), 0.96);
+
+  // Van Lew, J. T. (2016). On thermal characterization of breeder pebble beds
+  // with microscale numerical modeling of thermofluid and pebble-pebble
+  // interactions (Doctoral dissertation, University of California, Los Angeles)
+  // Equations 3.25 to 3.27.
+}
+
+double
+calculate_solid_macrogap_resistance(const double radius_one,
+                                    const double radius_two,
+                                    const double thermal_conductivity_one,
+                                    const double thermal_conductivity_two,
+                                    const double contact_radius_squared)
+{
+  // resistance_i = characteristic length parallel to heat flux /
+  // (thermal_conductivity
+  // * characteristic area perpendicular to the heat flux)
+  const double resistance_one =
+    0.25 * M_PI * radius_one /
+    (M_PI * (radius_one * radius_one - contact_radius_squared) *
+     thermal_conductivity_one);
+  const double resistance_two =
+    0.25 * M_PI * radius_two /
+    (M_PI * (radius_two * radius_two - contact_radius_squared) *
+     thermal_conductivity_two);
+
+  return resistance_one + resistance_two;
+}
+
+double
+calculate_interstitial_gas_microgap_resistance(
+  const double equivalent_surface_roughness,
+  const double contact_radius_squared,
+  const double gas_parameter_m,
+  const double thermal_conductivity_gas,
+  const double maximum_pressure,
+  const double effective_microhardness)
+{
+  const double x_1 = 2.0 * maximum_pressure / effective_microhardness;
+  const double x_2 = 0.03 * maximum_pressure / effective_microhardness;
+
+  // If the values are out of the valid range ]0, 2[ of the erfc^-1 function,
+  // this resistance has no physical meaning. Particles are too close or not
+  // close enough for a microgap between them to make sense. This resistance is
+  // used in parallel with another one, so putting it to infinity ignores it in
+  // the total resistance.
+  if (x_1 >= 2.0 || x_1 <= 0.0)
+    {
+      return INFINITY;
+    }
+
+  const double a_1 = boost::math::erfc_inv(x_1);
+  const double a_2 = boost::math::erfc_inv(x_2) - a_1;
+
+  // 2.82842712475 = 2*sqrt(2)
+  return (2.82842712475 * equivalent_surface_roughness * a_2) /
+         (M_PI * thermal_conductivity_gas * contact_radius_squared *
+          std::log(1 + a_2 / (a_1 + gas_parameter_m /
+                                      (2.82842712475 *
+                                       equivalent_surface_roughness))));
+}
+
+double
+calculate_interstitial_gas_macrogap_resistance(
+  const double harmonic_radius,
+  const double thermal_conductivity_gas,
+  const double contact_radius_squared,
+  const double gas_parameter_m)
+{
+  const double A =
+    2. * sqrt(harmonic_radius * harmonic_radius - contact_radius_squared);
+  const double S = 2. * harmonic_radius -
+                   contact_radius_squared / harmonic_radius + gas_parameter_m;
+
+  return 2.0 / (M_PI * thermal_conductivity_gas * (S * log(S / (S - A)) - A));
+}
+
+void
+calculate_contact_thermal_conductance(
+  const double radius_one,
+  const double radius_two,
+  const double effective_youngs_modulus,
+  const double effective_real_youngs_modulus,
+  const double equivalent_surface_roughness,
+  const double equivalent_surface_slope,
+  const double effective_microhardness,
+  const double thermal_conductivity_one,
+  const double thermal_conductivity_two,
+  const double thermal_conductivity_gas,
+  const double gas_parameter_m,
+  const double normal_overlap,
+  const double normal_force_norm,
+  double      &thermal_conductance)
+{
+  const double harmonic_particle_conductivity =
+    harmonic_mean(thermal_conductivity_one, thermal_conductivity_two);
+  const double harmonic_radius = harmonic_mean(radius_one, radius_two);
+
+  // Calculation of contact radius
+  const double contact_radius =
+    calculate_corrected_contact_radius(harmonic_radius * 0.5,
+                                       effective_youngs_modulus,
+                                       effective_real_youngs_modulus,
+                                       normal_force_norm);
+
+  // Squared contact radius, often used in resistances calculations.
+  const double contact_radius_squared = contact_radius * contact_radius;
+
+  // In the same way as the contact radius, if the effective Young's modulus is
+  // underestimated, the normal overlap is overestimated so a correctional
+  // factor is applied here.
+  const double corrected_normal_overlap =
+    normal_overlap *
+    pow(effective_youngs_modulus / effective_real_youngs_modulus, 2.0 / 3.0);
+  const double maximum_pressure =
+    (2.0 * effective_real_youngs_modulus * corrected_normal_overlap) /
+    (M_PI * contact_radius + DBL_MIN);
+
+  // Calculation of each thermal resistance
+  const double resistance_macrocontact =
+    calculate_macrocontact_resistance(harmonic_particle_conductivity,
+                                      contact_radius);
+
+  const double resistance_microcontact =
+    calculate_microcontact_resistance(equivalent_surface_slope,
+                                      equivalent_surface_roughness,
+                                      effective_microhardness,
+                                      contact_radius_squared,
+                                      harmonic_particle_conductivity,
+                                      maximum_pressure);
+
+  const double resistance_solid_macrogap =
+    calculate_solid_macrogap_resistance(radius_one,
+                                        radius_two,
+                                        thermal_conductivity_one,
+                                        thermal_conductivity_two,
+                                        contact_radius_squared);
+
+  const double resistance_gas_microgap =
+    calculate_interstitial_gas_microgap_resistance(equivalent_surface_roughness,
+                                                   contact_radius_squared,
+                                                   gas_parameter_m,
+                                                   thermal_conductivity_gas,
+                                                   maximum_pressure,
+                                                   effective_microhardness);
+
+  const double resistance_gas_macrogap =
+    calculate_interstitial_gas_macrogap_resistance(harmonic_radius,
+                                                   thermal_conductivity_gas,
+                                                   contact_radius_squared,
+                                                   gas_parameter_m);
+
+  // Calculation of the final thermal conductance (1 / total resistance)
+  // conductance = 1 / resistance contact area pathway + 1 / resistance magrogap
+  // pathway)
+  thermal_conductance =
+    1.0 / (resistance_macrocontact + 1.0 / (1.0 / resistance_microcontact +
+                                            1.0 / resistance_gas_microgap)) +
+    1.0 / (resistance_solid_macrogap + resistance_gas_macrogap);
+}
+
+void
+apply_heat_transfer_on_local_particles(const double temperature_one,
+                                       const double temperature_two,
+                                       const double thermal_conductance,
+                                       double      &particle_one_heat_transfer,
+                                       double      &particle_two_heat_transfer)
+{
+  const double heat_transfer =
+    thermal_conductance * (temperature_two - temperature_one);
+  particle_one_heat_transfer += heat_transfer;
+  particle_two_heat_transfer -= heat_transfer;
+}
+
+void
+apply_heat_transfer_on_single_local_particle(const double temperature_one,
+                                             const double temperature_two,
+                                             const double thermal_conductance,
+                                             double &particle_one_heat_transfer)
+{
+  particle_one_heat_transfer +=
+    thermal_conductance * (temperature_two - temperature_one);
+}
---
+++ tests/dem/particle_particle_thermal_resistances.cc
@@ -0,0 +1,294 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025 The Lethe Authors
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
+
+
+/**
+ * @brief This test checks the calculation of the thermal resistance network used to model conduction between two particles.
+ *
+ */
+
+
+#include <../tests/dem/test_particles_functions.h>
+#include <dem/particle_particle_contact_force.h>
+#include <dem/particle_particle_heat_transfer.h>
+
+
+template <int dim, typename PropertiesIndex>
+void
+test()
+{
+  // Creating the mesh and refinement
+  parallel::distributed::Triangulation<dim> triangulation(MPI_COMM_WORLD);
+  const int                                 hyper_cube_length = 1;
+  GridGenerator::hyper_cube(triangulation,
+                            -1 * hyper_cube_length,
+                            hyper_cube_length,
+                            true);
+  const int refinement_number = 2;
+  triangulation.refine_global(refinement_number);
+  MappingQ<dim> mapping(1);
+
+  // Defining simulation general parameters
+  DEMSolverParameters<dim>                              dem_parameters;
+  Parameters::Lagrangian::LagrangianPhysicalProperties &properties =
+    dem_parameters.lagrangian_physical_properties;
+  Parameters::Lagrangian::ModelParameters<dim> &model_param =
+    dem_parameters.model_parameters;
+
+  const Tensor<1, dim> g{{0, 0, 0}};
+  const double         dt                                    = 0.001;
+  const double         particle_diameter                     = 0.01;
+  const double         youngs_modulus                        = 5000000;
+  const double         poisson_ratio                         = 0.22;
+  const double         specific_heat                         = 840;
+  properties.particle_type_number                            = 1;
+  properties.youngs_modulus_particle[0]                      = youngs_modulus;
+  properties.poisson_ratio_particle[0]                       = poisson_ratio;
+  properties.restitution_coefficient_particle[0]             = 0.8;
+  properties.friction_coefficient_particle[0]                = 1;
+  properties.rolling_friction_coefficient_particle[0]        = 0.02;
+  properties.density_particle[0]                             = 2521;
+  properties.rolling_viscous_damping_coefficient_particle[0] = 0.;
+  properties.surface_energy_particle[0]                      = 0.;
+  properties.hamaker_constant_particle[0]                    = 0.;
+  model_param.rolling_resistance_method =
+    Parameters::Lagrangian::RollingResistanceMethod::constant_resistance;
+
+  const double neighborhood_threshold = std::pow(1.3 * particle_diameter, 2);
+
+  // Defining parameters for thermal DEM
+  const double real_youngs_modulus          = 65.e9;
+  const double equivalent_surface_roughness = 25e-9;
+  const double equivalent_surface_slope     = 0.078;
+  const double effective_microhardness      = 9e9;
+  const double thermal_conductivity_one     = 1;
+  const double thermal_conductivity_two     = 1;
+  const double thermal_conductivity_gas     = 0.027;
+  const double dynamic_viscosity_gas        = 1.85e-5;
+  const double specific_heat_gas            = 1006;
+  const double specific_heats_ratio_gas     = 1;
+  const double molecular_mean_free_path_gas = 68e-9;
+  const double thermal_accommodation        = 0.7;
+
+  // Defining particle handler
+  Particles::ParticleHandler<dim> particle_handler(
+    triangulation, mapping, PropertiesIndex::n_properties);
+
+  // Creating containers manager for finding cell neighbor and also broad and
+  // fine particle-particle search objects
+  DEMContactManager<dim, PropertiesIndex> contact_manager;
+
+  // Finding cell neighbors
+  typename dem_data_structures<dim>::periodic_boundaries_cells_info
+    dummy_pbc_info;
+  contact_manager.execute_cell_neighbors_search(triangulation, dummy_pbc_info);
+
+  // Inserting two particles in contact
+  Point<3>           position_1 = {0., 0, 0};
+  const unsigned int id_1       = 0;
+  Point<3>           position_2 = {0.0099, 0, 0};
+  const unsigned int id_2       = 1;
+
+  // Constructing particle iterators from particle positions (inserting
+  // particles)
+  Particles::ParticleIterator<dim> pit_1 = construct_particle_iterator<dim>(
+    particle_handler, triangulation, position_1, id_1);
+  Particles::ParticleIterator<dim> pit_2 = construct_particle_iterator<dim>(
+    particle_handler, triangulation, position_2, id_2);
+
+  // Setting particle properties
+  Tensor<1, dim>     v_1{{0, 0, 0}};
+  Tensor<1, dim>     omega_1{{0, 0, 0}};
+  Tensor<1, dim>     v_2{{0, 0, 0}};
+  Tensor<1, dim>     omega_2{{0, 0, 0}};
+  const double       mass        = 1;
+  const unsigned int type        = 0;
+  const double       T_initial_1 = 300;
+  const double       T_initial_2 = 100;
+
+  set_particle_properties<dim, PropertiesIndex>(
+    pit_1, type, particle_diameter, mass, v_1, omega_1);
+  set_particle_properties<dim, PropertiesIndex>(
+    pit_2, type, particle_diameter, mass, v_2, omega_2);
+
+  pit_1->get_properties()[PropertiesIndex::T]             = T_initial_1;
+  pit_1->get_properties()[PropertiesIndex::specific_heat] = specific_heat;
+
+  pit_2->get_properties()[PropertiesIndex::T]             = T_initial_2;
+  pit_2->get_properties()[PropertiesIndex::specific_heat] = specific_heat;
+
+  // Initializing variables
+  std::vector<Tensor<1, 3>> torque;
+  std::vector<Tensor<1, 3>> force;
+  std::vector<double>       MOI;
+
+  particle_handler.sort_particles_into_subdomains_and_cells();
+  force.resize(particle_handler.get_max_local_particle_index());
+  torque.resize(force.size());
+  MOI.resize(force.size());
+  for (auto &moi_val : MOI)
+    moi_val = 1;
+
+  contact_manager.update_local_particles_in_cells(particle_handler);
+
+  // Dummy Adaptive sparse contacts object and particle-particle broad search
+  AdaptiveSparseContacts<dim, PropertiesIndex> dummy_adaptive_sparse_contacts;
+  contact_manager.execute_particle_particle_broad_search(
+    particle_handler, dummy_adaptive_sparse_contacts);
+
+  // Calling fine search
+  contact_manager.execute_particle_particle_fine_search(neighborhood_threshold);
+
+  // Calling non linear force
+  ParticleParticleContactForce<
+    dim,
+    PropertiesIndex,
+    Parameters::Lagrangian::ParticleParticleContactForceModel::
+      hertz_mindlin_limit_overlap,
+    Parameters::Lagrangian::RollingResistanceMethod::constant_resistance>
+    nonlinear_force_object(dem_parameters);
+  nonlinear_force_object.calculate_particle_particle_contact_force(
+    contact_manager.get_local_adjacent_particles(),
+    contact_manager.get_ghost_adjacent_particles(),
+    contact_manager.get_local_local_periodic_adjacent_particles(),
+    contact_manager.get_local_ghost_periodic_adjacent_particles(),
+    contact_manager.get_ghost_local_periodic_adjacent_particles(),
+    dt,
+    torque,
+    force);
+
+  // Calculating additional parameters for thermal DEM
+  auto     particle_one          = particle_handler.begin();
+  auto     particle_two          = std::next(particle_one);
+  Point<3> particle_one_location = particle_one->get_location();
+  Point<3> particle_two_location = particle_two->get_location();
+
+  const double radius_one = particle_diameter * 0.5;
+  const double radius_two = particle_diameter * 0.5;
+  const double effective_youngs_modulus =
+    youngs_modulus / (2.0 * (1. - poisson_ratio * poisson_ratio));
+  const double effective_real_youngs_modulus =
+    real_youngs_modulus / (2.0 * (1. - poisson_ratio * poisson_ratio));
+  const double harmonic_particle_conductivity =
+    harmonic_mean(thermal_conductivity_one, thermal_conductivity_two);
+  const double harmonic_radius = harmonic_mean(radius_one, radius_two);
+  const double normal_overlap =
+    (radius_one + radius_two) -
+    particle_one_location.distance(particle_two_location);
+  const double normal_force_norm = force[particle_one->get_id()].norm();
+  const double prandtl_gas =
+    dynamic_viscosity_gas * specific_heat_gas / thermal_conductivity_gas;
+  const double gas_parameter_m =
+    2. * (2. - thermal_accommodation) / thermal_accommodation *
+    (2. * specific_heats_ratio_gas) / (1. + specific_heats_ratio_gas) *
+    molecular_mean_free_path_gas / prandtl_gas;
+
+  // Calculating the contact radius
+  const double contact_radius =
+    calculate_corrected_contact_radius(harmonic_radius * 0.5,
+                                       effective_youngs_modulus,
+                                       effective_real_youngs_modulus,
+                                       normal_force_norm);
+  const double contact_radius_squared = contact_radius * contact_radius;
+
+  const double corrected_normal_overlap =
+    normal_overlap *
+    pow(effective_youngs_modulus / effective_real_youngs_modulus, 2.0 / 3.0);
+  const double maximum_pressure =
+    (2.0 * effective_real_youngs_modulus * corrected_normal_overlap) /
+    (M_PI * contact_radius);
+
+  // Calculating each thermal resistance
+  const double resistance_macrocontact =
+    calculate_macrocontact_resistance(harmonic_particle_conductivity,
+                                      contact_radius);
+
+  const double resistance_microcontact =
+    calculate_microcontact_resistance(equivalent_surface_slope,
+                                      equivalent_surface_roughness,
+                                      effective_microhardness,
+                                      contact_radius_squared,
+                                      harmonic_particle_conductivity,
+                                      maximum_pressure);
+
+  const double resistance_solid_macrogap =
+    calculate_solid_macrogap_resistance(radius_one,
+                                        radius_two,
+                                        thermal_conductivity_one,
+                                        thermal_conductivity_two,
+                                        contact_radius_squared);
+
+  const double resistance_gas_microgap =
+    calculate_interstitial_gas_microgap_resistance(equivalent_surface_roughness,
+                                                   contact_radius_squared,
+                                                   gas_parameter_m,
+                                                   thermal_conductivity_gas,
+                                                   maximum_pressure,
+                                                   effective_microhardness);
+
+  const double resistance_gas_macrogap =
+    calculate_interstitial_gas_macrogap_resistance(harmonic_radius,
+                                                   thermal_conductivity_gas,
+                                                   contact_radius_squared,
+                                                   gas_parameter_m);
+
+  // Calculating total conductance and total resistance
+  const double total_conductance =
+    1. / (resistance_macrocontact +
+          1. / (1. / resistance_microcontact + 1. / resistance_gas_microgap)) +
+    1. / (resistance_solid_macrogap + resistance_gas_macrogap);
+  const double total_resistance = 1. / total_conductance;
+
+  // Output
+  deallog << " contact radius : " << contact_radius << std::endl
+          << " resistance macrocontact : " << resistance_macrocontact
+          << std::endl
+          << " resistance microcontact: " << resistance_microcontact
+          << std::endl
+          << " resistance solid macrogap : " << resistance_solid_macrogap
+          << std::endl
+          << " resistance gas microgap : " << resistance_gas_microgap
+          << std::endl
+          << " resistance gas macrogap : " << resistance_gas_macrogap
+          << std::endl
+          << " total resistance : " << total_resistance << std::endl
+          << " total conductance : " << total_conductance << std::endl;
+}
+
+int
+main(int argc, char **argv)
+{
+  try
+    {
+      Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
+
+      initlog();
+      test<3, DEM::DEMMPProperties::PropertiesIndex>();
+    }
+  catch (std::exception &exc)
+    {
+      std::cerr << std::endl
+                << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Exception on processing: " << std::endl
+                << exc.what() << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
+    }
+  catch (...)
+    {
+      std::cerr << std::endl
+                << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Unknown exception!" << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
+    }
+  return 0;
+}
