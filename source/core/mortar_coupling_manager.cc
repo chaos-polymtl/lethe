e15683822bf5239431db0d53f5d47ab2f1d865b6
Bruna Campos <bccampos@uwaterloo.ca>
2025-04-17 17:54:14 -0400

    Create mortar coupling manager instantiations

---
+++ source/core/mortar_coupling_manager.cc
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025 The Lethe Authors
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
+
+#include "core/mortar_coupling_manager.h"
+
+template class MortarManager<2>;
+template class CouplingOperator<2>;
