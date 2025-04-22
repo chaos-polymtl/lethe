c62f1a475100a5f64741b87dee04fbee6322205a
Bruna Campos <bccampos@uwaterloo.ca>
2025-04-22 12:55:15 -0400

    Add test to check generated points

---
+++ tests/mortar/manager_04.cc
@@ -0,0 +1,225 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025 The Lethe Authors
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
+
+/**
+ * @brief Mortar: check generated points using implemented function to
+ * read mesh and manifolds, and computing number of faces at the rotor-stator interface
+ * and the rotor radius.
+ */
+
+ #include <deal.II/distributed/tria.h>
+
+ #include <deal.II/fe/mapping_q.h>
+ 
+ #include <deal.II/grid/grid_generator.h>
+ #include <deal.II/grid/grid_out.h>
+ #include <deal.II/grid/grid_tools.h>
+ #include <deal.II/grid/manifold_lib.h>
+ #include <deal.II/grid/tria.h>
+ 
+ #include <deal.II/numerics/data_out.h>
+ #include <deal.II/numerics/data_out_faces.h>
+ #include <deal.II/numerics/data_postprocessor.h>
+ #include <deal.II/numerics/vector_tools.h>
+ 
+ // Lethe
+ #include <core/boundary_conditions.h>
+ #include <core/grids.h>
+ #include <core/parameters.h>
+ #include <core/mortar_coupling_manager.h>
+ 
+ // Tests (with common definitions)
+ #include <deal.II/base/conditional_ostream.h>
+ 
+ #include <../tests/tests.h>
+ 
+ #include <fstream>
+ 
+ void
+ test()
+ {
+   const MPI_Comm comm = MPI_COMM_WORLD;
+   
+   const unsigned int dim            = 2;
+   const unsigned int mapping_degree = 3;
+   const unsigned int n_quadrature_points = 3;
+
+   Parameters::Mesh                       mesh_parameters;
+   Parameters::Mortar<dim>                mortar_parameters;
+   Parameters::Manifolds                  manifolds_parameters;
+   BoundaryConditions::BoundaryConditions boundary_conditions;
+   boundary_conditions.type[0] = BoundaryConditions::BoundaryType::none;
+ 
+   // Stator mesh parameters
+   mesh_parameters.type                     = Parameters::Mesh::Type::dealii;
+   mesh_parameters.grid_type                = "hyper_shell";
+   mesh_parameters.grid_arguments           = "0, 0 : 0.5 : 1.0 : 6 : true";
+   mesh_parameters.scale                    = 1;
+   mesh_parameters.simplex                  = false;
+   mesh_parameters.initial_refinement       = 2;
+   mesh_parameters.refine_until_target_size = false;
+   mesh_parameters.boundaries_to_refine     = std::vector<int>();
+   mesh_parameters.initial_refinement_at_boundaries = 0;
+ 
+   // Rotor mesh parameters
+   mortar_parameters.enable           = "true";
+   mortar_parameters.rotor_mesh       = std::make_shared<Parameters::Mesh>();
+   mortar_parameters.rotor_mesh->type = Parameters::Mesh::Type::dealii;
+   mortar_parameters.rotor_mesh->grid_type      = "hyper_shell";
+   mortar_parameters.rotor_mesh->grid_arguments = "0, 0 : 0.25 : 0.5 : 6 : true";
+   mortar_parameters.rotor_mesh->scale          = 1;
+   mortar_parameters.rotor_mesh->simplex        = false;
+   mortar_parameters.stator_boundary_id         = 0;
+   mortar_parameters.rotor_boundary_id          = 3; // after shifting
+ 
+   // Initialized merged triangulation
+   parallel::distributed::Triangulation<dim> triangulation(comm);
+ 
+   // Merge stator and rotor triangulations
+   read_mesh_and_manifolds_for_stator_and_rotor(triangulation,
+                                                mesh_parameters,
+                                                manifolds_parameters,
+                                                false,
+                                                boundary_conditions,
+                                                mortar_parameters);
+ 
+  unsigned int        n_subdivisions = 0;
+  std::vector<double> radius_vec;
+  const double        tolerance = 1e-8;
+
+  /* Check faces at the rotor-stator interface and compute radius of rotor
+   * domain */
+  for (const auto &face :
+       triangulation.active_face_iterators())
+    {
+      if (face->at_boundary())
+        {
+          if (face->boundary_id() == mortar_parameters.rotor_boundary_id)
+            {
+              n_subdivisions++;
+              for (unsigned int vertex_index = 0;
+                   vertex_index < face->n_vertices();
+                   vertex_index++)
+                {
+                  auto v = face->vertex(vertex_index);
+                  radius_vec.emplace_back(sqrt(
+                    pow((v[0] - mortar_parameters.center_of_rotation[0]), 2) +
+                    pow((v[1] - mortar_parameters.center_of_rotation[1]), 2)));
+                }
+            }
+        }
+    }
+
+double radius =
+  std::reduce(radius_vec.begin(), radius_vec.end()) / radius_vec.size();
+  
+deallog << "Faces at rotor-stator interface : " << n_subdivisions << std::endl;
+deallog << "Rotor radius : " << radius << std::endl;
+
+// cell angle variation
+const double delta = 2 * numbers::PI / n_subdivisions;
+
+// rotate inner mesh using random scaling factors
+for (const double scale :
+     {0.0, 0.1, 0.5, 0.9, 1.1, 2.1, n_subdivisions - 0.9})
+  {
+    const double rotate = delta * scale;
+    deallog << "Rotation angle: " << rotate << std::endl;
+
+    const MortarManager<dim> manager(n_subdivisions,
+                                     n_quadrature_points,
+                                     radius,
+                                     rotate);
+
+    const auto print = [&](const double shift) {
+      for (unsigned int i = 0; i < n_subdivisions; ++i)
+        {
+          // center point of each cell (in radians)
+          const double rad = delta * (i + shift + 0.5);
+
+          deallog << "Shift: " << shift << std::endl;
+          deallog << "Cell center at " << rad << ": " << std::endl;
+
+          const auto indices = manager.get_indices(rad);
+          const auto weights = manager.get_weights(rad);
+          const auto points  = manager.get_points(rad);
+          const auto normals = manager.get_normals(rad);
+
+          for (unsigned int i = 0; i < indices.size(); ++i)
+            {
+              deallog << indices[i] << ": " << weights[i] << "   " << points [i][0] << "   " << points[i][1] << "   " << normals[i][0] << "   " << normals[i][1]  << std::endl;
+            }
+          deallog << std::endl;
+        }
+      deallog << std::endl;
+    };
+
+    // print generated points for aligned mesh (scale = 0.0)
+    print(0.0);
+
+    // print generated points for non-aligned mesh
+    if (scale != 0.0)
+      print(scale - std::floor(scale)); // outer (fixed) mesh
+
+    if (scale != 0.0)
+      print(scale); // inner (rotated) mesh
+
+    deallog << std::endl << std::endl << std::endl;
+  }
+ 
+   // Generate vtu file
+   DataOut<dim>       data_out;
+   MappingQ<dim, dim> mapping(mapping_degree);
+ 
+   DataOutBase::VtkFlags flags;
+   flags.write_higher_order_cells = true;
+   data_out.set_flags(flags);
+   data_out.attach_triangulation(triangulation);
+ 
+   Vector<double> ranks(triangulation.n_active_cells());
+   ranks = Utilities::MPI::this_mpi_process(comm);
+   data_out.add_data_vector(ranks, "ranks");
+   data_out.build_patches(mapping,
+                          mapping_degree + 1,
+                          DataOut<dim>::CurvedCellRegion::curved_inner_cells);
+   data_out.write_vtu_in_parallel("out.vtu", comm);
+ }
+ 
+ int
+ main(int argc, char *argv[])
+ {
+   try
+     {
+       initlog();
+       Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv, 1);
+       test();
+     }
+   catch (std::exception &exc)
+     {
+       std::cerr << std::endl
+                 << std::endl
+                 << "----------------------------------------------------"
+                 << std::endl;
+       std::cerr << "Exception on processing: " << std::endl
+                 << exc.what() << std::endl
+                 << "Aborting!" << std::endl
+                 << "----------------------------------------------------"
+                 << std::endl;
+       return 1;
+     }
+   catch (...)
+     {
+       std::cerr << std::endl
+                 << std::endl
+                 << "----------------------------------------------------"
+                 << std::endl;
+       std::cerr << "Unknown exception!" << std::endl
+                 << "Aborting!" << std::endl
+                 << "----------------------------------------------------"
+                 << std::endl;
+       return 1;
+     }
+ 
+   return 0;
+ }
+ 
