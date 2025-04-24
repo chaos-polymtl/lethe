f09932ba57f7a9459fad08147e15066b73117134
Olivier Gaboriault <oliviergabo@gmail.com>
2025-04-24 14:57:16 -0400

    Progress

---
+++ tests/dem/particle_ray_tracing_find_cells.cc
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025 The Lethe Authors
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
+
+/**
+ * @brief Check if the particle containing the particle and their neighbors are
+ * correctly flag for the particle ray-tracing
+ */
