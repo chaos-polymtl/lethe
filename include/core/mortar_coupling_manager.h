e71074c5ce38b949438be6361880c9aaecd3bd05
Bruna Campos <bccampos@uwaterloo.ca>
2025-04-29 10:11:47 -0400

    Fix radius computation for mpi framework in coupling manager

--- include/core/mortar_coupling_manager.h
+++ include/core/mortar_coupling_manager.h
@@ -614,67 +614,67 @@ CouplingOperator<dim, n_components, Number>::CouplingOperator(
   , constraints(constraints)
   , quadrature(quadrature)
 {
-  unsigned int        n_subdivisions_active = 0;
-  std::vector<double> radius_vec;
-  const double        tolerance = 1e-8;
+  // Number of subdivisions per process
+  unsigned int n_subdivisions_local = 0;
+  // Number of vertices at the boundary per process
+  unsigned int n_vertices_local = 0;
+  // Tolerance for rotor radius computation
+  const double tolerance = 1e-8;
+  // Min and max values for rotor radius computation
+  double radius_min = 1e12;
+  double radius_max = 1e-12;
 
-  /* Check faces at the rotor-stator interface */
-  for (const auto &cell : dof_handler.get_triangulation().active_cell_iterators())
+  // Check number of faces and vertices at the rotor-stator interface
+  for (const auto &cell :
+       dof_handler.get_triangulation().active_cell_iterators())
     {
       if (cell->is_locally_owned())
         {
-          // Looping through all the faces of the cell
           for (const auto &face : cell->face_iterators())
             {
-              // Check to see if the face is located at boundary
               if (face->at_boundary())
                 {
                   if (face->boundary_id() ==
                       mortar_parameters.rotor_boundary_id)
                     {
-                      n_subdivisions_active++;
+                      n_subdivisions_local++;
+                      for (unsigned int vertex_index = 0;
+                           vertex_index < face->n_vertices();
+                           vertex_index++)
+                        {
+                          n_vertices_local++;
+                          auto   v = face->vertex(vertex_index);
+                          double radius_current =
+                            v.distance(mortar_parameters.center_of_rotation);
+                          radius_min = std::min(radius_min, radius_current);
+                          radius_max = std::max(radius_max, radius_current);
+                        }
                     }
                 }
             }
         }
     }
-  
+
+  // Total number of faces
   const unsigned int n_subdivisions =
-    Utilities::MPI::sum(n_subdivisions_active, dof_handler.get_triangulation().get_communicator());
+    Utilities::MPI::sum(n_subdivisions_local,
+                        dof_handler.get_triangulation().get_communicator());
 
-  /* Compute radius of rotor domain */
-  for (const auto &face : dof_handler.get_triangulation().active_face_iterators())
-    {
-      if (face->at_boundary())
-        {
-          if (face->boundary_id() == mortar_parameters.rotor_boundary_id)
-            {
-              for (unsigned int vertex_index = 0;
-                   vertex_index < face->n_vertices();
-                   vertex_index++)
-                {
-                  auto v = face->vertex(vertex_index);
-                  radius_vec.emplace_back(
-                    hypot((v[0] - mortar_parameters.center_of_rotation[0]),
-                          (v[1] - mortar_parameters.center_of_rotation[1])));
-                }
-            }
-        }
-    }
-
-  auto diff = std::abs(*std::max_element(radius_vec.begin(), radius_vec.end()) -
-                       *std::min_element(radius_vec.begin(), radius_vec.end()));
+  // Min and max values over all processes
+  radius_min =
+    Utilities::MPI::min(radius_min,
+                        dof_handler.get_triangulation().get_communicator());
+  radius_max =
+    Utilities::MPI::max(radius_max,
+                        dof_handler.get_triangulation().get_communicator());
 
   AssertThrow(
-    diff < tolerance,
+    std::abs(radius_max - radius_min) < tolerance,
     ExcMessage(
       "The computed radius of the rotor mesh has a variation greater than the tolerance across the rotor domain, meaning that the prescribed center of rotation and the rotor geometry are not in accordance."));
 
-  double radius =
-    std::reduce(radius_vec.begin(), radius_vec.end()) / radius_vec.size();
-
-  std::cout << __LINE__ << std::endl;
-  std::cout << n_subdivisions << std::endl;
+  // Final radius value
+  const double radius = radius_min;
 
   init(mapping,
        dof_handler,
