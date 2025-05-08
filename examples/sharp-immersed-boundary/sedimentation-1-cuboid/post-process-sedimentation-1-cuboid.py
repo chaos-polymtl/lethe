73252899d1e87c99faa7b17896a0259258dcb35f
JustinLamouche <135653039+Julamipolymtl@users.noreply.github.com>
2025-05-08 07:57:39 -0400

    Add Cuboid Sedimentation Example Present in Lethe Paper (#1508)
    Description
    Add an example for the cuboid sedimentation example from the Lethe 1.0 paper. In this example we showcase the sedimentation of a cuboid (non-spherical) particle dropped into a water-glycol solution and compare to experimental results from the S18 case of Wang et al.
    
    Co-authored-by: Olivier Guévremont <guevremont.o@gmail.com>
    Co-authored-by: Bruno Blais <blais.bruno@gmail.com>
    Co-authored-by: OresteMarquis <83602443+OresteMarquis@users.noreply.github.com>

---
+++ examples/sharp-immersed-boundary/sedimentation-1-cuboid/post-process-sedimentation-1-cuboid.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from cycler import cycler
+
+colors=['#1b9e77','#d95f02','#7570b3','#e7298a','#66a61e','#e6ab02']
+plt.rcParams['axes.prop_cycle'] = cycler(color = colors)
+plt.rcParams['figure.facecolor'] = 'white'
+plt.rcParams['figure.figsize'] = (10,8)
+plt.rcParams['lines.linewidth'] = 4
+plt.rcParams['lines.markersize'] = '11'
+plt.rcParams['markers.fillstyle'] = "none"
+plt.rcParams['lines.markeredgewidth'] = 2
+plt.rcParams['legend.columnspacing'] = 2
+plt.rcParams['legend.handlelength'] = 3
+plt.rcParams['legend.handletextpad'] = 0.2
+plt.rcParams['legend.frameon'] = True
+plt.rcParams['legend.fancybox'] = False
+plt.rcParams['xtick.major.width'] = 2
+plt.rcParams['xtick.major.size'] = 5
+plt.rcParams['ytick.major.size'] = 5
+plt.rcParams['ytick.major.width'] = 2
+plt.rcParams['font.size'] = '25'
+plt.rcParams['font.family']='DejaVu Serif'
+plt.rcParams['font.serif']='cm'
+plt.rcParams['savefig.bbox']='tight'
+
+B = 2
+vol = B**3
+rho = 0.00759
+rho_0= 0.00124
+mass = vol * rho
+I= mass*B**2 / 6
+g=981
+
+Uc = np.sqrt(4./3. * g * B * np.abs(rho-rho_0)/rho_0 )
+tc = B / Uc 
+t_fact = 1/tc
+
+
+mat=np.loadtxt("out/ib_force.00.dat",skiprows=1)
+t=mat[:,1]
+vy=np.abs(mat[:,15])
+
+experimental_data = np.loadtxt("S18.dat",skiprows=1)
+plt.plot(experimental_data[:,0],experimental_data[:,1],'ks', mfc='none',label="Wang et al.")
+plt.plot(t*t_fact,vy,color=colors[0],lw=3, label="Lethe")
+
+plt.ylabel("Sedimentation velocity, $v_y$ [cm/s]")
+plt.xlabel("Dimensionless time, $t^*$ [-]")
+plt.legend()
+
+plt.savefig("velocity-comparison.png", dpi=300)
+plt.show()
