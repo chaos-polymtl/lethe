99c669c43689ae3041c89344f84c9db3b77db717
Bruno Blais <blais.bruno@gmail.com>
2025-07-21 10:57:41 -0400

    remove two useless checks

--- tests/dem/integration_schemes_accuracy.cc
+++ tests/dem/integration_schemes_accuracy.cc
@@ -98,16 +98,7 @@ test()
   std::vector<double>       MOI;
 
   particle_handler.sort_particles_into_subdomains_and_cells();
-#if (DEAL_II_VERSION_MAJOR < 10 && DEAL_II_VERSION_MINOR < 4)
-  {
-    unsigned int max_particle_id = 0;
-    for (const auto &particle : particle_handler)
-      max_particle_id = std::max(max_particle_id, particle.get_id());
-    force.resize(max_particle_id + 1);
-  }
-#else
   force.resize(particle_handler.get_max_local_particle_index());
-#endif
   torque.resize(force.size());
   MOI.resize(force.size());
   MOI[0] = 1.;
