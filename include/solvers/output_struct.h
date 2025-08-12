eaa60230a26cd7ecba0739b2a6e88517447e82a3
voferreira <voferreira@outlook.com>
2025-08-11 23:15:34 -0400

    Refactor output in its own file and use variant of type OutputStruct

--- include/solvers/output_struct.h
+++ include/solvers/output_struct.h
@@ -1,15 +1,20 @@
-//
-// Created by victor on 2025-08-11.
-//
+// SPDX-FileCopyrightText: Copyright (c) 2025 The Lethe Authors
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
 
-#ifndef LETHE_OUTPUT_STRUCT_H
-#define LETHE_OUTPUT_STRUCT_H
+#ifndef lethe_output_struct_h
+#define lethe_output_struct_h
+
+#include <core/vector.h>
+
+#include <solvers/postprocessors.h>
+
 
 /**
  * @brief Struct containing information about the solution output.
  * It is used to pass all the information required upon calling
  * add_data_vector() for the DataOut instance without losing track of its
- * attributes such as name and data component interpretation.
+ * attributes such as name and data component interpretation. This version of
+ * the struct uses Postprocessors.
  */
 template <int dim, typename VectorType>
 struct OutputStructPostprocessor
@@ -25,11 +30,24 @@ struct OutputStructPostprocessor
   OutputStructPostprocessor(
     const VectorType                              &solution,
     const std::shared_ptr<DataPostprocessor<dim>> &data_postprocessor)
-    : dof_handler(nullptr)
-    , solution(solution)
+    : solution(solution)
     , data_postprocessor(data_postprocessor)
-    , is_postprocessor(true)
   {}
+  const VectorType                              &solution;
+  const std::shared_ptr<DataPostprocessor<dim>> &data_postprocessor;
+};
+
+/**
+ * @brief Struct containing information about the solution output.
+ * It is used to pass all the information required upon calling
+ * add_data_vector() for the DataOut instance without losing track of its
+ * attributes such as name and data component interpretation. This version of
+ * the struct uses the DoF handler, the present solution field, and the data
+ * component interpretation vector.
+ */
+template <int dim, typename VectorType>
+struct OutputStructDoFHandler
+{
   /**
    * @brief Constructor for when data is not related to a data_postprocessor.
    *
@@ -42,7 +60,7 @@ struct OutputStructPostprocessor
    * @param[in] data_component_interpretation Vector with data component
    * interpretation of the solution.
    */
-  OutputStruct(
+  OutputStructDoFHandler(
     const DoFHandler<dim>          &dof_handler,
     const VectorType               &solution,
     const std::vector<std::string> &solution_names,
@@ -52,19 +70,16 @@ struct OutputStructPostprocessor
     , solution(solution)
     , solution_names(solution_names)
     , data_component_interpretation(data_component_interpretation)
-    , is_postprocessor(false)
-    , data_postprocessor(nullptr)
-  {
-    const DoFHandler<dim> &dof_handler_ref =
-      dynamic_cast<const DoFHandler<dim> &>(dof_handler);
-  }
-  const DoFHandler<dim>   &dof_handler;
-  const VectorType        &solution;
-  std::vector<std::string> solution_names;
-  std::vector<DataComponentInterpretation::DataComponentInterpretation>
-                                                 data_component_interpretation;
-  const std::shared_ptr<DataPostprocessor<dim>> &data_postprocessor;
-  bool                                           is_postprocessor;
+  {}
+  const DoFHandler<dim>         &dof_handler;
+  const VectorType              &solution;
+  const std::vector<std::string> solution_names;
+  const std::vector<DataComponentInterpretation::DataComponentInterpretation>
+    data_component_interpretation;
 };
 
-#endif // LETHE_OUTPUT_STRUCT_H
+template <int dim, typename VectorType>
+using OutputStruct = std::variant<OutputStructPostprocessor<dim, VectorType>,
+                                  OutputStructDoFHandler<dim, VectorType>>;
+
+#endif
