a36f47404a5c1671ea1c8e0a86ccdd48d32c6a8a
Amishga Alphonius <107414376+AmishgaAlphonius@users.noreply.github.com>
2025-09-04 16:33:55 -0400

    Add favicon (#1662)
    Description
    This PR adds a favicon to the landing page and the user documentation. The addition of the favicon to the doxygen documentation is not trivial and it will done in a subsequent PR.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Lethe'
-copyright = '2021, Lethe'
+copyright = '2021-2025, Lethe'
 author = 'Lethe-CFD'
 
 # The full version, including alpha/beta/rc tags
@@ -62,6 +62,9 @@ html_theme = 'furo'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Set favicon
+html_favicon = '_static/lethe_favicon.ico'
 
 latex_engine = 'xelatex'
 latex_elements = {
