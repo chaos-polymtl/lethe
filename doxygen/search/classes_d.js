bfa5f0139676b8dfc78b0e2d31846bd8c265a243
blaisb <blais.bruno@gmail.com>
2025-09-09 05:53:44 +0000

    Deploying to gh-pages-v3 from @ chaos-polymtl/lethe@7cf70d12eb842abf14194a01d5257b50a5776150 🚀

--- doxygen/search/classes_d.js
+++ doxygen/search/classes_d.js
@@ -3,6 +3,7 @@ var searchData=
   ['objectproperties_0',['ObjectProperties',['../structIBParticlesDEM_1_1ObjectProperties.html',1,'IBParticlesDEM']]],
   ['ongoingcollisionlog_1',['OngoingCollisionLog',['../classOngoingCollisionLog.html',1,'']]],
   ['opencascadeshape_2',['OpenCascadeShape',['../classOpenCascadeShape.html',1,'']]],
-  ['outputstructpostprocessor_3',['OutputStructPostprocessor',['../structOutputStructPostprocessor.html',1,'']]],
-  ['outputstructsolution_4',['OutputStructSolution',['../structOutputStructSolution.html',1,'']]]
+  ['outputstructcellvector_3',['OutputStructCellVector',['../structOutputStructCellVector.html',1,'']]],
+  ['outputstructpostprocessor_4',['OutputStructPostprocessor',['../structOutputStructPostprocessor.html',1,'']]],
+  ['outputstructsolution_5',['OutputStructSolution',['../structOutputStructSolution.html',1,'']]]
 ];
--- doxygen/search/typedefs_9.js
+++ doxygen/search/typedefs_9.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['operatortype_0',['OperatorType',['../classMFNavierStokesPreconditionGMGBase.html#a9f9732358468a055aa6d38c0d547fef5',1,'MFNavierStokesPreconditionGMGBase']]],
-  ['outputstruct_1',['OutputStruct',['../output__struct_8h.html#a182ecebe01bc65dd24b4785174e0c11b',1,'output_struct.h']]]
+  ['outputstruct_1',['OutputStruct',['../output__struct_8h.html#a24c1f4e015db04387ac9c28a58ee7378',1,'output_struct.h']]]
 ];
