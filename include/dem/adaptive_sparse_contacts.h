e6cb80fd3bd10a38bb4c4e486c308a83c0e3373a
Bruno Blais <blais.bruno@gmail.com>
2025-09-11 08:27:38 +0200

    Modify license and add changelog entry

--- include/dem/adaptive_sparse_contacts.h
+++ include/dem/adaptive_sparse_contacts.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright (c) 2024 The Lethe Authors
+// SPDX-FileCopyrightText: Copyright (c) 2024-2025 The Lethe Authors
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
 
 #ifndef lethe_adaptive_sparse_contacts_h
