1f48f38a3763dbbb228d7eee33d39bb2bb25c9e2
antonellasuccar <antonella.succar@polymtl.ca>
2025-09-15 15:30:16 -0400

    Started adding boundary vertices gathering function

--- include/dem/find_boundary_cells_information.h
+++ include/dem/find_boundary_cells_information.h
@@ -33,7 +33,7 @@ public:
    * @brief The build function builds all the boundary cell information
    * structure that finds which cell has a boundary face, boundary line or a
    * boundary point. Additionally, it identifies which cells are located in the
-   * vicinity (controlled by the max cell diameter of floating walls.
+   * vicinity (controlled by the max cell diameter) of floating walls.
    *
    * @param triangulation Triangulation to access the information of the cells
    * @param floating_wall_properties Properties of floating walls specified in
