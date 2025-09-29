39b929f1fad9da1bea6d22ff62d43d07419357a4
Bruno Blais <blais.bruno@gmail.com>
2025-09-29 15:38:09 +0200

    Apply suggestion from @antonellasuccar
    Co-authored-by: Antonella Succar <142912677+antonellasuccar@users.noreply.github.com>

--- include/core/boundary_conditions.h
+++ include/core/boundary_conditions.h
@@ -697,7 +697,7 @@ namespace BoundaryConditions
     AssertThrow(
       boundary_ids.size() > 0,
       ExcMessage(
-        "A boundary id has not been set for one of the fluid dynamics boundary condition. Please ensure that the id is set for every boundary condition."));
+        "A boundary id has not been set for one of the heat transfer boundary conditions. Please ensure that the id is set for every boundary condition."));
 
     // Loop through all boundary ids to ensure that they are all non-negative
     // and unique
