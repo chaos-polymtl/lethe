a38ee57f79df060c7f905d0b90ba6352b92451f9
Bruno Blais <blais.bruno@gmail.com>
2025-10-06 17:35:51 +0200

    Ident

--- tests/fem-dem/particle_projector_02.cc
+++ tests/fem-dem/particle_projector_02.cc
@@ -226,22 +226,24 @@ test_void_fraction_qcm(const unsigned int fe_degree,
     }
 
   // Integrate the force field over the cells to check force conservation
-  FEValues<3> fe_values(*particle_projector.mapping,
-                        *particle_projector.particle_fluid_force_without_drag.fe,
-                        *particle_projector.quadrature,
-                        update_values | update_JxW_values);
+  FEValues<3> fe_values(
+    *particle_projector.mapping,
+    *particle_projector.particle_fluid_force_without_drag.fe,
+    *particle_projector.quadrature,
+    update_values | update_JxW_values);
 
   // Field extractor if dim components are used
   FEValuesExtractors::Vector vector_extractor;
   vector_extractor.first_vector_component = 0;
   Tensor<1, 3>              total_particle_force_on_fluid({0, 0, 0});
   std::vector<Tensor<1, 3>> force_values(fe_values.n_quadrature_points);
-  for (auto cell : particle_projector.particle_fluid_force_without_drag.dof_handler
-                     .active_cell_iterators())
+  for (auto cell : particle_projector.particle_fluid_force_without_drag
+                     .dof_handler.active_cell_iterators())
     {
       fe_values.reinit((cell));
       fe_values[vector_extractor].get_function_values(
-        particle_projector.particle_fluid_force_without_drag.particle_field_solution,
+        particle_projector.particle_fluid_force_without_drag
+          .particle_field_solution,
         force_values);
       for (unsigned int q = 0; q < fe_values.n_quadrature_points; ++q)
         total_particle_force_on_fluid +=
@@ -260,7 +262,8 @@ test_void_fraction_qcm(const unsigned int fe_degree,
                                "void_fraction");
       data_out.add_data_vector(
         particle_projector.particle_fluid_force_without_drag.dof_handler,
-        particle_projector.particle_fluid_force_without_drag.particle_field_solution,
+        particle_projector.particle_fluid_force_without_drag
+          .particle_field_solution,
         "force_pf");
       data_out.build_patches();
 
