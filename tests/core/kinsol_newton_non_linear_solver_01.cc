bbb546ed1e84caf4077109fe0899dd10a70249c0
Bruno Blais <blais.bruno@gmail.com>
2025-11-06 15:18:26 +0100

    Fix copyright

--- tests/core/kinsol_newton_non_linear_solver_01.cc
+++ tests/core/kinsol_newton_non_linear_solver_01.cc
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright (c) 2019-2021, 2024 The Lethe Authors
+// SPDX-FileCopyrightText: Copyright (c) 2019-2025 The Lethe Authors
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
 
 /**
