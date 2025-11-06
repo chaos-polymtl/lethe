925f721d74d71d246bcbd21e9c5b9c97f5e5cd1b
Olivier Guévremont <guevremont.o@gmail.com>
2025-11-06 15:06:29 -0500

    Fix clang v20 for lethe_grid_tool tests (#1783)
    Fix clang v20 warnings for lethe_grid_tool tests

--- tests/core/lethe_grid_tool_mesh_cut_by_flat_3.cc
+++ tests/core/lethe_grid_tool_mesh_cut_by_flat_3.cc
@@ -14,6 +14,8 @@
 
 #include <deal.II/numerics/data_out.h>
 
+#include <algorithm>
+
 // This include is only required for 9.7 and above.
 #if DEAL_II_VERSION_GTE(9, 7, 0)
 #  include <deal.II/grid/cell_data.h>
@@ -80,14 +82,13 @@ test()
                                                           flat_cell,
                                                           vertice_to_cell);
 
-  std::sort(cells_cut.begin(),
-            cells_cut.end(),
-            [](typename DoFHandler<3>::active_cell_iterator cell_1,
-               typename DoFHandler<3>::active_cell_iterator cell_2) {
-              return cell_1->global_active_cell_index() <
-                     cell_2->global_active_cell_index();
-            });
-
+  std::ranges::sort(
+    cells_cut,
+    [](const typename DoFHandler<3>::active_cell_iterator &cell_1,
+       const typename DoFHandler<3>::active_cell_iterator &cell_2) {
+      return cell_1->global_active_cell_index() <
+             cell_2->global_active_cell_index();
+    });
 
   for (auto &cell : cells_cut)
     {
