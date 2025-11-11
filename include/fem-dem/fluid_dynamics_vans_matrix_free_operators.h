5899777ec3df854f687afbc34e149c8a688bcfc8
Bruno Blais <blais.bruno@gmail.com>
2025-11-11 09:50:45 -0500

    Update include/fem-dem/fluid_dynamics_vans_matrix_free_operators.h
    Co-authored-by: Antonella Succar <142912677+antonellasuccar@users.noreply.github.com>

--- include/fem-dem/fluid_dynamics_vans_matrix_free_operators.h
+++ include/fem-dem/fluid_dynamics_vans_matrix_free_operators.h
@@ -148,7 +148,7 @@ protected:
   Table<2, Tensor<1, dim, VectorizedArray<number>>> particle_fluid_force;
 
   /// Table with correct alignment for vectorization to store the values of the
-  /// particle-fluid drag.
+  /// particle-fluid drag. The drag force in this table is volumetric, resulting from the projection used.
   Table<2, Tensor<1, dim, VectorizedArray<number>>> particle_fluid_drag;
 
   /// Table with correct alignment for vectorization to store the values of the
