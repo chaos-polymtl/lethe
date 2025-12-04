1a0db1620137d3db7fe3dcec2d42c9cfd9fd3a19
Olivier Gaboriault <oliviergabo@gmail.com>
2025-12-04 14:55:04 -0500

    Updated test description

--- tests/dem/distribution_lognormal.cc
+++ tests/dem/distribution_lognormal.cc
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
 
 /**
- * @brief Inserting particles following a normal distribution.
+ * @brief Inserting particles following a lognormal distribution.
  */
 
 // Deal.II includes
