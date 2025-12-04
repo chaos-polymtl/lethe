9a3aa1a8e5ad79dfb4c3014e4d26ab955fac5e2e
antonellasuccar <antonella.succar@polymtl.ca>
2025-12-04 18:22:05 -0500

    Update example repo

--- examples/unresolved-cfd-dem/gas-solid-fluidized-bed-cylindrical/plot-pressure-2.py
+++ examples/unresolved-cfd-dem/gas-solid-fluidized-bed-cylindrical/plot-pressure-2.py
@@ -13,96 +13,114 @@ colors=['#1b9e77','#d95f02','#7570b3','#e7298a','#66a61e','#e6ab02']
 
 plt.rcParams['axes.prop_cycle'] = cycler(color=colors)
 
-t,p,p2 = np.loadtxt("output_modelB_test/pressure_drop.dat",unpack=True,skiprows=1)
-tA,pA,p2A = np.loadtxt("output_modelA/pressure_drop.dat",unpack=True,skiprows=1)
-# t,p,p2 = np.loadtxt("output_ramp/pressure_drop.dat",unpack=True,skiprows=1)
+# Helper functions
+def bin_and_average(time, pressure, bins, averaging_window):
+    indices = np.digitize(time, bins) - 1
+    mean_p, std_p = [], []
+    for i in range(len(bins)-1):
+        mask = (indices == i) & (time > (bins[i+1] - averaging_window))
+        if np.any(mask):
+            mean_p.append(pressure[mask].mean())
+            std_p.append(pressure[mask].std())
+        else:
+            mean_p.append(np.nan)
+            std_p.append(np.nan)
+    return np.array(mean_p), np.array(std_p)
 
+# === Plotting function ===
+def plot_pressure(Re, dataset_keys, labels):
+    for key, label in zip(dataset_keys, labels):
+        plt.errorbar(Re, datasets[key]["mean"], yerr=datasets[key]["std"], fmt='o-', capsize=3, label=label)
+    plt.plot([0, max(Re)], [delta_p_analytical]*2, 'k--', label="Analytical Δp")
+    plt.plot([Re_mf_WY_inlet]*2, [0, max([datasets[k]["mean"].max() for k in dataset_keys])*1.1], ':', label="Wen-Yu")
+    plt.plot([Re_mf_N_inlet]*2, [0, max([datasets[k]["mean"].max() for k in dataset_keys])*1.1], '-.', label="Noda")
+    plt.xlabel("Re")
+    plt.ylabel("$\\Delta p$")
+    plt.legend()
+    plt.show()
+
+
+# === Load data ===
+data_files = {
+    "A_MB": "output_modelA_mb/pressure_drop.dat",
+    "B_MB": "output_modelB_mb/pressure_drop.dat",
+    "A_MB_proj": "output_modelA_project_forces/pressure_drop.dat",
+    "B_MB_proj": "output_modelB_project_forces/pressure_drop.dat",
+    "A_MF": "output_modelA_mf/pressure_drop.dat",
+    "A_MF_ramp_bdf2": "output_modelA_mf_ramp/pressure_drop.dat",
+    "A_MF_ramp": "output_modelA_mf_ramp_bdf2/pressure_drop.dat"
+}
+
+datasets = {}
+for key, path in data_files.items():
+    t, p, _ = np.loadtxt(path, unpack=True, skiprows=1)
+    datasets[key] = {"t": t, "p": p}
+    
+# Column diameter
 D= 0.02
 # Bed cross sectional area
 Ab = np.pi*(D/2)**2
+
+# Particle properties
 dp = 5e-4
 Vp = 4/3*np.pi*(dp/2)**3
-Np = 200000
-nu = 1e-5
-g = 9.81
 rho_p = 1000
+Np = 200000
+
+# Fluid properties
+nu = 1e-5
 rho_f = 1
 mu = nu*rho_f
+
+g = 9.81
 Ar =  g * rho_f * (rho_p-rho_f)*dp**3 / mu**2
 print("Ar: ", Ar)
+
+# Minimum fluidization velocity
 Re_mf_WY = (33.7**2+0.0408*Ar)**0.5 - 33.7
 U_mf_WY = Re_mf_WY * nu / dp 
 Re_mf_WY_inlet = U_mf_WY * D/nu
-print("Wen-Yu")
-print("  Re_p: ",Re_mf_WY)
-print("  U_mf: ",U_mf_WY)
+
 Re_mf_N = (19.29**2+0.0276*Ar)**0.5 - 19.29
 U_mf_N = Re_mf_N * nu / dp
 Re_mf_N_inlet= U_mf_N * D/nu
-print("Noda")
-print("  Re_p: ",Re_mf_N)
-print("  U_mf: ",U_mf_N)
+
+print("Wen-Yu: Re_p =", Re_mf_WY, "U_mf =", U_mf_WY)
+print("Noda: Re_p =", Re_mf_N, "U_mf =", U_mf_N)
 
 # Analytical pressure drop
 delta_p_analytical = Np*Vp*(rho_p-rho_f)*g / Ab
 print("Analytical pressure drop: ", delta_p_analytical)
 
+# Calculating the pressure drop at each velocity step for all datasets
+
 # Time interval for each velocity
 delta_t = 0.05
-# delta_t = 0.001
+averaging_window = 0.025 # Time interval over which the pressure drop is averaged
 
-exp_p=408.34
-
-t_min = t.min()
-t_max = t.max()
-
-# Bin edges from t_min to t_max with step delta_t
+t_min = min(ds["t"].min() for ds in datasets.values())
+t_max = max(ds["t"].max() for ds in datasets.values())
 bins = np.arange(t_min, t_max + delta_t, delta_t)
-indices = np.digitize(t, bins) - 1
+bin_centers = (bins[:-1] + bins[1:]) / 2
 
-mean_p = []
-std_p = []
-bin_centers = []
+# Compute pressure drop at each velocity step
+for key, ds in datasets.items():
+    mean_p, std_p = bin_and_average(ds["t"], ds["p"], bins, averaging_window)
+    datasets[key]["mean"] = mean_p
+    datasets[key]["std"] = std_p
 
-for i in range(len(bins)-1):
-    mask = indices == i
-    if np.any(mask):  # check if there are points in this bin
-        mean_p.append(p[mask].mean())
-        std_p.append(p[mask].std())
-        bin_centers.append((bins[i] + bins[i+1])/2)
-
-bin_centers = np.array(bin_centers)
-print("Bin centers:", bin_centers)
-mean_p = np.array(mean_p)
-std_p = np.array(std_p)
-print("Mean pressures:", mean_p)
-
-mean_p_A = []
-std_p_A = []
-
-for i in range(len(bins)-1):
-    mask = indices == i
-    if np.any(mask):  # check if there are points in this bin
-        mean_p_A.append(pA[mask].mean())
-        std_p_A.append(pA[mask].std())
-
-bin_centers = np.array(bin_centers)
-print("Bin centers:", bin_centers)
-mean_p_A = np.array(mean_p_A)
-std_p_A = np.array(std_p_A)
-print("Mean pressures:", mean_p_A)
-
-velocity = np.minimum(0.02 * np.floor(bin_centers / 0.05 + 1), 0.2)
-# velocity = np.minimum(0.2 * bin_centers, 0.2)
+# Reynolds numbers for x-axis
+velocity = np.minimum(0.02 * np.floor(bin_centers / 0.05 + 1), 0.4)
 Re = velocity * D / nu
-print("Reynolds numbers:", Re)
 
-plt.plot(Re, mean_p, label="Model B")
-# plt.plot(Re, mean_p_A, label="Model A")
-plt.plot([0,max(Re)],[delta_p_analytical,delta_p_analytical],'k--',label="Analytical pressure drop")
-plt.plot([Re_mf_WY_inlet, Re_mf_WY_inlet],[0,450],':',label="Wen-Yu")
-plt.plot([Re_mf_N_inlet, Re_mf_N_inlet],[0,450],'-.',label="Noda")
-plt.xlabel("Re")
-plt.ylabel("$\\Delta p$")
-plt.legend()
-plt.show()
+plot_pressure(
+    Re,
+    ["A_MB", "A_MF", "B_MB"],
+    ["Model A MB", "Model A MF", "Model B MB"]
+)
+
+plot_pressure(
+    Re,
+    ["A_MB_proj", "A_MF", "B_MB_proj"],
+    ["Model A MB-proj", "Model A MF", "Model B MB-proj"]
+)
