6794ff7b4ebd32190cc3e6bb57d6bf96b1ba9287
Olivier Gaboriault <oliviergabo@gmail.com>
2025-12-10 14:36:13 -0500

    Add the message to the cfd-dem solvers

--- source/fem-dem/cfd_dem_coupling_matrix_free.cc
+++ source/fem-dem/cfd_dem_coupling_matrix_free.cc
@@ -78,6 +78,8 @@ CFDDEMMatrixFree<dim>::setup_distribution_type()
                 lpp.seed_for_distributions[particle_type] + this_mpi_process);
             break;
         }
+      size_distribution_object_container[particle_type]
+        ->print_psd_declaration_string(particle_type, this->pcout);
 
       maximum_particle_diameter = std::max(
         maximum_particle_diameter,
