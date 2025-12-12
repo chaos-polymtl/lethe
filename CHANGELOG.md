55662d386416d6a17fc1fb878f5013350fc0beb8
hepap <47506601+hepap@users.noreply.github.com>
2025-12-12 17:05:25 -0500

    Improve extract slice tool (#1848)
    Description
    The extract-slice-from-vtu.py tool was not working properly with --np > 1 because an argument (file path) was missing in the call of extract_slice.
    
    When the flag group files in the .prm was set to >1, the extract-slice-from-vtu.py would not slice all .vtus associated with a .pvtu.
    
    There was no sliced .pvd file outputted, so the slices could not be used easily in other post-processing tools using pvd.
    
    Solution
    The missing argument was added.
    
    pyvista can read pvtus correctly, so the list of files to read is based on pvtus instead of vtus.
    
    The pvd file of the slices is now outputted.
    
    Testing
    No new test added, it works now in parallel and with pvtus.
    
    Documentation
    No documentation related.
    
    Co-authored-by: Bruno Blais <blais.bruno@gmail.com>
    Co-authored-by: Olivier Guévremont <guevremont.o@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -37,3 +37,9 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/).
 ### Fixed
 
 - MAJOR/MINOR/PATCH Description (#PR).
+
+## [Master] - 2025/12/10
+
+### Fixed
+
+- MINOR The extract-slice-from-vtu.py tool was not working properly with --np > 1 because an argument (file path) was missing in the call of extract_slice. This PR fixes this bug and add 2 functionalities. 1) When the flag group files in the .prm (parallel output of Lethe) was set to a value larger than 1, the extract-slice-from-vtu.py would not slice all .vtus associated with a .pvtu. Since pyvista can read .pvtus correctly, the list of files to read is now based on .pvtus instead of .vtus. 2) There was no sliced .pvd file generated, so the slices could not be used easily in other post-processing tools using .pvds. The .pvd file of the slices is now generated. [#1848] (https://github.com/chaos-polymtl/lethe/pull/1848)
--- contrib/postprocessing/extract-from-vtu/extract-slice-from-vtu.py
+++ contrib/postprocessing/extract-from-vtu/extract-slice-from-vtu.py
@@ -25,7 +25,7 @@ from tqdm import tqdm
 FUNCTIONS
 """
 
-def get_vtus_path_from_pvd(pvd_file_path: str) -> list:
+def get_pvtus_path_from_pvd(pvd_file_path: str) -> list:
     """
     Extract the .vtu files if a .pvd file is provided.
 
@@ -38,39 +38,38 @@ def get_vtus_path_from_pvd(pvd_file_path: str) -> list:
     root = tree.getroot()
     pvtu_files = [dataset.get("file") for dataset in root.findall(".//DataSet")]
 
-    # Extract .vtu files from each .pvtu file
-    vtu_files = []
+    # Extract .pvtu file
+    pvtu_files_complete = []
     for pvtu_file in pvtu_files:
-        tree = ET.parse(os.path.join(os.path.dirname(pvd_file_path), pvtu_file))
-        root = tree.getroot()
-        for piece in root.findall(".//Piece"):
-            vtu_file = piece.get("Source")  # Extract the linked .vtu file from the "Source" attribute
-            if vtu_file:
-                vtu_files.append(os.path.join(os.path.dirname(pvd_file_path), vtu_file))
+        pvtu_files_complete.append(os.path.join(os.path.dirname(pvd_file_path), pvtu_file))
 
-    return vtu_files
+    return pvtu_files_complete
 
-def extract_slice(vtu_file_path: list, origin: np.array, normal: np.array, save_path: str) -> None:
+def extract_slice(file_path: list, origin: np.array, normal: np.array, save_path: str) -> None:
     """
     Create a slice from a .vtu file using the pyvista library and save it in a new .vtk file.
 
-    :param vtu_file_path: the path to the .vtu file
+    :param file_path: the path to the .vtu or .pvtu file
     :param origin: the coordinates of a point on the slice
     :param normal: the normal direction to the slice
     :param save_path: the path to the sliced vtu save folder
     """
 
     # slice the file
-    sliced_solution = pv.read(vtu_file_path).slice(normal=normal, origin=origin, generate_triangles=True)
+    sliced_solution = pv.read(file_path).slice(normal=normal, origin=origin, generate_triangles=True)
     sliced_solution = sliced_solution.cast_to_unstructured_grid()
 
     # If the slice is empty (the slice does not intersect the domain), return
     if sliced_solution.n_points == 0:
-        print(f"Slice is empty for file {vtu_file_path}")
+        print(f"Slice is empty for file {file_path}")
         return
 
     # Modify the file name and save the file
-    file_name = os.path.basename(vtu_file_path)
+    file_name = os.path.basename(file_path)
+
+    # If a .pvtu file was provided, change the extension to .vtu, because this tool writes the solution in .vtu file format
+    file_name=file_name.replace(".pvtu", ".vtu")
+
     sliced_solution.save(save_path+"/sliced-" + file_name)
 
     return
@@ -98,17 +97,31 @@ def run()-> None:
 
     print("Slicing vtus using point: ", origin, " and normal: ", normal)
 
-    if args.file.endswith(".vtu"):
+    if (args.file.endswith(".vtu") or args.file.endswith(".pvtu")):
         extract_slice(args.file, origin=origin, normal=normal, save_path=args.save_folder)
     elif args.file.endswith(".pvd"):
-        vtu_files_path = get_vtus_path_from_pvd(args.file)
+        pvtu_files_path = get_pvtus_path_from_pvd(args.file)
         if args.np > 1:
             pool = Pool(args.np)
-            list(tqdm(pool.imap(partial(extract_slice, origin=origin, normal=normal), vtu_files_path), 
-                        total=len(vtu_files_path), desc="Processing VTU files"))
+            list(tqdm(pool.imap(partial(extract_slice, origin=origin, normal=normal, save_path=args.save_folder), pvtu_files_path), 
+                        total=len(pvtu_files_path), desc="Processing PVTU files"))
         else:
-            for vtu_file_path in tqdm(vtu_files_path, desc="Processing VTU files"):
-                extract_slice(vtu_file_path, origin, normal, args.save_folder)
+            for pvtu_file_path in tqdm(pvtu_files_path, desc="Processing PVTU files"):
+                extract_slice(pvtu_file_path, origin, normal, args.save_folder)
+        
+        # Write the sliced .pvd
+        sliced_pvd_file_name = args.save_folder+"/sliced-"+ os.path.basename(args.file)
+        sliced_pvd_file = open(sliced_pvd_file_name, "w")
+        pvd_file = open(args.file,"r")
+
+        pvd_file_lines = pvd_file.read()
+        pvd_file_lines = pvd_file_lines.replace('file="','file="sliced-')
+        pvd_file_lines = pvd_file_lines.replace(".pvtu", ".vtu")
+        sliced_pvd_file.write(pvd_file_lines)
+
+        sliced_pvd_file.close()
+        pvd_file.close()
+
     else:
         print("Invalid file format, please provide a .vtu or a .pvd file")
 
