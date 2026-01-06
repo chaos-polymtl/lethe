c1c7b0a1d4c547a89edae2dc948948b64089ec57
blaisb <blais.bruno@gmail.com>
2026-01-06 11:10:17 +0000

    Deploying to gh-pages-v3 from @ chaos-polymtl/lethe@2806ff6d6376a698c4fe1647b6949f0b8d8b00c7 🚀

--- doxygen/search/enumvalues_c.js
+++ doxygen/search/enumvalues_c.js
@@ -1,12 +1,13 @@
 var searchData=
 [
-  ['manual_0',['manual',['../namespaceParameters.html#aea64bd5414c051b692d8df44e5ccd369a3c78b35502b2693fefdfc51cba3a53a5',1,'Parameters']]],
-  ['mass_1',['mass',['../namespaceDEM_1_1DEMProperties.html#ad4c9c6eeafe6423a5619617d6c8a5987a46b35361355f5d09246f5c232dd922b5',1,'DEM::DEMProperties::mass'],['../namespaceDEM_1_1CFDDEMProperties.html#ae9734b9d29480269e4764c5092d87b2fac533f124c36197d6291a5145cff179f6',1,'DEM::CFDDEMProperties::mass'],['../namespaceDEM_1_1DEMMPProperties.html#aca0676848b97f3864735e524aac253f8a350708db287c03473fa23b181abd447b',1,'DEM::DEMMPProperties::mass']]],
-  ['mobile_2',['mobile',['../classAdaptiveSparseContacts.html#a9cf65bd3e1a790313fcf3f6b708ab230a845f62dcad807a5258e4c5ed41e1d12e',1,'AdaptiveSparseContacts']]],
-  ['mobility_5fstatus_3',['mobility_status',['../structParameters_1_1Testing.html#ad685a47cec3b7e8562c8b6444e8a9a39abdfd37e8359d01ed10577a9d0d609c15',1,'Parameters::Testing']]],
-  ['modela_4',['modelA',['../namespaceParameters.html#a885ce66c74635a68dd078d94aedddac6aaab653aeeee220006e2b9f692569492c',1,'Parameters']]],
-  ['modelb_5',['modelB',['../namespaceParameters.html#a885ce66c74635a68dd078d94aedddac6ab35bb34924c70e61eec8a8bae8cac690',1,'Parameters']]],
-  ['moe_6',['moe',['../structParameters_1_1Stabilization.html#a94f05a62f14f6e768934d0036a56a79ca7f33334d4c2f6dd6ffc701944cec2f1c',1,'Parameters::Stabilization']]],
-  ['momentum_5ftransfer_5fcoefficient_7',['momentum_transfer_coefficient',['../namespaceDEM_1_1CFDDEMProperties.html#ae9734b9d29480269e4764c5092d87b2fa700c2c8a14a99cc06d10c0231720f7df',1,'DEM::CFDDEMProperties']]],
-  ['multiple_5fstep_5fbdf_8',['multiple_step_bdf',['../structParameters_1_1SimulationControl.html#a5fef9c9888f530e256aed538f319fd23a1c37ff61e0d4dfbb46475ead409a821c',1,'Parameters::SimulationControl']]]
+  ['magnetic_5ffield_0',['magnetic_field',['../namespaceBoundaryConditions.html#a07df76d63cde5cb5bcbea0d9c25a54b8a8732c340b849ed4f600a10f719097755',1,'BoundaryConditions']]],
+  ['manual_1',['manual',['../namespaceParameters.html#aea64bd5414c051b692d8df44e5ccd369a3c78b35502b2693fefdfc51cba3a53a5',1,'Parameters']]],
+  ['mass_2',['mass',['../namespaceDEM_1_1DEMProperties.html#ad4c9c6eeafe6423a5619617d6c8a5987a46b35361355f5d09246f5c232dd922b5',1,'DEM::DEMProperties::mass'],['../namespaceDEM_1_1CFDDEMProperties.html#ae9734b9d29480269e4764c5092d87b2fac533f124c36197d6291a5145cff179f6',1,'DEM::CFDDEMProperties::mass'],['../namespaceDEM_1_1DEMMPProperties.html#aca0676848b97f3864735e524aac253f8a350708db287c03473fa23b181abd447b',1,'DEM::DEMMPProperties::mass']]],
+  ['mobile_3',['mobile',['../classAdaptiveSparseContacts.html#a9cf65bd3e1a790313fcf3f6b708ab230a845f62dcad807a5258e4c5ed41e1d12e',1,'AdaptiveSparseContacts']]],
+  ['mobility_5fstatus_4',['mobility_status',['../structParameters_1_1Testing.html#ad685a47cec3b7e8562c8b6444e8a9a39abdfd37e8359d01ed10577a9d0d609c15',1,'Parameters::Testing']]],
+  ['modela_5',['modelA',['../namespaceParameters.html#a885ce66c74635a68dd078d94aedddac6aaab653aeeee220006e2b9f692569492c',1,'Parameters']]],
+  ['modelb_6',['modelB',['../namespaceParameters.html#a885ce66c74635a68dd078d94aedddac6ab35bb34924c70e61eec8a8bae8cac690',1,'Parameters']]],
+  ['moe_7',['moe',['../structParameters_1_1Stabilization.html#a94f05a62f14f6e768934d0036a56a79ca7f33334d4c2f6dd6ffc701944cec2f1c',1,'Parameters::Stabilization']]],
+  ['momentum_5ftransfer_5fcoefficient_8',['momentum_transfer_coefficient',['../namespaceDEM_1_1CFDDEMProperties.html#ae9734b9d29480269e4764c5092d87b2fa700c2c8a14a99cc06d10c0231720f7df',1,'DEM::CFDDEMProperties']]],
+  ['multiple_5fstep_5fbdf_9',['multiple_step_bdf',['../structParameters_1_1SimulationControl.html#a5fef9c9888f530e256aed538f319fd23a1c37ff61e0d4dfbb46475ead409a821c',1,'Parameters::SimulationControl']]]
 ];
