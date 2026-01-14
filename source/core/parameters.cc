f7a20190a195f1a51bc8e501cff668b8bf32cd0e
Oreste Marquis <oreste.marquis@polymtl.ca>
2026-01-14 13:49:14 -0500

    copyright

--- source/core/parameters.cc
+++ source/core/parameters.cc
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright (c) 2019-2025 The Lethe Authors
+// SPDX-FileCopyrightText: Copyright (c) 2019-2026 The Lethe Authors
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
 
 #include <core/parameters.h>
