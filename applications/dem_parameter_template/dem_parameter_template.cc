e3e0920661be096be39a8a1319650c4fef9b787c
Bruna Campos <bccampos@uwaterloo.ca>
2026-01-19 10:18:47 -0500

    update headers

--- applications/dem_parameter_template/dem_parameter_template.cc
+++ applications/dem_parameter_template/dem_parameter_template.cc
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright (c) 2020-2021, 2024 The Lethe Authors
+// SPDX-FileCopyrightText: Copyright (c) 2020-2021, 2024, 2026 The Lethe Authors
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
 
 // check the read and write of simulationcontrol
--- prototypes/direct_gls_navier_stokes/direct_gls_navier_stokes.cc
+++ prototypes/direct_gls_navier_stokes/direct_gls_navier_stokes.cc
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright (c) 2020, 2024 The Lethe Authors
+// SPDX-FileCopyrightText: Copyright (c) 2020, 2024, 2026 The Lethe Authors
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
 
 #include <deal.II/base/function.h>
