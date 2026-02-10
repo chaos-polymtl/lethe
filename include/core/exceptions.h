603ad57c480a88545ef553e0ad05579ad28bd249
Bruno Blais <blais.bruno@gmail.com>
2026-02-10 12:25:27 +0100

    Document the exceptions and solid objects

--- include/core/exceptions.h
+++ include/core/exceptions.h
@@ -1,9 +1,13 @@
 // SPDX-FileCopyrightText: Copyright (c) 2022-2025 The Lethe Authors
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
 
-/*
- * This file defines the exceptions common to multiple solvers in Lethe
- * to avoid code repetition.
+/**
+ * @file exceptions.h
+ * @brief Common exception declarations shared across multiple solvers in Lethe.
+ *
+ * This file defines deal.II-style exception macros used to report errors
+ * related to the Nitsche immersed boundary method across different solver
+ * types, avoiding code repetition.
  */
 
 #ifndef lethe_exceptions_h
@@ -13,6 +17,14 @@
 
 using namespace dealii;
 
+/**
+ * @brief Exception raised when a solver does not support Nitsche solid
+ * restriction but solids are defined in the parameter file.
+ *
+ * @param arg1 Number of solids defined in the parameter file.
+ * @param arg2 Name of the current solver that does not support Nitsche.
+ * @param arg3 Name of the solver that should be used instead.
+ */
 DeclException3(SolidWarning,
                unsigned int,
                std::string,
@@ -21,6 +33,12 @@ DeclException3(SolidWarning,
                << " solver does not support nitsche restriction. Use " << arg3
                << " solver instead.");
 
+/**
+ * @brief Exception raised when no solid is defined but the solver attempts
+ * to assemble a Nitsche restriction.
+ *
+ * @param arg1 Name of the solver in which the error occurred.
+ */
 DeclException1(
   NoSolidWarning,
   std::string,
