ccc0a7ac7bd92ca30007b0175af1d67df2004746
Amishga Alphonius <107414376+AmishgaAlphonius@users.noreply.github.com>
2026-02-16 11:21:05 -0500

    Improve doxygen documentation interface (#1908)
    Description
    This PR improves the Doxygen documentation interface. We integrate Doxygen Awesome CSS with dark/light mode support as the base theme and extend it with several enhancements:
    
    Add a GitHub widget with live repository statistics (stars and forks).
    Update the main page graph to include the latest applications, highlighted in blue.
    Implement responsive layout adjustments to improve content organization across window sizes.
    Fix the header (including the search bar) to the top of the page for persistent navigation (no more need to scroll back up the page).
    Dynamically display the current Lethe version in the project title (read from CMakeLists.txt).
    Add a CHAOS logo to the footer with copyright and auto-updating year.
    Add direct links to the Lethe project page and the CHAOS Laboratory website in the navigation bar.
    All features are compatible with both dark and light modes.

--- doc/doxygen/main.h
+++ doc/doxygen/main.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright (c) 2023-2025 The Lethe Authors
+// SPDX-FileCopyrightText: Copyright (c) 2023-2026 The Lethe Authors
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
 
 /**
@@ -9,13 +9,32 @@
  *
  * @dot
  *    digraph main_classes {
-      graph [bgcolor="transparent", align=true, ranksep=1.5];
+      graph [bgcolor="transparent", align=true, ranksep=1, nodesep=0.5];
       node [fontname="FreeSans",fontsize=15,
         shape=record,height=0.2,width=0.4,
         color="royalblue", fillcolor="white", style="filled"];
       edge [color="royalblue", weight=10];
       rankdir="LR";
       size = "16,10";
+
+
+      //---------------------------------
+      // Particles Ray Tracing
+      //---------------------------------
+
+      particles_ray_tracing_solver [label=<<B>RayTracingSolver</B> <br/>(lethe-particles-ray-tracing)>, href="https://chaos-polymtl.github.io/lethe/doxygen/classRayTracingSolver.html", tooltip="RayTracingSolver", fillcolor="#8ba5d4"];
+
+
+      //---------------------------------
+      // DEM
+      //---------------------------------
+
+      dem_solver [label=<<B>DEMSolver</B> <br/>(lethe-particles)>, href="https://chaos-polymtl.github.io/lethe/doxygen/classDEMSolver.html", tooltip="DEMSolver", fillcolor="#8ba5d4"];
+
+
+      //---------------------------------
+      // Physics Solver
+      //---------------------------------
 
       physics_solver [label="PhysicsSolver", href="https://chaos-polymtl.github.io/lethe/doxygen/classPhysicsSolver.html"];
 
@@ -26,9 +45,9 @@
       physics_solver:e -> navier_stokes_base:w [dir=back];
       physics_solver:e -> auxiliary_physics:w [dir=back];
 
-      navier_stokes_base_1 [label=<<B>FluidDynamicsMatrixBased</B> <br/>(lethe-fluid)>,href="https://chaos-polymtl.github.io/lethe/doxygen/classFluidDynamicsMatrixBased.html", tooltip="FluidDynamicsMatrixBased"];
-      navier_stokes_base_2 [label=<<B>FluidDynamicsBlock</B> <br/> (lethe-fluid-block)>,href="https://chaos-polymtl.github.io/lethe/doxygen/classFluidDynamicsBlock.html", tooltip="FluidDynamicsBlock"];
-      navier_stokes_base_3 [label=<<B>FluidDynamicsMatrixFree</B> <br/> (lethe-fluid-matrix-free)>,href="https://chaos-polymtl.github.io/lethe/doxygen/classFluidDynamicsMatrixFree.html", tooltip="FluidDynamicsMatrixFree"];
+      navier_stokes_base_1 [label=<<B>FluidDynamicsMatrixBased</B> <br/>(lethe-fluid)>,href="https://chaos-polymtl.github.io/lethe/doxygen/classFluidDynamicsMatrixBased.html", tooltip="FluidDynamicsMatrixBased", fillcolor="#8ba5d4"];
+      navier_stokes_base_2 [label=<<B>FluidDynamicsBlock</B> <br/> (lethe-fluid-block)>,href="https://chaos-polymtl.github.io/lethe/doxygen/classFluidDynamicsBlock.html", tooltip="FluidDynamicsBlock", fillcolor="#8ba5d4"];
+      navier_stokes_base_3 [label=<<B>FluidDynamicsMatrixFree</B> <br/> (lethe-fluid-matrix-free)>,href="https://chaos-polymtl.github.io/lethe/doxygen/classFluidDynamicsMatrixFree.html", tooltip="FluidDynamicsMatrixFree", fillcolor="#8ba5d4"];
 
       navier_stokes_base:e -> navier_stokes_base_1:w [dir=back];
       navier_stokes_base:e -> navier_stokes_base_2:w [dir=back];
@@ -38,25 +57,33 @@
       auxiliary_physics_2 [label="CahnHilliard",href="https://chaos-polymtl.github.io/lethe/doxygen/classCahnHilliard.html"];
       auxiliary_physics_3 [label="HeatTransfer",href="https://chaos-polymtl.github.io/lethe/doxygen/classHeatTransfer.html"];
       auxiliary_physics_4 [label="Tracer",href="https://chaos-polymtl.github.io/lethe/doxygen/classTracer.html"];
+      auxiliary_physics_5 [label="TimeHarmonicMaxwell",href="https://chaos-polymtl.github.io/lethe/doxygen/classTimeHarmonicMaxwell.html"];
 
       auxiliary_physics:e -> auxiliary_physics_1:w [dir=back];
       auxiliary_physics:e -> auxiliary_physics_2:w [dir=back];
       auxiliary_physics:e -> auxiliary_physics_3:w [dir=back];
       auxiliary_physics:e -> auxiliary_physics_4:w [dir=back];
+      auxiliary_physics:e -> auxiliary_physics_5:w [dir=back];
 
-      navier_stokes_base_1_1 [label=<<B>FluidDynamicsVANS</B> <br/>(lethe-fluid-vans)>,href="https://chaos-polymtl.github.io/lethe/doxygen/classFluidDynamicsVANS.html", tooltip="FluidDynamicsVANS"];
-      navier_stokes_base_1_2 [label=<<B>FluidDynamicsSharp</B> <br/>(lethe-fluid-sharp)>,href="https://chaos-polymtl.github.io/lethe/doxygen/classFluidDynamicsSharp.html", tooltip="FluidDynamicsSharp"];
-      navier_stokes_base_1_3 [label=<<B>FluidDynamicsNitsche</B> <br/>(lethe-fluid-nitsche)>,href="https://chaos-polymtl.github.io/lethe/doxygen/classFluidDynamicsNitsche.html", tooltip="FluidDynamicsNitsche"];
+      navier_stokes_base_1_1 [label=<<B>FluidDynamicsVANS</B> <br/>(lethe-fluid-vans)>,href="https://chaos-polymtl.github.io/lethe/doxygen/classFluidDynamicsVANS.html", tooltip="FluidDynamicsVANS", fillcolor="#8ba5d4"];
+      navier_stokes_base_1_2 [label=<<B>FluidDynamicsSharp</B> <br/>(lethe-fluid-sharp)>,href="https://chaos-polymtl.github.io/lethe/doxygen/classFluidDynamicsSharp.html", tooltip="FluidDynamicsSharp", fillcolor="#8ba5d4"];
+      navier_stokes_base_1_3 [label=<<B>FluidDynamicsNitsche</B> <br/>(lethe-fluid-nitsche)>,href="https://chaos-polymtl.github.io/lethe/doxygen/classFluidDynamicsNitsche.html", tooltip="FluidDynamicsNitsche", fillcolor="#8ba5d4"];
 
       navier_stokes_base_1:e -> navier_stokes_base_1_1:w [dir=back];
       navier_stokes_base_1:e -> navier_stokes_base_1_2:w [dir=back];
       navier_stokes_base_1:e -> navier_stokes_base_1_3:w [dir=back];
 
-      navier_stokes_base_1_1_1 [label=<<B>CFDDEMSolver</B> <br/>(lethe-fluid-particles)>,href="https://chaos-polymtl.github.io/lethe/doxygen/classCFDDEMSolver.html", tooltip="CFDDEMSolver"];
+      navier_stokes_base_1_1_1 [label=<<B>CFDDEMSolver</B> <br/>(lethe-fluid-particles)>,href="https://chaos-polymtl.github.io/lethe/doxygen/classCFDDEMSolver.html", tooltip="CFDDEMSolver", fillcolor="#8ba5d4"];
 
       navier_stokes_base_1_1:e -> navier_stokes_base_1_1_1:w [dir=back];
 
-      dem_solver [label=<<B>DEM</B> <br/>(lethe-particles)>, href="https://chaos-polymtl.github.io/lethe/doxygen/classDEMSolver.html", tooltip="DEM"];
+      navier_stokes_base_3_1 [label=<<B>FluidDynamicsVANSMatrixFree</B> <br/>(lethe-fluid-vans-matrix-free)>,href="https://chaos-polymtl.github.io/lethe/doxygen/classFluidDynamicsVANSMatrixFree.html", tooltip="FluidDynamicsVANSMatrixFree", fillcolor="#8ba5d4"];
+
+      navier_stokes_base_3:e -> navier_stokes_base_3_1:w [dir=back];
+
+      navier_stokes_base_3_1_1 [label=<<B>CFDDEMMatrixFree</B> <br/>(lethe-fluid-particles-matrix-free)>,href="https://chaos-polymtl.github.io/lethe/doxygen/classCFDDEMMatrixFree.html", tooltip="CFDDEMMatrixFree", fillcolor="#8ba5d4"];
+
+      navier_stokes_base_3_1:e -> navier_stokes_base_3_1_1:w [dir=back];
     }
  * @enddot
  */
