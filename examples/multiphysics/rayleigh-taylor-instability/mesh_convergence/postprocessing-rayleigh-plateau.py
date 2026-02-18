444a35ffc1f88110b2e03fe7d12b2619f676d834
hepap <47506601+hepap@users.noreply.github.com>
2026-02-18 02:05:49 -0500

    Add cases of the VOF article (#1912)
    Description
    This PR adds all the prms and scripts used to run the cases presented in the VOF article (rising bubble 3D, capillary migration, and Rayleigh-Plateau instability). The files are as they were used, so there is place for improvement (portability between simulation. The documentation of the examples will be added later.
    
    Co-authored-by: Amishga Alphonius <107414376+AmishgaAlphonius@users.noreply.github.com>

---
+++ examples/multiphysics/rayleigh-taylor-instability/mesh_convergence/postprocessing-rayleigh-plateau.py
@@ -0,0 +1,261 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 The Lethe Authors
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
+
+#############################################################################
+"""
+Postprocessing code for Rayleigh-Plateau example
+
+"""
+#############################################################################
+
+'''Importing Libraries'''
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+from matplotlib.legend_handler import HandlerTuple
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes, mark_inset
+from matplotlib.ticker import MultipleLocator
+
+import pyvista as pv
+import argparse
+import os
+import pandas as pd
+#############################################################################
+
+'''Plot formating'''
+
+from cycler import cycler
+
+colors=[]
+pro_colors = ['#006d2c','#31a354','#74c476','#bae4b3']
+pde_colors = ['#54278f','#756bb1','#9e9ac8','#cbc9e2']
+geo_colors = ['#a63603','#e6550d','#fd8d3c','#fdbe85']
+none_colors = ['#d7b5d8','#df65b0','#dd1c77','#980043']
+
+
+plt.rcParams['figure.facecolor'] = 'white'
+plt.rcParams['figure.figsize'] = (10,8)
+plt.rcParams['lines.linewidth'] = 4
+plt.rcParams['lines.markersize'] = '11'
+plt.rcParams['markers.fillstyle'] = "none"
+plt.rcParams['lines.markeredgewidth'] = 2
+plt.rcParams['legend.columnspacing'] = 2
+plt.rcParams['legend.handlelength'] = 2.8
+plt.rcParams['legend.handletextpad'] = 0.2
+plt.rcParams['legend.frameon'] = True
+plt.rcParams['legend.fancybox'] = False
+plt.rcParams['legend.fontsize'] = '18'
+plt.rcParams['xtick.major.width'] = 2
+plt.rcParams['xtick.major.size'] = 5
+plt.rcParams['ytick.major.size'] = 5
+plt.rcParams['ytick.major.width'] = 2
+plt.rcParams['font.size'] = '25'
+plt.rcParams['font.family']='DejaVu Serif'
+plt.rcParams['font.serif']='cm'
+plt.rcParams['savefig.bbox']='tight'
+
+plt.rcParams.update({
+    'text.usetex': True,
+    'text.latex.preamble': r'\usepackage{amsfonts}'
+})
+
+#############################################################################
+def save_tight(fig, path_base):
+  '''
+
+  :param fig:
+  :param path_base:
+  :return:
+  '''
+  fig.savefig(path_base + ".png", dpi=300, bbox_inches='tight')
+  fig.savefig(path_base + ".svg", dpi=300, bbox_inches='tight')
+
+def make_legend(save_path_base, lethe_linestyle,
+                              pde_colors, geo_colors, pro_colors):
+  '''
+
+  :param save_path_base:
+  :param f_values:
+  :param c1_ref_linestyles:
+  :param c1_ref_markers:
+  :param c1_alpha:
+  :param lethe_linestyle:
+  :param pde_colors:
+  :param geo_colors:
+  :param pro_colors:
+  :return:
+  '''
+  fig = plt.figure()
+  ax = fig.add_subplot(111)
+  labels = ["Coarse", "Medium", "Fine", "Extra-fine"]
+  header = [plt.plot([], marker="", ls="")[0]]
+
+  lethe_pde = [mlines.Line2D([], [], ls=lethe_linestyle, c=pde_colors[i]) for i in range(4)]
+  lethe_geo = [mlines.Line2D([], [], ls=lethe_linestyle, c=geo_colors[i]) for i in range(4)]
+
+  lgd = ax.legend(
+    handles=header + lethe_pde +
+            header + lethe_geo,
+    labels=["PDE-based"] + labels+
+           ["Geometric"] + labels,
+    ncol=2, loc="center", frameon=True, handler_map={tuple: HandlerTuple(ndivide=None)},
+    handletextpad=0.2)
+  for vpack in lgd._legend_handle_box.get_children():
+    for hpack in vpack.get_children()[:1]:
+      hpack.get_children()[0].set_width(0)
+  ax.axis('off')
+  fig.canvas.draw()
+  bbox = lgd.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+  fig.savefig(save_path_base + ".png", dpi=300, bbox_inches=bbox)
+  fig.savefig(save_path_base + ".svg", dpi=300, bbox_inches=bbox)
+  plt.close(fig)
+
+def add_inset(ax, fig, xlims, ylims, locator=None, box=(0.8, 0.1, 0.8, 0.8), loc="center",
+              mark=(2,3), tick_right=False, tick_top=False):
+  '''
+
+  :param ax:
+  :param fig:
+  :param xlims:
+  :param ylims:
+  :param locator:
+  :param box:
+  :param loc:
+  :param mark:
+  :param tick_right:
+  :param tick_top:
+  :return:
+  '''
+  axins = inset_axes(ax, width="60%", height="60%",
+                     bbox_to_anchor=box, bbox_transform=fig.transFigure, loc=loc)
+  axins.set_xlim(*xlims); axins.set_ylim(*ylims)
+  if locator:
+    axins.yaxis.set_major_locator(MultipleLocator(locator))
+  mark_inset(ax, axins, loc1=mark[0], loc2=mark[1], fc="none", ec="0.45")
+  if tick_right:
+    axins.yaxis.tick_right()
+  if tick_top:
+    axins.xaxis.tick_top()
+  return axins
+
+parser = argparse.ArgumentParser(description='Arguments for the postprocess of the Rayleigh-Plateau case')
+parser.add_argument("-f", "--file", type=str, nargs='+', help="Path to the output folder for the results. This is the folder that contains the results of the simulation (.vtu, .pvtu, .dat and .pvd files)", required=True)
+args, leftovers=parser.parse_known_args()
+
+make_legend(f'./legend',  '-', pde_colors, geo_colors, pro_colors)
+
+for k in range(len(args.file)):
+    fig_volume = plt.figure()
+    ax_volume = fig_volume.add_subplot(111)
+
+    fig_surface = plt.figure()
+    ax_surface = fig_surface.add_subplot(111)
+
+    output_dir_base = args.file[k]
+
+    ext = ""
+    if "geo" in output_dir_base:
+        colors = geo_colors
+        ext = "geo"
+    elif "pro" in output_dir_base:
+        colors = pro_colors
+        ext = "pro"
+    elif "pde" in output_dir_base:
+        colors = pde_colors
+        ext = "pde"
+    else:
+        colors = none_colors
+        ext = "none"
+
+
+    plt.rcParams['axes.prop_cycle'] = cycler(color = colors)
+    j=0
+    for i in range(6,10):
+        output_dir = output_dir_base.replace("ref_max05", "ref_max0"+str(i))
+        filename_mass = output_dir + "/mass_conservation_information.dat"
+
+        df_mass = pd.read_csv(filename_mass, header=0, sep=r'\s+')
+
+        label = "Ref. max " + str(i)
+        ax_volume.plot(df_mass['time'], 
+            df_mass['geometric_volume_fluid_1'] / df_mass['geometric_volume_fluid_1'].iloc[0],
+            "-", label=label, linewidth=2,color=colors[j])
+
+        ax_surface.plot(df_mass['time'], df_mass['surface_fluid_1'] / df_mass['surface_fluid_1'].iloc[0],
+            "-", label=label, linewidth=2,color=colors[j])
+        j += 1
+
+    ax_volume.set_ylabel(r'$V/{V_\mathrm{0}}[-]$')
+    ax_volume.set_xlabel(r'Time [s]')
+    ax_volume.set_xlim([-0.002, 0.08])
+    ax_volume.set_ylim([0.78, 1.08])
+
+    ax_surface.set_ylabel(r'$A_\Gamma/A_{\Gamma,0}[-]$')
+    ax_surface.set_xlabel(r'Time [s]')
+    ax_surface.set_xlim([-0.002, 0.08])
+    ax_surface.set_ylim([0.76, 1.02])
+
+    fig_volume_filename = './' + ext + '-volume-evolution.png'
+    fig_volume.savefig(fig_volume_filename,dpi=300, bbox_inches='tight')
+
+    fig_surface_filename = './' + ext + '-surface-evolution.png'
+
+    fig_surface.savefig(fig_surface_filename,dpi=300, bbox_inches='tight')
+
+if (len(args.file) > 1):
+  ext = "comparison"
+  fig_volume = plt.figure()
+  ax_volume = fig_volume.add_subplot(111)
+
+  fig_surface = plt.figure()
+  ax_surface = fig_surface.add_subplot(111)
+  for k in range(len(args.file)):
+
+    output_dir_base = args.file[k]
+
+    i=9
+    if "geo" in output_dir_base:
+        colors = geo_colors
+    elif "pro" in output_dir_base:
+        colors = pro_colors
+    elif "pde" in output_dir_base:
+        colors = pde_colors
+    else:
+        colors = none_colors
+
+    plt.rcParams['axes.prop_cycle'] = cycler(color = colors)
+    j=3
+
+    output_dir = output_dir_base.replace("ref_max05", "ref_max0"+str(i))
+    filename_mass = output_dir + "/mass_conservation_information.dat"
+
+    df_mass = pd.read_csv(filename_mass, header=0, sep=r'\s+')
+
+    label = "Ref. max " + str(i)
+    ax_volume.plot(df_mass['time'], 
+        df_mass['geometric_volume_fluid_1'] / df_mass['geometric_volume_fluid_1'].iloc[0],
+        "-", label=label, linewidth=2,color=colors[j])
+
+    ax_surface.plot(df_mass['time'], df_mass['surface_fluid_1'] / df_mass['surface_fluid_1'].iloc[0],
+        "-", label=label, linewidth=2,color=colors[j])
+    j += 1
+
+  ax_volume.set_ylabel(r'$V/{V_\mathrm{0}}[-]$')
+  ax_volume.set_xlabel(r'Time [s]')
+  ax_volume.set_xlim([-0.002, 0.08])
+  ax_volume.set_ylim([0.78, 1.08])
+
+  ax_surface.set_ylabel(r'$A_\Gamma/A_{\Gamma,0}[-]$')
+  ax_surface.set_xlabel(r'Time [s]')
+  ax_surface.set_xlim([-0.002, 0.08])
+  ax_surface.set_ylim([0.76, 1.02])
+
+  fig_volume_filename = './' + ext + '-volume-evolution.png'
+  fig_volume.savefig(fig_volume_filename,dpi=300, bbox_inches='tight')
+
+  fig_surface_filename = './' + ext + '-surface-evolution.png'
+
+  fig_surface.savefig(fig_surface_filename,dpi=300, bbox_inches='tight')
+
+
---
+++ examples/multiphysics/rayleigh-taylor-instability/mesh_convergence/rayleigh-plateau-3d-geo.tpl
@@ -0,0 +1,267 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 The Lethe Authors
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
+
+# Listing of Parameters
+# ---------------------
+
+set dimension = 3
+set print parameters = all
+
+#---------------------------------------------------
+# Simulation and IO Control
+#---------------------------------------------------
+
+subsection simulation control
+  set method            = bdf2
+  set time end          = 0.08
+  set time step         = MAX_TIME_STEP
+  set output name       = CASE_NAME
+  set output frequency  = 200
+  set output path       = ./output/
+  set adapt             = true
+  set max cfl           = 0.25
+  set max time step     = MAX_TIME_STEP
+  set group files       = 96
+  set subdivision       = 1
+  #set output time interval = 0.05, 0.08
+end
+
+#---------------------------------------------------
+# Multiphysics
+#---------------------------------------------------
+
+subsection multiphysics
+  set VOF = true
+end
+
+#---------------------------------------------------
+# VOF
+#---------------------------------------------------
+
+subsection VOF
+  subsection surface tension force
+    set enable                  = true
+    set output auxiliary fields = true
+  end
+  subsection interface regularization method
+    set type      = REGULARIZATION_TYPE
+    set frequency = REGULARIZATION_FREQUENCY
+    set verbosity = extra verbose
+    subsection projection-based interface sharpening
+      set interface sharpness = 1.5
+    end
+    subsection algebraic interface reinitialization
+      set steady-state criterion        = 1e-4
+      set max steps number              = 10000
+      set diffusivity multiplier = DIFFUSIVITY_MULT
+      set diffusivity power             = 1.0
+      set reinitialization CFL          = 0.25
+    end
+    subsection geometric interface reinitialization
+      set max reinitialization distance = REGULARIZATION_DISTANCE
+      set tanh thickness = TANH_THICKNESS
+      set transformation type = tanh
+    end
+  end
+  subsection phase filtration
+    set type = tanh
+    set beta = 20
+  end
+end
+
+#---------------------------------------------------
+# Initial condition
+#---------------------------------------------------
+
+subsection initial conditions
+  set type = nodal
+  subsection uvwp
+    set Function constants  = U=1.569
+    set Function expression = if((y^2 + z^2) <= 1.3110e-6, U, 0); 0; 0; 0
+  end
+  subsection VOF
+    set Function constants  = r=1.145e-3
+    set Function expression = 0.5 - 0.5 * tanh(sign(sqrt(y*y + z*z)-r)*min(abs(sqrt(y*y + z*z)-r), REGULARIZATION_DISTANCE) / TANH_THICKNESS)
+    set smoothing type      = none
+  end
+end
+
+#---------------------------------------------------
+# Physical Properties
+#---------------------------------------------------
+
+subsection physical properties
+  set number of fluids = 2
+  subsection fluid 0
+    set density             = 1.196
+    set kinematic viscosity = 2.54e-4
+  end
+  subsection fluid 1
+    set density             = 1196
+    set kinematic viscosity = 2.54e-5
+  end
+  set number of material interactions = 1
+  subsection material interaction 0
+    set type = fluid-fluid
+    subsection fluid-fluid interaction
+      set surface tension coefficient = 0.0674
+    end
+  end
+end
+
+#---------------------------------------------------
+# Mesh
+#---------------------------------------------------
+
+subsection mesh
+  set type               = dealii
+  set grid type          = subdivided_hyper_rectangle
+  set grid arguments     = 8, 2, 2: -0.0458, -0.01145, -0.01145 : 0.0458, 0.01145, 0.01145 : true
+  set initial refinement = INITIAL_REFINEMENT
+end
+
+#---------------------------------------------------
+# Mesh Adaptation
+#---------------------------------------------------
+
+subsection mesh adaptation
+  set type                     = kelly
+  set variable                 = phase
+  set fraction type            = fraction
+  set max refinement level     = MAX_REFINEMENT
+  set min refinement level     = MIN_REFINEMENT
+  set fraction refinement      = 0.99
+  set fraction coarsening      = 0.001
+  set initial refinement steps = 4
+  set frequency                = 10
+end
+
+# --------------------------------------------------
+# Boundary Conditions
+#---------------------------------------------------
+
+subsection boundary conditions
+  set number = 6
+  set time dependent = true
+  subsection bc 0
+    set id   = 0
+    set type = function
+    subsection u
+      set Function constants  = U=1.569, delta=0.3, kappa=0.7, r=1.145e-3
+      set Function expression = if ((y^2 + z^2) <= 1.3110e-6, U*(1 + delta*sin(kappa*U*t/r)), 0)
+    end
+  end
+  subsection bc 1
+    set id   = 1
+    set type = outlet
+    set beta = 0
+  end
+  subsection bc 2
+    set id   = 2
+    set type = outlet
+    set beta = 0
+  end
+  subsection bc 3
+    set id   = 3
+    set type = outlet
+    set beta = 0
+  end
+  subsection bc 4
+    set id   = 4
+    set type = outlet
+    set beta = 0
+  end
+  subsection bc 5
+    set id   = 5
+    set type = outlet
+    set beta = 0
+  end
+end
+
+# --------------------------------------------------
+# Boundary Conditions VOF
+#---------------------------------------------------
+
+subsection boundary conditions VOF
+  set number         = 6
+  set time dependent = true
+  subsection bc 0
+    set id   = 0
+    set type = dirichlet
+    subsection dirichlet
+      set Function constants  = r=1.145e-3
+      set Function expression = 0.5 - 0.5 * tanh(sign(sqrt(y*y + z*z)-r)*min(abs(sqrt(y*y + z*z)-r), REGULARIZATION_DISTANCE) / TANH_THICKNESS)
+    end
+  end
+  subsection bc 1
+    set id = 1
+  end
+  subsection bc 2
+    set id = 2
+  end
+  subsection bc 3
+    set id = 3
+  end
+  subsection bc 4
+    set id = 4
+  end
+  subsection bc 5
+    set id = 5
+  end
+end
+
+# --------------------------------------------------
+# Non-Linear Solver Control
+#---------------------------------------------------
+
+subsection non-linear solver
+  subsection fluid dynamics
+    set tolerance      = 1e-7
+    set max iterations = 20
+  end
+  subsection VOF
+    set tolerance      = 1e-10
+    set max iterations = 20
+  end
+end
+
+# --------------------------------------------------
+# Linear Solver Control
+#---------------------------------------------------
+
+subsection linear solver
+  subsection fluid dynamics
+    set relative residual = 5e-4
+    set minimum residual  = 1e-8
+    set ilu preconditioner fill = 0
+  end
+  subsection VOF
+    set relative residual = 1e-8
+    set minimum residual  = 1e-11
+  end
+end
+
+# --------------------------------------------------
+# Timer
+#---------------------------------------------------
+
+subsection timer
+  set type = iteration
+end
+
+# --------------------------------------------------
+# Restart
+#---------------------------------------------------
+
+subsection restart
+  set checkpoint = true
+  set frequency  = 50
+  set filename   = restart
+  set restart    = false
+end
+
+
+subsection stabilization
+  set vof dcdd stabilization = false
+end
+
