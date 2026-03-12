12d9c3b957a36e13e1d4b34c9c047e62931e17ca
Amishga Alphonius <amishga1999@gmail.com>
2026-03-11 21:50:32 -0400

    Update copyright headers

--- source/solvers/analytical_solutions.cc
+++ source/solvers/analytical_solutions.cc
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright (c) 2019, 2021-2023 The Lethe Authors
+// SPDX-FileCopyrightText: Copyright (c) 2019, 2021-2023, 2026 The Lethe Authors
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
 
 #include <solvers/analytical_solutions.h>
