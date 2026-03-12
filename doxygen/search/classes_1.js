87fdf50dee741274261d270c735894790e997fa5
blaisb <blais.bruno@gmail.com>
2026-03-12 13:58:03 +0000

    Deploying to gh-pages-v3 from @ chaos-polymtl/lethe@76393680d7ce434fe64afce31123e907ef4debe5 🚀

--- doxygen/search/classes_1.js
+++ doxygen/search/classes_1.js
@@ -1,12 +1,13 @@
 var searchData=
 [
   ['bcdem_0',['BCDEM',['../structParameters_1_1Lagrangian_1_1BCDEM.html',1,'Parameters::Lagrangian']]],
-  ['blockdiagpreconditioner_1',['BlockDiagPreconditioner',['../classBlockDiagPreconditioner.html',1,'']]],
-  ['blockschurpreconditioner_2',['BlockSchurPreconditioner',['../classBlockSchurPreconditioner.html',1,'']]],
-  ['boundary_5fcells_5finfo_5fstruct_3',['boundary_cells_info_struct',['../structboundary__cells__info__struct.html',1,'']]],
-  ['boundarycellsinfo_4',['BoundaryCellsInfo',['../structIBParticlesDEM_1_1BoundaryCellsInfo.html',1,'IBParticlesDEM']]],
-  ['boundarycellsinformation_5',['BoundaryCellsInformation',['../classBoundaryCellsInformation.html',1,'']]],
-  ['boundaryconditions_6',['BoundaryConditions',['../classBoundaryConditions_1_1BoundaryConditions.html',1,'BoundaryConditions']]],
-  ['boundarypostprocessor_7',['BoundaryPostprocessor',['../classBoundaryPostprocessor.html',1,'']]],
-  ['bufferrw_8',['BufferRW',['../classBufferRW.html',1,'']]]
+  ['birminghamfluidizedbedgrid_1',['BirminghamFluidizedBedGrid',['../classBirminghamFluidizedBedGrid.html',1,'']]],
+  ['blockdiagpreconditioner_2',['BlockDiagPreconditioner',['../classBlockDiagPreconditioner.html',1,'']]],
+  ['blockschurpreconditioner_3',['BlockSchurPreconditioner',['../classBlockSchurPreconditioner.html',1,'']]],
+  ['boundary_5fcells_5finfo_5fstruct_4',['boundary_cells_info_struct',['../structboundary__cells__info__struct.html',1,'']]],
+  ['boundarycellsinfo_5',['BoundaryCellsInfo',['../structIBParticlesDEM_1_1BoundaryCellsInfo.html',1,'IBParticlesDEM']]],
+  ['boundarycellsinformation_6',['BoundaryCellsInformation',['../classBoundaryCellsInformation.html',1,'']]],
+  ['boundaryconditions_7',['BoundaryConditions',['../classBoundaryConditions_1_1BoundaryConditions.html',1,'BoundaryConditions']]],
+  ['boundarypostprocessor_8',['BoundaryPostprocessor',['../classBoundaryPostprocessor.html',1,'']]],
+  ['bufferrw_9',['BufferRW',['../classBufferRW.html',1,'']]]
 ];
--- doxygen/search/files_6.js
+++ doxygen/search/files_6.js
@@ -1,7 +1,9 @@
 var searchData=
 [
-  ['grid_5fmotion_2ecc_0',['grid_motion.cc',['../grid__motion_8cc.html',1,'']]],
-  ['grid_5fmotion_2eh_1',['grid_motion.h',['../grid__motion_8h.html',1,'']]],
-  ['grids_2ecc_2',['grids.cc',['../grids_8cc.html',1,'']]],
-  ['grids_2eh_3',['grids.h',['../grids_8h.html',1,'']]]
+  ['grid_5fbirmingham_5ffluidized_5fbed_2ecc_0',['grid_birmingham_fluidized_bed.cc',['../grid__birmingham__fluidized__bed_8cc.html',1,'']]],
+  ['grid_5fbirmingham_5ffluidized_5fbed_2eh_1',['grid_birmingham_fluidized_bed.h',['../grid__birmingham__fluidized__bed_8h.html',1,'']]],
+  ['grid_5fmotion_2ecc_2',['grid_motion.cc',['../grid__motion_8cc.html',1,'']]],
+  ['grid_5fmotion_2eh_3',['grid_motion.h',['../grid__motion_8h.html',1,'']]],
+  ['grids_2ecc_4',['grids.cc',['../grids_8cc.html',1,'']]],
+  ['grids_2eh_5',['grids.h',['../grids_8h.html',1,'']]]
 ];
