ee9c1e762ab781b9adb7044aefb8bf3268546057
Bruna Campos <bccampos@uwaterloo.ca>
2026-03-30 16:15:47 -0400

    Adding template again

--- tests/mortar/projection_matrix_01.cc
+++ tests/mortar/projection_matrix_01.cc
@@ -56,7 +56,7 @@ get_projection_matrix(const FiniteElement<dim, spacedim> &fe1,
     reference_cell.get_gauss_type_quadrature(degree + 1);
 #else
   const auto quadrature_2 =
-    reference_cell.get_gauss_type_quadrature<dim>(degree + 1);
+    reference_cell.template get_gauss_type_quadrature<dim>(degree + 1);
 #endif
 
   std::vector<Point<dim>> points;
