33fe198c05bdb53c84bdb8eb5a761e4ac5d82027
Amishga Alphonius <amishga1999@gmail.com>
2026-04-26 23:54:00 -0400

    Increase output precision

--- source/solvers/heat_transfer.cc
+++ source/solvers/heat_transfer.cc
@@ -2262,8 +2262,10 @@ HeatTransfer<dim>::postprocess_melt_volume_and_surface(const bool gather_cls)
     "time", this->simulation_control->get_current_time());
   this->melt_volume_table.set_scientific("time", true);
   this->melt_volume_table.add_value(melt_volume_column_name, melt_volume);
+  this->melt_volume_table.set_precision(melt_volume_column_name, 8);
   this->melt_volume_table.set_scientific(melt_volume_column_name, true);
   this->melt_volume_table.add_value(melt_contour_column_name, melt_surface);
+  this->melt_volume_table.set_precision(melt_contour_column_name, 8);
   this->melt_volume_table.set_scientific(melt_contour_column_name, true);
 }
 
