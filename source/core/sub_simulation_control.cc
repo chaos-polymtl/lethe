6cdaa4009b72584f6fa76c8a7ba5132ac6cb6339
Bruno Blais <blais.bruno@gmail.com>
2026-05-02 09:19:12 +0200

    Remove all clang warning + fix folder creation apple

--- source/core/sub_simulation_control.cc
+++ source/core/sub_simulation_control.cc
@@ -18,47 +18,47 @@ SubSimulationControlDEM::SubSimulationControlDEM(
       fraction_of_rayleigh_characteristic_time)
   , iteration_number(0)
 {
-  Assert(time_interval > 0,
+  Assert(this->time_interval > 0,
          ExcMessage("The time interval must be strictly positive"));
 
 
-  if (iteration_logic == DEMSubIterationLogic::fixed_number_of_iterations)
+  if (this->iteration_logic == DEMSubIterationLogic::fixed_number_of_iterations)
     {
       Assert(
-        coupling_frequency > 0,
+        this->coupling_frequency > 0,
         ExcMessage(
           "The coupling frequency must be positive. A value of zero or a negative value was encountered. The simulation will stop."));
 
       // A fixed number of iterations is requested, the time step is fixed
       // using the coupling frequency and the time interval.
-      time_step                  = time_interval / coupling_frequency;
-      total_number_of_iterations = coupling_frequency;
+      time_step = this->time_interval / this->coupling_frequency;
+      total_number_of_iterations = this->coupling_frequency;
     }
   else //(iteration_logic ==
        // DEMSubIterationLogic::fixed_fraction_of_rayleigh_time_step)
     {
-      Assert(rayleigh_characteristic_time > 0,
+      Assert(this->rayleigh_characteristic_time > 0,
              ExcMessage(
                "The Rayleigh characteristic time must be strictly positive"));
       Assert(
-        fraction_of_rayleigh_characteristic_time > 0,
+        this->fraction_of_rayleigh_characteristic_time > 0,
         ExcMessage(
           "The fraction of the Rayleigh characteristic time must be strictly positive"));
 
       // A fixed fraction of the Rayleigh time step is used. The number of
       // iterations will be manually calculated
-      time_step =
-        fraction_of_rayleigh_characteristic_time * rayleigh_characteristic_time;
+      time_step = this->fraction_of_rayleigh_characteristic_time *
+                  this->rayleigh_characteristic_time;
 
       // We calculate the total number of iterations with the proposed time step
       // We then take the ceiling of it to have a integer number of time steps
       total_number_of_iterations =
-        static_cast<unsigned int>(std::ceil(time_interval / time_step));
+        static_cast<unsigned int>(std::ceil(this->time_interval / time_step));
 
       // Since the total number of iterations needs to be an integer, the actual
       // value of the time step may have changed as a result of the previous
       // calculations. We then recalculate the time step.
-      time_step = time_interval / double(total_number_of_iterations);
+      time_step = this->time_interval / double(total_number_of_iterations);
 
       Assert(
         total_number_of_iterations > 0,
