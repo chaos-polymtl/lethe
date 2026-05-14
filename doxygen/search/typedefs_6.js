44f81b11b7128d2014c77c7243c3f4683cdb13bb
blaisb <blais.bruno@gmail.com>
2026-05-14 16:36:33 +0000

    Deploying to gh-pages-v3 from @ chaos-polymtl/lethe@abf73a640a7f13c137223faa3ff0b5b035432f8f 🚀

--- doxygen/search/typedefs_6.js
+++ doxygen/search/typedefs_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['local_5fneighbor_5flist_0',['local_neighbor_list',['../classRayTracingSolver.html#ad419665d9cb9c2ce446db3efb05fc754',1,'RayTracingSolver']]],
-  ['lstransfertype_1',['LSTransferType',['../classMFNavierStokesPreconditionGMGBase.html#a3e65523e07b2d99117fbc9827a12f333',1,'MFNavierStokesPreconditionGMGBase']]]
+  ['lstransfertype_1',['lstransfertype',['../classMFNavierStokesVANSPreconditionGMG.html#a299c5ff69809f40ba7ad0bf16bf7a161',1,'MFNavierStokesVANSPreconditionGMG::LSTransferType'],['../classMFNavierStokesPreconditionGMGBase.html#a3e65523e07b2d99117fbc9827a12f333',1,'MFNavierStokesPreconditionGMGBase::LSTransferType']]]
 ];
