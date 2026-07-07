3acc93984805525e4649a241b91635a7335dc22c
blaisb <blais.bruno@gmail.com>
2026-07-07 15:26:57 +0000

    Deploying to gh-pages-v3 from @ chaos-polymtl/lethe@fb15615f893ffc22e69b9e1de0123ac18a037941 🚀

--- doxygen/search/classes_0.js
+++ doxygen/search/classes_0.js
@@ -3,10 +3,11 @@ var searchData=
   ['adaptivesparsecontacts_0',['AdaptiveSparseContacts',['../classAdaptiveSparseContacts.html',1,'']]],
   ['adaptivesparsecontacts_3c_20dim_2c_20dem_3a_3acfddemproperties_3a_3apropertiesindex_20_3e_1',['AdaptiveSparseContacts&lt; dim, DEM::CFDDEMProperties::PropertiesIndex &gt;',['../classAdaptiveSparseContacts.html',1,'']]],
   ['adaptivesparsecontacts_3c_20dim_2c_20demproperties_3a_3apropertiesindex_20_3e_2',['AdaptiveSparseContacts&lt; dim, DEMProperties::PropertiesIndex &gt;',['../classAdaptiveSparseContacts.html',1,'']]],
-  ['ale_3',['ALE',['../classParameters_1_1ALE.html',1,'Parameters']]],
-  ['analyticalsolution_4',['AnalyticalSolution',['../classAnalyticalSolutions_1_1AnalyticalSolution.html',1,'AnalyticalSolutions']]],
-  ['auxiliaryphysics_5',['AuxiliaryPhysics',['../classAuxiliaryPhysics.html',1,'']]],
-  ['auxiliaryphysics_3c_20dim_2c_20globalvectortype_20_3e_6',['AuxiliaryPhysics&lt; dim, GlobalVectorType &gt;',['../classAuxiliaryPhysics.html',1,'']]],
-  ['averagescalar_7',['AverageScalar',['../classAverageScalar.html',1,'']]],
-  ['averagevelocities_8',['AverageVelocities',['../classAverageVelocities.html',1,'']]]
+  ['adjacent_5fparticle_5fpairs_5fvalue_3',['adjacent_particle_pairs_value',['../structDEM_1_1dem__data__structures_1_1adjacent__particle__pairs__value.html',1,'DEM::dem_data_structures']]],
+  ['ale_4',['ALE',['../classParameters_1_1ALE.html',1,'Parameters']]],
+  ['analyticalsolution_5',['AnalyticalSolution',['../classAnalyticalSolutions_1_1AnalyticalSolution.html',1,'AnalyticalSolutions']]],
+  ['auxiliaryphysics_6',['AuxiliaryPhysics',['../classAuxiliaryPhysics.html',1,'']]],
+  ['auxiliaryphysics_3c_20dim_2c_20globalvectortype_20_3e_7',['AuxiliaryPhysics&lt; dim, GlobalVectorType &gt;',['../classAuxiliaryPhysics.html',1,'']]],
+  ['averagescalar_8',['AverageScalar',['../classAverageScalar.html',1,'']]],
+  ['averagevelocities_9',['AverageVelocities',['../classAverageVelocities.html',1,'']]]
 ];
--- doxygen/search/typedefs_0.js
+++ doxygen/search/typedefs_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['adjacent_5fpairs_5ffor_5fcontact_5ft_0',['adjacent_pairs_for_contact_t',['../particle__particle__fine__search_8h.html#adac49a536881eedae454fa56e438bbda',1,'particle_particle_fine_search.h']]],
-  ['adjacent_5fparticle_5fpairs_1',['adjacent_particle_pairs',['../structDEM_1_1dem__data__structures.html#a665b75b4bfe78f3da5a8325dc70645f3',1,'DEM::dem_data_structures']]]
+  ['adjacent_5fparticle_5fpairs_1',['adjacent_particle_pairs',['../structDEM_1_1dem__data__structures.html#ab5dd40a0eabc15f878eb52200d78654c',1,'DEM::dem_data_structures']]]
 ];
