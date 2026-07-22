29f7bad20377a93fb15118c8e9ab9394c53218bc
pierrepensec <pierre.pensec@ensta-paris.fr>
2026-07-22 11:44:55 -0400

    Indent and release_notes

--- source/core/grids.cc
+++ source/core/grids.cc
@@ -330,8 +330,7 @@ attach_grid_to_triangulation(
             ExcMessage(
               "Unsupported mesh type - uniform_channel_with_meshed_square_prism mesh with simplex is not supported"));
 
-          GridCavityMw<dim, spacedim> grid(
-            mesh_parameters.grid_arguments);
+          GridCavityMw<dim, spacedim> grid(mesh_parameters.grid_arguments);
           grid.make_grid(triangulation);
 
           GridTools::scale(mesh_parameters.scale, triangulation);
