939d0da8a021f41eda38e9c21f51cfaf8e287269
Bruna Campos <bccampos@uwaterloo.ca>
2026-08-05 13:39:52 -0400

    WIP

---
+++ include/core/grid_cube_merged.h
@@ -0,0 +1,61 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026 The Lethe Authors
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
+
+#ifndef lethe_cube_merged_grid_h
+#define lethe_cube_merged_grid_h
+
+#include <deal.II/base/utilities.h>
+
+#include <deal.II/grid/manifold_lib.h>
+
+#include <sstream>
+
+using namespace dealii;
+
+/**
+ * @brief Class that creates a custom geometry that combines an extruded plate with a hole and an inner cylinder to form a cube.
+ *
+ * @tparam dim The dimension of the mesh, must be 3.
+ * @tparam spacedim The dimension of the space where the mesh is defined, must be 3.
+ */
+
+template <int dim, int spacedim>
+class GridCubeMerged
+{
+public:
+  /**
+   * @brief Constructor for the CubeMergedGrid.
+   *
+   * @param[in] grid_arguments. A string with 3 parameters, @code "subdivisions:
+   * radius : half height", separated by a colon. The subdivisions parameter is
+   * the number of subdivisions in the x direction, the radius parameter is the
+   * radius of the cylinder and the half height parameter is half the height of
+   * the cylinder.
+   */
+  GridCubeMerged(const std::string &grid_arguments);
+
+  /**
+   * @brief make_grid. The make_grid function generates cylindrical meshes with different meshing strategies for the circular part of the geometry, depending on the cylinder type specified in the constructor. It also constructs the geometry manifold with SphericalManifold.
+   *
+   * @param[out] triangulation. The triangulation object on which the grid is
+   * generated
+   */
+  void
+  make_grid(Triangulation<dim, spacedim> &triangulation);
+
+private:
+  /// Arguments used to generate the grid
+  std::string grid_arguments;
+  /// File name of the extruded plate with hole mesh
+  std::string plate_file_name;
+  /// ID corresponding to the cylindrical boundary at the extruded plate with
+  /// hole mesh
+  unsigned int cylindrical_bid_plate;
+  /// File name of the cylinder mesh
+  std::string cylinder_file_name;
+  /// ID corresponding to the cylindrical boundary at the cylinder mesh
+  unsigned int cylindrical_bid_cylinder;
+};
+
+
+#endif
---
+++ source/core/grid_cube_merged.cc
@@ -0,0 +1,104 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026 The Lethe Authors
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception OR LGPL-2.1-or-later
+
+#include <core/grid_cube_merged.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_tools.h>
+
+template <int dim, int spacedim>
+GridCylinder<dim, spacedim>::GridCylinder(const std::string &grid_arguments)
+{
+  if constexpr (!(dim == 3 && spacedim == 3))
+    {
+      AssertThrow(
+        false,
+        ExcMessage(
+          "Custom cube merged mesh is only supported in 3D space with 3D elements."));
+      return;
+    }
+
+  this->grid_arguments = grid_arguments;
+
+  // Separate arguments of the string
+  std::vector<std::string> arguments;
+  std::stringstream        s_stream(grid_arguments);
+  while (s_stream.good())
+    {
+      std::string substr;
+      getline(s_stream, substr, ':');
+      arguments.push_back(substr);
+    }
+
+  // Arguments declaration
+  if (arguments.size() != 4)
+    {
+      AssertThrow(
+        false,
+        ExcMessage(
+          "Mandatory cylinder parameters are (plate file name : cylinder file name)"));
+    }
+  else
+    {
+      this->plate_file_name          = arguments[0];
+      this->cylinder_file_name       = arguments[1];
+      this->cylindrical_bid_plate    = Utilities::string_to_int(arguments[2]);
+      this->cylindrical_bid_cylinder = Utilities::string_to_int(arguments[3]);
+
+      std::cout << "Plate: " << this->plate_file_name << std::endl;
+      std::cout << "Cylinder: " << this->cylinder_file_name << std::endl;
+    }
+}
+
+
+template <>
+void
+GridCylinder<3, 3>::make_grid(Triangulation<3, 3> &triangulation)
+{
+  // Create a temporary triangulation for the extruded plate with hole
+  Triangulation<3, 3> tria_plate;
+  GridIn<3, 3>        grid_plate;
+  grid_plate.attach_triangulation(tria_plate);
+  std::ifstream plate_input_file(this->plate_file_name);
+  grid_plate.read_msh(plate_input_file);
+
+  // Create a temporary triangulation for the inner cylinder
+  Triangulation<3, 3> tria_cylinder;
+  GridIn<3, 3>        grid_cylinder;
+  grid_cylinder.attach_triangulation(tria_cylinder);
+  std::ifstream plate_input_file(this->cylinder_file_name);
+  grid_plate.read_msh(cylinder_input_file);
+
+  // Merge triangulations
+  GridGenerator::merge_triangulations(
+    tria_plate, tria_cylinder, triangulation, 0.0, true, true);
+
+  // Set cylindrical manifolds
+  triangulation.set_manifold(this->cylindrical_bid_plate,
+                             CylindricalManifold(Tensor<1, 3>({0, 0, 1}),
+                                                 Point<3>({0, 0, 0})));
+
+  triangulation.set_manifold(this->cylindrical_bid_cylinder,
+                             CylindricalManifold(Tensor<1, 3>({0, 0, 1}),
+                                                 Point<3>({0, 0, 0})));
+}
+
+// Fallback make_grid definition for unsupported template parameters. This
+// provides a linker-visible symbol and a clear runtime error when the
+// class is instantiated for dim/spacedim combinations that are not
+// specialized above.
+template <int dim, int spacedim>
+void
+GridCylinder<dim, spacedim>::make_grid(
+  Triangulation<dim, spacedim> & /*triangulation*/)
+{
+  AssertThrow(
+    false,
+    ExcMessage(
+      "GridCylinder is only implemented for dim = 3 and spacedim = 3."));
+}
+
+// Explicit template instantiations
+template class GridCylinder<2, 2>;
+template class GridCylinder<2, 3>;
+template class GridCylinder<3, 3>;
