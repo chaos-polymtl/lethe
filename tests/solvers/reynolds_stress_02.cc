846cc6ceaa13f1a343d3c04afa94978deddb42c2
Charles Wilson <charles.wilson@etud.polymtl.ca>
2026-08-10 10:48:05 -0400

    ran pre-commit

--- tests/solvers/reynolds_stress_02.cc
+++ tests/solvers/reynolds_stress_02.cc
@@ -39,7 +39,7 @@ test()
   Parameters::SimulationControl simulation_control_parameters;
   simulation_control_parameters.method =
     Parameters::SimulationControl::TimeSteppingMethod::bdf1;
-  simulation_control_parameters.dt                                   = 0.1;
+  simulation_control_parameters.dt = 0.1;
   simulation_control_parameters.end_control =
     Parameters::SimulationControl::EndControl::time;
 
