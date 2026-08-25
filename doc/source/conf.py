101b90f55d63f4bf0e816291b2d154b7e0d489aa
Amishga Alphonius <amishga1999@gmail.com>
2026-08-25 13:44:40 -0400

    justify user documentation text

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -62,6 +62,7 @@ html_theme = 'furo'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['style.css']
 
 # Set favicon
 html_favicon = '_static/lethe_favicon.ico'
